ca709df882a9eaf8782f6e7d8044a1d9a871891e
halprin <halprin@users.noreply.github.com>
2022-10-03 16:45:35 -0600

    1: Add basic web server implementation

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/App.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/App.java
@@ -1,11 +1,15 @@
 package gov.hhs.cdc.trustedintermediary;
 
+import io.javalin.Javalin;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        var app = Javalin.create().start(8080);
+
+        app.get("/", ctx -> ctx.result(new App().getGreeting()));
     }
 }
