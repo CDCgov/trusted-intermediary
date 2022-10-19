dd8ab0178d6a11b067bb2c0e173153ce1774811c
jorge Lopez <jlopez@flexion.us>
2022-10-19 10:37:08 -0700

    Created ApplicationContext class

---
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/context/ApplicationContext.java
@@ -0,0 +1,27 @@
+/**
+ * ApplicationContext class works similar to a factory. The idea is to use this class in conjunction
+ * with annoations that will be used to inject the implementations. There is the option of using the
+ * getImplementation method to get an implementation of a class. This is a singleton class.
+ */
+package gov.hhs.cdc.trustedintermediary.context;
+
+public class ApplicationContext {
+
+    private static ApplicationContext obj;
+    private static final Map<Class<?>, Object> OBJECT_MAP = new HashMap<>();
+
+    private ApplicationContext() {}
+
+    public static void register(Class<?> clazz, Object implementation) {
+        this.OBJECT_MAP.put(clazz, implementation);
+    }
+
+    public static <T> T getImplementation(Class<T> clazz) {
+        return (T) this.OBJECT_MAP.get(clazz);
+    }
+
+    public static synchronized ApplicationContext getInstance() {
+        if (this.obj == null) this.obj = new ApplicationContext();
+        return this.obj;
+    }
+}
