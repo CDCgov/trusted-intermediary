46318d1f6321d3c93c8e3eb6376f3ac736fbcd5c
jorge Lopez <jlopez@flexion.us>
2022-10-19 10:52:21 -0700

    double checked locking

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/context/ApplicationContext.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/context/ApplicationContext.java
@@ -1,13 +1,14 @@
 /**
  * ApplicationContext class works similar to a factory. The idea is to use this class in conjunction
  * with annoations that will be used to inject the implementations. There is the option of using the
- * getImplementation method to get an implementation of a class. This is a singleton class.
+ * getImplementation method to get an implementation of a class. This is a double checked locking
+ * singleton class.
  */
 package gov.hhs.cdc.trustedintermediary.context;
 
 public class ApplicationContext {
 
-    private static ApplicationContext applicationContext = new ApplicationContext();
+    private static volatile ApplicationContext applicationContext = null;
     private static final Map<Class<?>, Object> OBJECT_MAP = new HashMap<>();
 
     private ApplicationContext() {}
@@ -21,6 +22,12 @@ public class ApplicationContext {
     }
 
     public static ApplicationContext getInstance() {
+        if (this.applicationContext == null) {
+            synchronized (ApplicationContext.class) {
+                if (this.applicationContext == null)
+                    this.applicationContext = new ApplicationContext();
+            }
+        }
         return this.applicationContext;
     }
 }
