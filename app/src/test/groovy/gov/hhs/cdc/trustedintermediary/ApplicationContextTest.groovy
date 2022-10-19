62e75184b05cdd80b52ee857f1ba8554cf323b75
jorge Lopez <jlopez@flexion.us>
2022-10-19 14:00:53 -0700

    Added test case for implementation retrieval

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/ApplicationContextTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/ApplicationContextTest.groovy
@@ -3,15 +3,25 @@ package gov.hhs.cdc.trustedintermediary.context
 import spock.lang.Specification
 
 class ApplicationContextTest extends Specification {
-    def "singleton object"() {
+    def "singleton object test"() {
         setup:
         def contextA = ApplicationContext.getInstance()
         def contextB = ApplicationContext.getInstance()
 
         when:
-        def result = contextA == contextB?true:false
+        def result = contextA == contextB
 
         then:
         result == true
+    }
+
+    def "implementation retrieval test"() {
+        setup:
+        def context = ApplicationContext.getInstance()
+        def result = "DogCow"
+        context.register(String.class, "DogCow")
+
+        expect:
+        result == context.getImplementation(String.class)
     }
 }
