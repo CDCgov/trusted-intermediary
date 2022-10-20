2806c5797193706b97e2127125be61286226bc3b
halprin <halprin@users.noreply.github.com>
2022-10-20 16:58:00 -0600

    13: Swap the Unit tests for the DomainRequest and DomainResponse

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/domainconnector/DomainResponseTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/domainconnector/DomainResponseTest.groovy
@@ -3,10 +3,10 @@ package gov.hhs.cdc.trustedintermediary.domainconnector
 import gov.hhs.cdc.trustedintermediary.PojoTestUtils
 import spock.lang.Specification
 
-class DomainRequestTest extends Specification {
+class DomainResponseTest extends Specification {
     def "test getters and setters"() {
         when:
-        PojoTestUtils.validateGettersAndSetters(DomainRequest.class)
+        PojoTestUtils.validateGettersAndSetters(DomainResponse.class)
 
         then:
         noExceptionThrown()
