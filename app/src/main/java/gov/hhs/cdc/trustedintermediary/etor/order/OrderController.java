bae4dceb645461fa8a52a9dd6d53e49f6a3228b8
halprin <halprin@users.noreply.github.com>
2022-11-10 15:42:15 -0700

    13: Created a dummy order controller

---
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/order/OrderController.java
@@ -0,0 +1,16 @@
+package gov.hhs.cdc.trustedintermediary.etor.order;
+
+public class OrderController {
+
+    private static final OrderController ORDER_CONTROLLER = new OrderController();
+
+    private OrderController() {}
+
+    public static OrderController getInstance() {
+        return ORDER_CONTROLLER;
+    }
+
+    public String parseOrder(String requestBody) {
+        return "DogCow sent in a lab order";
+    }
+}
