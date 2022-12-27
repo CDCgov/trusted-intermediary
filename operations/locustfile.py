5db93a4b28095e4f19860b912dcf6cfcfe886311
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2022-12-27 16:02:10 -0500

    WIP dod/76/load_testing_implementation: latency test for /health
    - WIP
    - latency test for 1 sec load time

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -1,15 +1,31 @@
-from locust import HttpUser, task, between
+import time
+from locust import HttpUser, User, task, constant
+
 
 class SampleUser(HttpUser):
-
-    wait_time = between(1, 5)
 
     # Each task gets called randomly, but the number next to task denotes
     # how many more times that task will get called
     @task
-    def hello_health(self):
-        self.client.get("/health")
+    def get_health_response(self):
+        with self.client.get("/health", catch_response=True) as response:
+            if response.text != "Operational":
+                response.failure("Unexpected response: " + response.text)
 
-    @task(5)  # this task will get called 5x more than the other
+    @task
+    def health_latency(self):
+        with self.client.get("/health", catch_response=True) as response:
+            if response.elapsed.total_seconds() < 1:
+                response.success()
+            elif response.elapsed.total_seconds() > 1.001:
+                response.failure("Response took longer than 1000 ms")
+
+    @task  # this task will get called 5x more than the other
+    def post_v1_etor_order_response(self):
+        with self.client.post("/v1/etor/order", catch_response=True) as response:
+            if response.text != "DogCow sent in a lab order":
+                response.failure("Unexpected response: " + response.text)
+
+    @task  # this task will get called 5x more than the other
     def hello_etor(self):
         self.client.post("/v1/etor/order")
