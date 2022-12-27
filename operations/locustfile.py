98e98946c38e19a3f58c2340a1f48fa62ee227a3
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2022-12-27 16:11:59 -0500

    dod/76/load_testing_implementation: add latency test for etor endpoint
    - added latency test for v1/etor/orders endpoint
    - fixed imports, renamed tests

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -1,5 +1,5 @@
 import time
-from locust import HttpUser, User, task, constant
+from locust import HttpUser, task
 
 
 class SampleUser(HttpUser):
@@ -7,25 +7,30 @@ class SampleUser(HttpUser):
     # Each task gets called randomly, but the number next to task denotes
     # how many more times that task will get called
     @task
-    def get_health_response(self):
+    def check_response_health(self):
         with self.client.get("/health", catch_response=True) as response:
             if response.text != "Operational":
                 response.failure("Unexpected response: " + response.text)
 
     @task
-    def health_latency(self):
+    def latency_get_health(self):
         with self.client.get("/health", catch_response=True) as response:
             if response.elapsed.total_seconds() < 1:
                 response.success()
             elif response.elapsed.total_seconds() > 1.001:
                 response.failure("Response took longer than 1000 ms")
 
-    @task  # this task will get called 5x more than the other
-    def post_v1_etor_order_response(self):
+    @task
+    def check_response_v1_etor_orders(self):
         with self.client.post("/v1/etor/order", catch_response=True) as response:
             if response.text != "DogCow sent in a lab order":
                 response.failure("Unexpected response: " + response.text)
 
-    @task  # this task will get called 5x more than the other
-    def hello_etor(self):
-        self.client.post("/v1/etor/order")
+    @task(5)  # this task will get called 5x more than the other
+    # this task will get called 5x more than the other
+    def latency_post_v1_etor_orders(self):
+        with self.client.post("/v1/etor/order", catch_response=True) as response:
+            if response.elapsed.total_seconds() < 1:
+                response.success()
+            elif response.elapsed.total_seconds() > 1.001:
+                response.failure("Response took longer than 1000 ms")
