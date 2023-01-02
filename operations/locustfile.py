fdbd1f6a18d91a2a5e32ca43910dd6896ac70bd9
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-01-02 08:57:24 -0500

    WIP dod/76/load_testing_implementation: refactoring
    - working on refactoring
    - added constants for latency failure tests.

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -14,11 +14,13 @@ class SampleUser(HttpUser):
 
     @task
     def latency_get_health(self):
+        MAX_SPEED = 1.000
+        OVER_ONE_SECOND = "Response took longer than 1000 ms"
         with self.client.get("/health", catch_response=True) as response:
-            if response.elapsed.total_seconds() < 1.000:
+            if response.elapsed.total_seconds() < MAX_SPEED:
                 response.success()
-            elif response.elapsed.total_seconds() >= 1.000:
-                response.failure("Response took longer than 1000 ms")
+            elif response.elapsed.total_seconds() >= MAX_SPEED:
+                response.failure(OVER_ONE_SECOND)
 
     @task
     def check_response_v1_etor_orders(self):
@@ -29,8 +31,10 @@ class SampleUser(HttpUser):
     @task(5)  # this task will get called 5x more than the other
     # this task will get called 5x more than the other
     def latency_post_v1_etor_orders(self):
+        MAX_SPEED = 1.000
+        OVER_ONE_SECOND = "Response took longer than 1000 ms"
         with self.client.post("/v1/etor/order", catch_response=True) as response:
-            if response.elapsed.total_seconds() < 1:
+            if response.elapsed.total_seconds() < MAX_SPEED:
                 response.success()
-            elif response.elapsed.total_seconds() > 1.001:
-                response.failure("Response took longer than 1000 ms")
+            elif response.elapsed.total_seconds() >= MAX_SPEED:
+                response.failure(OVER_ONE_SECOND)
