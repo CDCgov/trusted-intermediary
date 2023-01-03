1a19ad296f2a6fd6e0f2cdfded6f7b3b697a8628
halprin <halprin@users.noreply.github.com>
2023-01-03 10:45:34 -0700

    76: Remove unused methods from Order

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/order/Order.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/order/Order.java
@@ -1,8 +1,5 @@
 package gov.hhs.cdc.trustedintermediary.etor.order;
 
-import java.time.LocalDateTime;
-import java.util.regex.Pattern;
-
 public class Order {
     private String id;
     private String destination;
@@ -60,26 +57,6 @@ public class Order {
     }
 
     public void setCreatedAt(String createdAt) {
-
         this.createdAt = createdAt;
-    }
-
-    //    public void setCreateAt(LocalDateTime createdAt, DateTimeFormatter format) {
-    //        LocalDateTime timestamp = LocalDateTime.parse(createdAt);
-    //        this.createAt = timestamp.format(format);
-    //    }
-
-    private boolean isValidDateTime(LocalDateTime createdAt) {
-        String dateTimeFormatRegex =
-                "\\b(0[1-9]|1[0-2])(\\s|-){1}([1-9]|[12][0-9]|3[01])(\\s|-){1}(19[0-9][0-9]|20[0-2][0-3])(\\s)([1-9]|1[0-9]|2[01234])(:)(0[0-9]|[1-5][0-9])\\b";
-        String ldtRegex =
-                "\\b(19[0-9][0-9]|20[0-2][0-3])(-)"
-                        + "(0[1-9]|[1-9]|1[0-2])(-)"
-                        + "(0[1-9]|[1-9]|[12][0-9]|3[01])(T)"
-                        + "(0[1-9]|[1-5][0-9])(:)"
-                        + "(0[1-9]|[1-5][0-9])(:)"
-                        + "(0[1-9]|[1-5][0-9])([\\.]\\d+)?\\b";
-        Pattern pattern = Pattern.compile(ldtRegex);
-        return pattern.matcher(createdAt.toString()).matches();
     }
 }
