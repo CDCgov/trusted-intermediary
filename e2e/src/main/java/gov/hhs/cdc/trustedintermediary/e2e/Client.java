3098cbcf57515cf3d266c50fe027a189f6eda327
halprin <halprin@users.noreply.github.com>
2023-01-03 15:16:04 -0700

    76: Integration tests pass

--- e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/Client.java
+++ e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/Client.java
@@ -1,11 +1,14 @@
 package gov.hhs.cdc.trustedintermediary.e2e;
 
 import java.io.IOException;
-import java.util.Map;
+import org.apache.hc.client5.http.fluent.Content;
+import org.apache.hc.client5.http.fluent.ContentResponseHandler;
 import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.impl.classic.AbstractHttpClientResponseHandler;
+import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
-import org.apache.hc.core5.http.Header;
-import org.apache.hc.core5.http.message.BasicHeader;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
 
 /** Mocks a client sending a request to the API * */
 public class Client {
@@ -18,30 +21,39 @@ public class Client {
 
         var response = Request.get(protocolDomain + path).execute();
 
-        return response.returnContent().asString();
+        return response.handleResponse(new ContentResponseHandlerWithoutException()).asString();
     }
 
-    public static String post(String path) throws IOException {
+    public static String post(String path, String body) throws IOException {
         System.out.println("Calling the backend at POST " + path);
 
-        var response = Request.post(protocolDomain + path).execute();
-
-        return response.returnContent().asString();
-    }
-
-    public static String post(String path, Map<String, String> headers, String body)
-            throws IOException {
-        System.out.println("Calling the backend at POST " + path);
-
-        Header destination = new BasicHeader("Destination", headers.get("Destination"));
-        Header client = new BasicHeader("Client", headers.get("Client"));
         var response =
                 Request.post(protocolDomain + path)
-                        .addHeader(destination)
-                        .addHeader(client)
                         .bodyString(body, ContentType.APPLICATION_JSON)
                         .execute();
 
-        return response.returnContent().asString();
+        return response.handleResponse(new ContentResponseHandlerWithoutException()).asString();
+    }
+
+    public static class ContentResponseHandlerWithoutException extends ContentResponseHandler {
+
+        /**
+         * Very similar code to {@link
+         * AbstractHttpClientResponseHandler#handleResponse(ClassicHttpResponse)} but doesn't throw
+         * an exception when getting a 4xx or 5xx status code.
+         */
+        @Override
+        public Content handleResponse(final ClassicHttpResponse response) throws IOException {
+
+            if (response.getCode() >= HttpStatus.SC_REDIRECTION) {
+                HttpEntity entity = response.getEntity();
+                if (entity == null) {
+                    return null;
+                }
+                return handleEntity(entity);
+            }
+
+            return super.handleResponse(response);
+        }
     }
 }
---
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/OrderTest.groovy
@@ -0,0 +1,37 @@
+package gov.hhs.cdc.trustedintermediary.e2e
+
+import spock.lang.Specification
+
+class OrderTest extends Specification {
+
+    def "order message is returned from demo ETOR order endpoint"() {
+        given:
+        def expected = """{"id":"an ID","destination":"Massachusetts","createdAt":"2022-12-21T08:34:27Z","client":"MassGeneral","content":"missing content"}"""
+
+        when:
+        def responseBody = Client.post("/v1/etor/order","""{
+                "id": "an ID",
+                "destination": "Massachusetts",
+                "createdAt": "2022-12-21T08:34:27Z",
+                "client": "MassGeneral"
+            }""")
+
+        then:
+        responseBody == expected
+    }
+
+    def "bad response given poorly formatted JSON"() {
+
+        when:
+        def responseBody = Client.post("/v1/etor/order","""{
+                "id": "an ID",
+                "destination": "Massachusetts",
+                "createdAt": "2022-12-21T08:34:27Z",
+                "client": "MassGeneral"
+            """)
+        //notice the missing end } above
+
+        then:
+        responseBody == "Server Error"
+    }
+}
