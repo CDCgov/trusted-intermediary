195ec03e232e4ac8e83cd572118688d1c76bfe6d
halprin <halprin@users.noreply.github.com>
2023-01-04 15:28:20 -0700

    76: name clean-up

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -7,12 +7,11 @@ class SampleUser(HttpUser):
     # how many more times that task will get called than other tasks
 
     @task
-    def latency_get_health(self):
+    def get_health(self):
         self.client.get("/health")
 
     @task(5)  # this task will get called 5x more than the other
-    # this task will get called 5x more than the other
-    def latency_post_v1_etor_orders(self):
+    def post_v1_etor_orders(self):
         self.client.post("/v1/etor/order", json={
             "id": "asdf-12341-jkl-7890",
             "destination": "Massachusetts",
@@ -22,7 +21,7 @@ class SampleUser(HttpUser):
 
 
 @events.quitting.add_listener
-def assert_latency_stats(environment):
+def assert_stats(environment):
     if environment.stats.total.fail_ratio > 0.01:
         print("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
