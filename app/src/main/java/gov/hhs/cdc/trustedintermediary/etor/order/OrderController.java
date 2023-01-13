10a05b4ef9ef6115bfa1808d086f0933af52a914
halprin <halprin@users.noreply.github.com>
2023-01-13 08:59:25 -0700

    77: Push the latest stuff

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/order/OrderController.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/order/OrderController.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.etor.order;
 
+import ca.uhn.fhir.context.FhirContext;
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest;
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainResponse;
 import gov.hhs.cdc.trustedintermediary.wrappers.Formatter;
@@ -7,6 +8,7 @@ import gov.hhs.cdc.trustedintermediary.wrappers.FormatterProcessingException;
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
 import java.util.Map;
 import javax.inject.Inject;
+import org.hl7.fhir.instance.model.api.IBase;
 
 /**
  * Creates an in-memory representation of an order to be ingested by the system, and return response
@@ -30,14 +32,23 @@ public class OrderController {
 
     public Order parseOrder(DomainRequest request) {
         logger.logInfo("Parsing order");
-        Order order;
+        Order order = new Order();
 
-        try {
-            order = formatter.convertToObject(request.getBody(), Order.class);
-        } catch (FormatterProcessingException e) {
-            logger.logError("Unable to convert request body to order object");
-            throw new RuntimeException(e);
-        }
+        //        try {
+        //            order = formatter.convertToObject(request.getBody(), Order.class);
+        //        } catch (FormatterProcessingException e) {
+        //            logger.logError("Unable to convert request body to order object");
+        //            throw new RuntimeException(e);
+        //        }
+
+        FhirContext context = FhirContext.forR4();
+        var pather = context.newFhirPath();
+        var parser = context.newJsonParser();
+        var terser = context.newTerser();
+        var patient = parser.parseResource(request.getBody());
+
+        var answer = pather.evaluateFirst(patient, "/dogcow", IBase.class);
+        logger.logInfo("answer=" + answer);
 
         return order;
     }
