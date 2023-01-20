28eea16381056b5223d24693f2b5ff432a0eaed6
halprin <halprin@users.noreply.github.com>
2023-01-20 16:14:05 -0700

    77: Get closer to the unit test working

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsControllerTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsControllerTest.groovy
@@ -2,9 +2,15 @@ package gov.hhs.cdc.trustedintermediary.etor.demographics
 
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest
+import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
 import gov.hhs.cdc.trustedintermediary.wrappers.Formatter
 import gov.hhs.cdc.trustedintermediary.wrappers.FormatterProcessingException
-import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
+import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
+import org.hl7.fhir.instance.model.api.IBase
+import org.hl7.fhir.r4.model.DateTimeType
+import org.hl7.fhir.r4.model.IdType
+import org.hl7.fhir.r4.model.IntegerType
+import org.hl7.fhir.r4.model.StringType
 import spock.lang.Specification
 
 class PatientDemographicsControllerTest extends Specification {
@@ -19,9 +25,15 @@ class PatientDemographicsControllerTest extends Specification {
         given:
         def mockOrderId = "asdf-12341-jkl-7890"
 
-        def formatter = Mock(Jackson)
-        //        formatter.convertToObject(_ as String, _ as Class) >> new PatientDemographics(mockOrderId, "Massachusetts", "2022-12-21T08:34:27Z", "MassGeneral", "NBS panel for Clarus the DogCow")
-        TestApplicationContext.register(Formatter, formatter)
+        def fhir = Mock(HapiFhir)
+        fhir.fhirPathEvaluateFirst(_ as IBase, "id", IdType) >> Optional.of(new IdType("requestId"))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "identifier.value", _ as Class<? extends IBase>) >> new StringType("patientId")
+        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').given.first()", _ as Class<? extends IBase>) >> new StringType("Clarus")
+        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').family", _ as Class<? extends IBase>) >> new StringType("DogCow")
+        fhir.fhirPathEvaluateFirst(_ as IBase, "birthDate.extension.where(url='http://hl7.org/fhir/StructureDefinition/patient-birthTime').value", _ as Class<? extends IBase>) >> new DateTimeType("2022-12-21T08:34:27Z")
+        fhir.fhirPathEvaluateFirst(_ as IBase, "multipleBirth", _ as Class<? extends IBase>) >> new IntegerType(1)
+
+        TestApplicationContext.register(HapiFhir, fhir)
 
         def request = new DomainRequest()
 
