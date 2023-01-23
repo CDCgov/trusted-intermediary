82544ae084f56e93cd59a7804b988f8d565c2f0f
halprin <halprin@users.noreply.github.com>
2023-01-23 15:05:25 -0600

    77: got the mocking working

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsControllerTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsControllerTest.groovy
@@ -2,16 +2,16 @@ package gov.hhs.cdc.trustedintermediary.etor.demographics
 
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirImplementation
 import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
 import gov.hhs.cdc.trustedintermediary.wrappers.Formatter
 import gov.hhs.cdc.trustedintermediary.wrappers.FormatterProcessingException
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
 import org.hl7.fhir.instance.model.api.IBase
-import org.hl7.fhir.r4.model.DateTimeType
-import org.hl7.fhir.r4.model.IdType
-import org.hl7.fhir.r4.model.IntegerType
-import org.hl7.fhir.r4.model.StringType
+import org.hl7.fhir.r4.model.*
 import spock.lang.Specification
+
+import java.time.ZonedDateTime
 
 class PatientDemographicsControllerTest extends Specification {
 
@@ -23,15 +23,25 @@ class PatientDemographicsControllerTest extends Specification {
 
     def "parseDemographics works"() {
         given:
-        def mockOrderId = "asdf-12341-jkl-7890"
+        def mockRequestId = "asdf-12341-jkl-7890"
+        def mockPatientId = "patientId"
+        def mockFirstName = "Clarus"
+        def mockLastName = "DogCow"
+        def mockSex = Enumerations.AdministrativeGender.UNKNOWN.toCode()
+        def mockBirthDate = "2022-12-21T08:34:27Z"
+        def mockBirthNumber = 1
 
-        def fhir = Mock(HapiFhir)
-        fhir.fhirPathEvaluateFirst(_ as IBase, "id", IdType) >> Optional.of(new IdType("requestId"))
-        fhir.fhirPathEvaluateFirst(_ as IBase, "identifier.value", _ as Class<? extends IBase>) >> new StringType("patientId")
-        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').given.first()", _ as Class<? extends IBase>) >> new StringType("Clarus")
-        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').family", _ as Class<? extends IBase>) >> new StringType("DogCow")
-        fhir.fhirPathEvaluateFirst(_ as IBase, "birthDate.extension.where(url='http://hl7.org/fhir/StructureDefinition/patient-birthTime').value", _ as Class<? extends IBase>) >> new DateTimeType("2022-12-21T08:34:27Z")
-        fhir.fhirPathEvaluateFirst(_ as IBase, "multipleBirth", _ as Class<? extends IBase>) >> new IntegerType(1)
+        def fhir = Mock(HapiFhirImplementation)
+
+        fhir.parseResource(_ as String, _ as Class) >> new Patient()
+
+        fhir.fhirPathEvaluateFirst(_ as IBase, "id", IdType) >> Optional.of(new IdType(mockRequestId))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "identifier.value", StringType) >> Optional.of(new StringType(mockPatientId))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').given.first()", StringType) >> Optional.of(new StringType(mockFirstName))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "name.where(use='official').family", StringType) >> Optional.of(new StringType(mockLastName))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "gender", Enumeration) >> Optional.of(new Enumeration<>(new Enumerations.AdministrativeGenderEnumFactory(), Enumerations.AdministrativeGender.fromCode(mockSex)))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "birthDate.extension.where(url='http://hl7.org/fhir/StructureDefinition/patient-birthTime').value", DateTimeType) >> Optional.of(new DateTimeType(mockBirthDate))
+        fhir.fhirPathEvaluateFirst(_ as IBase, "multipleBirth", IntegerType) >> Optional.of(new IntegerType(mockBirthNumber))
 
         TestApplicationContext.register(HapiFhir, fhir)
 
@@ -40,11 +50,16 @@ class PatientDemographicsControllerTest extends Specification {
         TestApplicationContext.injectRegisteredImplementations()
 
         when:
-        def parsedOrder = PatientDemographicsController.getInstance().parseDemographics(request)
+        def patientDemographics = PatientDemographicsController.getInstance().parseDemographics(request)
 
         then:
-        noExceptionThrown()
-        parsedOrder.getRequestId() == mockOrderId
+        patientDemographics.getRequestId() == mockRequestId
+        patientDemographics.getPatientId() == mockPatientId
+        patientDemographics.getFirstName() == mockFirstName
+        patientDemographics.getLastName() == mockLastName
+        patientDemographics.getSex() == mockSex
+        patientDemographics.getBirthDateTime() == ZonedDateTime.parse(mockBirthDate)
+        patientDemographics.getBirthOrder() == mockBirthNumber
     }
 
     def "parseDemographics fails by the formatter"() {
