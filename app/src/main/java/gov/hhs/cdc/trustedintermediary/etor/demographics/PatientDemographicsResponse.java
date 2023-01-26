8a16415690edb6d65364b7e11c7c45d9ee80c283
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-01-25 20:02:58 -0500

    story-77-fhir_in-finij: modified response body
    - modified response body to extract demographic data
    - e2e tests pass when app is running

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsResponse.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/demographics/PatientDemographicsResponse.java
@@ -1,11 +1,17 @@
 package gov.hhs.cdc.trustedintermediary.etor.demographics;
 
+import java.time.ZonedDateTime;
 import javax.annotation.Nonnull;
 
 /** Contains an ID that is reflected from the patient demographic data. */
 public class PatientDemographicsResponse {
 
     private String id;
+    private String firstName;
+    private String lastName;
+    private String sex;
+    private ZonedDateTime birthDateTime;
+    private Integer birthOrder;
 
     public PatientDemographicsResponse(String id) {
         setId(id);
@@ -13,6 +19,11 @@ public class PatientDemographicsResponse {
 
     public PatientDemographicsResponse(@Nonnull PatientDemographics patientDemographics) {
         setId(patientDemographics.getRequestId());
+        setFirstName(patientDemographics.getFirstName());
+        setLastName(patientDemographics.getLastName());
+        setSex(patientDemographics.getSex());
+        //        setBirthDateTime(patientDemographics.getBirthDateTime());
+        setBirthOrder(patientDemographics.getBirthOrder());
     }
 
     public String getId() {
@@ -21,5 +32,45 @@ public class PatientDemographicsResponse {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public ZonedDateTime getBirthDateTime() {
+        return birthDateTime;
+    }
+
+    public void setBirthDateTime(ZonedDateTime birthDateTime) {
+        this.birthDateTime = birthDateTime;
+    }
+
+    public Integer getBirthOrder() {
+        return birthOrder;
+    }
+
+    public void setBirthOrder(Integer birthOrder) {
+        this.birthOrder = birthOrder;
     }
 }
