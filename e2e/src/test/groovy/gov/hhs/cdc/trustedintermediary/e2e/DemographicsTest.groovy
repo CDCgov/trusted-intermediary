e13708ad132c6f84378bc6e014514115393e324b
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-02-01 15:01:37 -0500

    task/77: setup spec to use json files
    - replaced call to json with call to json file

--- e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/DemographicsTest.groovy
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/DemographicsTest.groovy
@@ -4,102 +4,19 @@ import spock.lang.Specification
 
 class DemographicsTest extends Specification {
 
-    def newbornPatient = """{
-          "resourceType": "Patient",
-          "id": "infant-twin-1",
-          "text": {
-            "status": "generated",
-            "div": "<div xmlns=\\"http://www.w3.org/1999/xhtml\\"><p><b>Jaina Solo (OFFICIAL)</b> female, DoB: 2017-05-15 ( Medical record number: MRN7465737865)</p></div>"
-          },
-          "extension": [
-            {
-              "url": "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName",
-              "valueString": "Organa"
-            }
-          ],
-          "identifier": [
-            {
-              "type": {
-                "coding": [
-                  {
-                    "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
-                    "code": "MR"
-                  }
-                ]
-              },
-              "system": "http://coruscanthealth.org/main-hospital/patient-identifier",
-              "value": "MRN7465737865"
-            },
-            {
-              "system": "http://new-republic.gov/galactic-citizen-identifier",
-              "value": "7465737865"
-            }
-          ],
-          "name": [
-            {
-              "use": "official",
-              "family": "Solo",
-              "given": [
-                "Jaina"
-              ]
-            }
-          ],
-          "gender": "female",
-          "birthDate": "2017-05-15",
-          "_birthDate": {
-            "extension": [
-              {
-                "url": "http://hl7.org/fhir/StructureDefinition/patient-birthTime",
-                "valueDateTime": "2017-05-15T17:11:00+01:00"
-              }
-            ]
-          },
-          "multipleBirthInteger": 1,
-          "contact": [
-            {
-              "relationship": [
-                {
-                  "coding": [
-                    {
-                      "system": "http://snomed.info/sct",
-                      "code": "72705000",
-                      "display": "Mother"
-                    },
-                    {
-                      "system": "http://terminology.hl7.org/CodeSystem/v2-0131",
-                      "code": "N"
-                    },
-                    {
-                      "system": "http://terminology.hl7.org/CodeSystem/v3-RoleCode",
-                      "code": "MTH"
-                    }
-                  ]
-                }
-              ],
-              "name": {
-                "use": "maiden",
-                "family": "Organa",
-                "given": [
-                  "Leia"
-                ]
-              },
-              "telecom": [
-                {
-                  "system": "phone",
-                  "value": "+31201234567",
-                  "use": "mobile"
-                }
-              ]
-            }
-          ]
-        }"""
+
+    def newbornPatientFilePath = "src/test/resources/newborn_patient.json"
+    def newbornPatientJsonFile = new File(newbornPatientFilePath)
+    def newbornPatientJsonFileString = newbornPatientJsonFile.getText("UTF-8")
+
+
 
     def "a demographics response is returned from the ETOR demographics endpoint"() {
         given:
         def expected = """{"fhirResourceId":"Patient/infant-twin-1","patientId":"MRN7465737865"}"""
 
         when:
-        def responseBody = Client.post("/v1/etor/demographics", newbornPatient)
+        def responseBody = Client.post("/v1/etor/demographics", newbornPatientJsonFileString)
 
         then:
         responseBody == expected
@@ -108,7 +25,7 @@ class DemographicsTest extends Specification {
     def "bad response given for poorly formatted JSON"() {
 
         when:
-        def responseBody = Client.post("/v1/etor/demographics", newbornPatient.substring(1))
+        def responseBody = Client.post("/v1/etor/demographics", newbornPatientJsonFileString.substring(1))
         //removed beginning '{' to make this JSON invalid
 
         then:
