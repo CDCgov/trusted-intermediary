031433440e7f2cbcfb891cf01d9c456fb6548c6b
halprin <halprin@users.noreply.github.com>
2023-02-14 11:15:15 -0600

    79: Get the unit test to pass

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistrationTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistrationTest.groovy
@@ -4,6 +4,7 @@ import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainResponse
 import gov.hhs.cdc.trustedintermediary.domainconnector.HttpEndpoint
+import gov.hhs.cdc.trustedintermediary.etor.demographics.ConvertAndSendLabOrderUsecase
 import gov.hhs.cdc.trustedintermediary.etor.demographics.NextOfKin
 import gov.hhs.cdc.trustedintermediary.etor.demographics.PatientDemographics
 import gov.hhs.cdc.trustedintermediary.etor.demographics.PatientDemographicsController
@@ -56,11 +57,14 @@ class EtorDomainRegistrationTest extends Specification {
         mockDemographicsController.parseDemographics(_ as DomainRequest) >> new PatientDemographics(mockRequestId, "a patient ID", "George", "Washington", "male", ZonedDateTime.now(), 1, "Asian", new NextOfKin("King", "George", "555-867-5309"))
         mockDemographicsController.constructResponse(_ as PatientDemographicsResponse) >> new DomainResponse(418)
 
-        def domainRequest = new DomainRequest()
+        def mockUsecase = Mock(ConvertAndSendLabOrderUsecase)
 
         TestApplicationContext.register(EtorDomainRegistration, domainRegistration)
         TestApplicationContext.register(PatientDemographicsController, mockDemographicsController)
+        TestApplicationContext.register(ConvertAndSendLabOrderUsecase, mockUsecase)
         TestApplicationContext.injectRegisteredImplementations()
+
+        def domainRequest = new DomainRequest()
 
         when:
         def response = domainRegistration.handleOrder(domainRequest)
