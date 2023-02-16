751b37f14d5b642e6b8458de669455482bd0268a
halprin <halprin@users.noreply.github.com>
2023-02-16 16:52:31 -0600

    79: Unit test the patient conversion

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderConverterTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderConverterTest.groovy
@@ -1,27 +1,56 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi
 
+import gov.hhs.cdc.trustedintermediary.etor.demographics.NextOfKin
 import gov.hhs.cdc.trustedintermediary.etor.demographics.PatientDemographics
+import org.hl7.fhir.r4.model.DateTimeType
+import org.hl7.fhir.r4.model.Patient
 import spock.lang.Specification
 
+import java.time.ZonedDateTime
+
 class HapiLabOrderConverterTest extends Specification {
-    def "conversion between demographics and the LabOrder is correct"() {
+    def "the demographics correctly constructs a patient in the lab order"() {
         given:
+        def fhirResourceId = "fhir123id"
+        def patientId = "patient123id"
+        def firstName = "John"
+        def lastName = "Doe"
+        def sex = "male"
+        def birthDateTime = ZonedDateTime.now()
+        def birthOrder = 1
+        def race = "Asian"
+        def nextOfKinFirstName = "Jaina"
+        def nextOfKinLastName = "Solo"
+        def nextOfKinPhoneNumber = "555-555-5555"
+        def nextOfKin = new NextOfKin(nextOfKinFirstName, nextOfKinLastName, nextOfKinPhoneNumber)
         def demographics = new PatientDemographics(
-                'fhir123id',
-                'patient123id',
-                'John',
-                'Doe',
-                'M',
-                null,
-                null,
-                null,
-                null
+                fhirResourceId,
+                patientId,
+                firstName,
+                lastName,
+                sex,
+                birthDateTime,
+                birthOrder,
+                race,
+                nextOfKin
                 )
 
         when:
-        def labOrder = HapiLabOrderConverter.getInstance().convertToOrder(demographics).getUnderlyingOrder()
+        def labOrderBundle = HapiLabOrderConverter.getInstance().convertToOrder(demographics).getUnderlyingOrder()
 
         then:
-        labOrder.getId() == demographics.getFhirResourceId()
+        def patient = labOrderBundle.getEntry().get(0).getResource() as Patient
+
+        patient.getId() == fhirResourceId
+        patient.getIdentifierFirstRep().getValue() == patientId
+        patient.getNameFirstRep().getFamily() == lastName
+        patient.getNameFirstRep().getGiven().get(0).getValue() == firstName
+        patient.getGender().toCode() == sex
+        patient.getBirthDateElement().getExtensionByUrl("http://hl7.org/fhir/StructureDefinition/patient-birthTime").getValue().toString() == new DateTimeType(Date.from(birthDateTime.toInstant())).toString()
+        patient.getMultipleBirthIntegerType().getValue() == birthOrder
+        patient.getExtensionByUrl("http://hl7.org/fhir/us/core/StructureDefinition/us-core-race").getExtensionByUrl("text").getValue().toString() == race
+        patient.getContactFirstRep().getName().getFamily() == nextOfKinLastName
+        patient.getContactFirstRep().getName().getGiven().get(0).getValue() == nextOfKinFirstName
+        patient.getContactFirstRep().getTelecomFirstRep().getValue() == nextOfKinPhoneNumber
     }
 }
