5421c640071daeea27441d670260ac7da604a316
halprin <halprin@users.noreply.github.com>
2023-02-16 16:04:07 -0600

    79: Convert the demographics to a new Patient

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderConverter.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderConverter.java
@@ -3,15 +3,21 @@ package gov.hhs.cdc.trustedintermediary.external.hapi;
 import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrder;
 import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrderConverter;
 import gov.hhs.cdc.trustedintermediary.etor.demographics.PatientDemographics;
+import java.util.Date;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.ContactPoint;
+import org.hl7.fhir.r4.model.DateTimeType;
+import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.Enumerations;
+import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.IntegerType;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.StringType;
 
 /**
  * Converts {@link PatientDemographics} to a Hapi-specific FHIR lab order ({@link HapiLabOrder} or
@@ -29,7 +35,7 @@ public class HapiLabOrderConverter implements LabOrderConverter {
     @Override
     public LabOrder<Bundle> convertToOrder(final PatientDemographics demographics) {
         var labOrder = new Bundle();
-        labOrder.setId(demographics.getFhirResourceId());
+
         labOrder.addEntry(
                 new Bundle.BundleEntryComponent().setResource(createPatientResource(demographics)));
 
@@ -58,17 +64,42 @@ public class HapiLabOrderConverter implements LabOrderConverter {
 
         patient.setGender(Enumerations.AdministrativeGender.fromCode(demographics.getSex()));
 
-        // birth date
-        //        patient.setBirthDateElement(new
-        // DateType().addExtension("http://hl7.org/fhir/StructureDefinition/patient-birthTime", new
-        // DateTimeType(demographics.getBirthDateTime().to)))
+        // birth date and time
+        var birthDate = new DateType(Date.from(demographics.getBirthDateTime().toInstant()));
+        birthDate.addExtension(
+                "http://hl7.org/fhir/StructureDefinition/patient-birthTime",
+                new DateTimeType(Date.from(demographics.getBirthDateTime().toInstant())));
+        patient.setBirthDateElement(birthDate);
 
         patient.setMultipleBirth(new IntegerType(demographics.getBirthOrder()));
 
         // race
-        //        patient.
+        var raceExtension =
+                new Extension()
+                        .setUrl("http://hl7.org/fhir/us/core/StructureDefinition/us-core-race");
+        raceExtension.addExtension("text", new StringType(demographics.getRace()));
+        patient.addExtension(raceExtension);
 
-        // next of kind
+        // next of kin
+        var nextOfKinRelationship =
+                new CodeableConcept(
+                        new Coding(
+                                "http://terminology.hl7.org/CodeSystem/v2-0131",
+                                "N",
+                                "Next of kin"));
+        var nextOfKinName =
+                new HumanName()
+                        .setFamily(demographics.getNextOfKin().getLastName())
+                        .addGiven(demographics.getNextOfKin().getFirstName());
+        var nextOfKinTelecom =
+                new ContactPoint()
+                        .setSystem(ContactPoint.ContactPointSystem.PHONE)
+                        .setValue(demographics.getNextOfKin().getPhoneNumber());
+        patient.addContact(
+                new Patient.ContactComponent()
+                        .addRelationship(nextOfKinRelationship)
+                        .setName(nextOfKinName)
+                        .addTelecom(nextOfKinTelecom));
 
         return patient;
     }
