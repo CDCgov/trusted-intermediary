e7062f48ddea06175d03eabcab747ca879a777eb
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-02-17 11:11:04 -0500

    task/79: WIP send order to local file

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/external/localfile/LocalFileLabOrderSender.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/external/localfile/LocalFileLabOrderSender.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.external.localfile;
 
+import ca.uhn.fhir.context.FhirContext;
 import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrder;
 import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrderSender;
 
@@ -15,6 +16,26 @@ public class LocalFileLabOrderSender implements LabOrderSender {
 
     @Override
     public void sendOrder(final LabOrder<?> order) {
+
+        //        // Create a FHIR context
+        FhirContext ctx = FhirContext.forR4();
+        //
+        //        // Create a Patient resource to serialize
+        //        Patient patient = new Patient();
+        //        patient.addName().setFamily("Simpson").addGiven("James");
+        //
+        //        // Instantiate a new JSON parser
+        //        IParser parser = ctx.newJsonParser();
+        //
+        //        // Serialize it
+        //        String serialized = parser.encodeResourceToString(patient);
+        //        System.out.println(serialized);
+        System.out.println("THIS IS THE STRING: " + order.getUnderlyingOrder().toString());
+        //        String fileName = "/xyz/test.txt";
+        //        String messageToWrite = "My long string";
+        //        Files.writeString(Paths.get(fileName), messageToWrite,
+        // StandardCharsets.ISO_8859_1);
+
         // convert the order to JSON?
         // save JSON? to a local file
     }
