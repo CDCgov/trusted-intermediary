7f9e2519052b16675fc76fdb7d0296438e737e2a
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-02-20 15:41:41 -0500

    task/79: changed test setup
    - moved some actions from test setup

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/localfile/LocalFileLabOrderSenderTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/localfile/LocalFileLabOrderSenderTest.groovy
@@ -2,6 +2,7 @@ package gov.hhs.cdc.trustedintermediary.external.localfile
 
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrder
+import gov.hhs.cdc.trustedintermediary.etor.demographics.LabOrderSender
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirImplementation
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
 import org.hl7.fhir.r4.model.Bundle
@@ -12,14 +13,15 @@ class LocalFileLabOrderSenderTest extends Specification{
     def setup() {
         TestApplicationContext.reset()
         TestApplicationContext.init()
-        TestApplicationContext.register(HapiFhir, HapiFhirImplementation.getInstance())
+        // TestApplicationContext.register(HapiFhir, HapiFhirImplementation.getInstance())
         TestApplicationContext.register(LocalFileLabOrderSender, LocalFileLabOrderSender.getInstance())
-        TestApplicationContext.injectRegisteredImplementations()
+        // TestApplicationContext.injectRegisteredImplementations()
     }
 
     def "send order works"() {
 
         given:
+        def fhir = Mock(HapiFhir)
         LabOrder<?> mockOrder = new LabOrder<Bundle>() {
 
                     @Override
@@ -28,8 +30,10 @@ class LocalFileLabOrderSenderTest extends Specification{
                     }
                 }
 
-        when:
+        TestApplicationContext.register(HapiFhir, HapiFhirImplementation.getInstance())
+        TestApplicationContext.injectRegisteredImplementations()
 
+        when:
         LocalFileLabOrderSender.getInstance().sendOrder(mockOrder)
 
         then:
