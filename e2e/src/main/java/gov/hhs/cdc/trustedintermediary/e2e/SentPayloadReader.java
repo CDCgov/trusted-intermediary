ef805575ba1ce875e613148bfa48e89f84a3fd8e
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-03-03 14:13:37 -0500

    clean up

--- e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/SentPayloadReader.java
+++ e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/SentPayloadReader.java
@@ -5,10 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class SentPayloadReader {
-    /*
-    1. Enhance findFilePayload to find the file if it is somewhere else.
-    2. Add tests for the sent payload
-     */
 
     public static String read() throws IOException {
         Path payloadFile = findFilePayload();
@@ -19,16 +15,13 @@ public class SentPayloadReader {
     public static Path findFilePayload() {
 
         Path expectedFilePath = Path.of("..", "app", "localfilelaborder.json");
-        // TODO
-        // ways to check if a file exists, and return that path
-        Boolean doesFileExist = Files.exists(expectedFilePath);
+
+        boolean doesFileExist = Files.exists(expectedFilePath);
 
         if (!doesFileExist) {
             expectedFilePath = Path.of("..", "localfilelaborder.json");
             doesFileExist = Files.exists(expectedFilePath);
         }
-
-        // throw an exception if not found (done by line 16)
 
         return expectedFilePath;
     }
