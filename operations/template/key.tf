b48b30941417ba87859b24b0287bcdcce618b01a
halprin <halprin@users.noreply.github.com>
2023-03-03 14:58:58 -0600

    79: Terraform format

--- operations/template/key.tf
+++ operations/template/key.tf
@@ -12,8 +12,8 @@ resource "azurerm_key_vault" "key_storage" {
 
 resource "azurerm_key_vault_access_policy" "allow_github_deployer" {
   key_vault_id = azurerm_key_vault.key_storage.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = "d59c2c86-de5e-41b7-a752-0869a73f5a60"  //github app registration
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "d59c2c86-de5e-41b7-a752-0869a73f5a60" //github app registration
 
   secret_permissions = [
     "Set",
@@ -25,8 +25,8 @@ resource "azurerm_key_vault_access_policy" "allow_github_deployer" {
 
 resource "azurerm_key_vault_access_policy" "allow_api_read" {
   key_vault_id = azurerm_key_vault.key_storage.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = azurerm_linux_web_app.api.identity.0.principal_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_linux_web_app.api.identity.0.principal_id
 
   secret_permissions = [
     "List",
