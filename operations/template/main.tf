ed5a83531d24cbb7df7f339de7aeeea062a7c2d0
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-03-14 12:51:05 -0400

    Added ENV value to terraform

--- operations/template/main.tf
+++ operations/template/main.tf
@@ -35,6 +35,7 @@ resource "azurerm_linux_web_app" "api" {
     DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.registry.login_server}"
     DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.registry.admin_username
     DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.registry.admin_password
+    ENV = "local"
   }
 
   identity {
