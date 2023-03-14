bf9bd251faafc1e5bed30aa9efab8f2fff8a46ed
halprin <halprin@users.noreply.github.com>
2023-03-14 16:31:41 -0500

    79: Add JavaDocs

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/YamlCombiner.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/YamlCombiner.java
@@ -2,6 +2,7 @@ package gov.hhs.cdc.trustedintermediary.wrappers;
 
 import java.util.Set;
 
+/** Combines YAML strings into one monolithic YAML string. */
 public interface YamlCombiner {
     String combineYaml(Set<String> yamlStrings) throws YamlCombinerException;
 }
