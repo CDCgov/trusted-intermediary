fee66ad2d0a5d96dd33d2629fc19751731c8239a
halprin <halprin@users.noreply.github.com>
2023-03-29 11:23:35 -0500

    84: Delete the FHIR path support from our use of HapiFhir

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirImplementation.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirImplementation.java
@@ -1,11 +1,8 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.fhirpath.IFhirPath;
 import ca.uhn.fhir.parser.IParser;
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir;
-import java.util.Optional;
-import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 /** Concrete implementation that calls the Hapi FHIR library. */
@@ -14,18 +11,11 @@ public class HapiFhirImplementation implements HapiFhir {
     private static final HapiFhirImplementation INSTANCE = new HapiFhirImplementation();
 
     private static final FhirContext CONTEXT = FhirContext.forR4();
-    private static final IFhirPath PATHER = CONTEXT.newFhirPath();
 
     private HapiFhirImplementation() {}
 
     public static HapiFhirImplementation getInstance() {
         return INSTANCE;
-    }
-
-    @Override
-    public <T extends IBase> Optional<T> fhirPathEvaluateFirst(
-            final IBase fhirResource, final String fhirPath, final Class<T> clazz) {
-        return PATHER.evaluateFirst(fhirResource, fhirPath, clazz);
     }
 
     @Override
