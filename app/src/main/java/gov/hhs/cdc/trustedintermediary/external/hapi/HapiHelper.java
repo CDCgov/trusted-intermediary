5bb42a5929a99d7968081840a05c75898534e91e
halprin <halprin@users.noreply.github.com>
2023-03-30 08:55:37 -0500

    84: Add unit tests and JavaDocs for the HapiHelper

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiHelper.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiHelper.java
@@ -4,9 +4,20 @@ import java.util.stream.Stream;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Resource;
 
-public class HapiHelper {
+/** Helper class that works on HapiFHIR constructs. */
+class HapiHelper { // not a public class so it is package-private (meaning only other classes in
+    // this package can access it
     private HapiHelper() {}
 
+    /**
+     * Returns a {@link Stream} of FHIR resources inside the provided {@link Bundle} that match the
+     * given resource type.
+     *
+     * @param bundle The bundle to search.
+     * @param resourceType The class of the resource to search for.
+     * @param <T> The type that either is or extends {@link Resource}.
+     * @return The stream of the given resource type.
+     */
     public static <T extends Resource> Stream<T> resourcesInBundle(
             Bundle bundle, Class<T> resourceType) {
         return bundle.getEntry().stream()
---
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiHelperTest.groovy
@@ -0,0 +1,40 @@
+package gov.hhs.cdc.trustedintermediary.external.hapi
+
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Patient
+import org.hl7.fhir.r4.model.Provenance
+import org.hl7.fhir.r4.model.ResourceType
+import org.hl7.fhir.r4.model.ServiceRequest
+import spock.lang.Specification
+
+class HapiHelperTest extends Specification {
+    def "resourcesInBundle return a stream of a specific type of resources in a FHIR Bundle"() {
+        given:
+        def patients = [
+            new Patient(),
+            new Patient(),
+            new Patient(),
+            new Patient(),
+            new Patient(),
+            new Patient()
+        ]
+
+        def bundle = new Bundle()
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(0)))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(1)))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(new ServiceRequest()))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(2)))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(3)))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(new Provenance()))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(4)))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(new ServiceRequest()))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(new ServiceRequest()))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(patients.get(5)))
+
+        when:
+        def patientStream = HapiHelper.resourcesInBundle(bundle, Patient)
+
+        then:
+        patientStream.allMatch {patients.contains(it) && it.getResourceType() == ResourceType.Patient }
+    }
+}
