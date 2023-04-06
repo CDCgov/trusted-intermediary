625318dadfb9d544acda8608dff5d010fee43c09
halprin <halprin@users.noreply.github.com>
2023-04-06 10:09:28 -0500

    84: JavaDocs for the TokenGenerationException

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/TokenGenerationException.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/TokenGenerationException.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.wrappers;
 
+/** Thrown when the {@link AuthEngine} cannot create a token. */
 public class TokenGenerationException extends Exception {
     public TokenGenerationException(String errorMessage, Throwable e) {
         super(errorMessage, e);
