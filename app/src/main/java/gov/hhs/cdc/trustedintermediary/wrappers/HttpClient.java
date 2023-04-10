f1df31935894f73471b73f9ba8f4536d0ed037ed
Jorge Lopez <49923512+jorg3lopez@users.noreply.github.com>
2023-04-10 10:22:54 -0700

    Better Exception Handling (HttpClient) (#267)
    * skeleton for
    Secrets interface
    AzureSecrets
    LocalSecrets
    AzureSecretsTest
    LocalSecretsTest
    
    * refatoring: LocalSecretsTest
    
    * Added azure key-vault dependency and LocalSecrets getKey works retrieving local key
    
    * Addons:
    KeyVault interface
    AzureKeyVault Impl.
    registered KeyVault in Application context
    
    * AzureSecretsTest works
    
    * deleted key vault interface
    
    * register Secrets.class
    
    * deleted comment
    
    * Unit test:
    deleted AzureSecrets unit test
    LocalSecrets unit test passes
    
    * Exclude AzureSecrets from Jacoco
    
    * deleted registration of logger in unit test
    
    * readprivatekey-works unit test passes
    
    * RequestToken unit test passes
    
    * requested changes
    
    * adding minidev:json-smart to fix azure-identity vulnerability
    
    * deleting minidev
    
    * refactor comments
    
    * refactoring unit test
    
    * http exception hadling, no unit tests
    
    * added unit test
    
    * all unit tests work
    
    * unit test
    
    * javadocs
    
    * refactor unit test

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/HttpClient.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/HttpClient.java
@@ -1,9 +1,8 @@
 package gov.hhs.cdc.trustedintermediary.wrappers;
 
-import java.io.IOException;
 import java.util.Map;
 
 /** This interface provides a generic blueprint for HTTP operations */
 public interface HttpClient {
-    String post(String path, Map<String, String> headerMap, String body) throws IOException;
+    String post(String path, Map<String, String> headerMap, String body) throws HttpClientException;
 }
--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/apache/ApacheClientTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/apache/ApacheClientTest.groovy
@@ -1,5 +1,8 @@
 package gov.hhs.cdc.trustedintermediary.external.apache
 
+import gov.hhs.cdc.trustedintermediary.context.ApplicationContext
+import gov.hhs.cdc.trustedintermediary.wrappers.HttpClient
+import gov.hhs.cdc.trustedintermediary.wrappers.HttpClientException
 import org.apache.hc.core5.http.message.BasicHeader
 import spock.lang.Specification
 
@@ -23,5 +26,16 @@ class ApacheClientTest extends Specification {
 
         then:
         actual.toString() == bhArr.toString()
+    }
+
+    def "Http request with error"() {
+        given:
+        def httpClient = ApacheClient.getInstance()
+
+        when:
+        def res = httpClient.post("https://fake-uri.com", null, "fake body")
+
+        then:
+        thrown(HttpClientException)
     }
 }
