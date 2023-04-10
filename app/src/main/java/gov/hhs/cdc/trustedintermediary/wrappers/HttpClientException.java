b59581bc3be1d8058ea8f02777304b8d1024401f
jorge Lopez <jlopez@flexion.us>
2023-04-10 09:23:09 -0700

    javadocs

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/HttpClientException.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/HttpClientException.java
@@ -2,6 +2,7 @@ package gov.hhs.cdc.trustedintermediary.wrappers;
 
 import java.io.IOException;
 
+/** Custom exception class use to catch any exception coming from an HTTP request */
 public class HttpClientException extends IOException {
 
     public HttpClientException(String message, Throwable cause) {
