8e274340bef49c17fa740575a457bf9fbd4a6e77
jorge Lopez <jlopez@flexion.us>
2023-04-12 14:54:07 -0700

    added unit test

---
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/demographics/UnabletoSendLabOrderExceptionTest.groovy
@@ -0,0 +1,20 @@
+package gov.hhs.cdc.trustedintermediary.etor.demographics
+
+import gov.hhs.cdc.trustedintermediary.wrappers.HttpClientException
+import spock.lang.Specification
+
+class UnabletoSendLabOrderExceptionTest extends Specification {
+    def "contructor works"() {
+
+        given:
+        def message = "something blew up!"
+        def cause = new HttpClientException(message, new IOException())
+
+        when:
+        def exception = new UnableToSendLabOrderException(message, cause)
+
+        then:
+        exception.getMessage() == message
+        exception.getCause() == cause
+    }
+}
