92f17ee1f706ee03e58aec334804fbef8be83de2
jorge Lopez <jlopez@flexion.us>
2023-04-21 13:19:08 -0700

    increase thread count

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamLabOrderSenderTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamLabOrderSenderTest.groovy
@@ -202,7 +202,7 @@ class ReportStreamLabOrderSenderTest extends Specification {
     def "setRsTokenCache synchronization works"() {
         given:
         def rsLabOrderSender = ReportStreamLabOrderSender.getInstance()
-        def threadCount = 10
+        def threadCount = 100
         def expected = "lock is working"
         def actual = "lock is not working"
 
