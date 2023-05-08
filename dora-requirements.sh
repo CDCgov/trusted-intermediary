481bc1c862704411cedadb103081733f45ee307e
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-05-08 13:35:55 -0700

    Added devops-deployment-metrics repo setup

--- dora-requirements.sh
+++ dora-requirements.sh
@@ -9,5 +9,15 @@ sudo apt install python3.9
 # Install package manager (poetry)
 curl -sSL https://install.python-poetry.org | python3 -
 
+# Clone repo
+git clone https://github.com/flexion/devops-deployment-metrics.git
+cd devops-deployment-metrics
+
 # Install project dependencies
 poetry install
+
+# Create toml configuration file with custom values
+sed -e '14,18d' -e 's/my_github_owner/CDCgov/' -e 's/my_github_repository/trusted-intermediary/' -e 's/12345678/42635194/' sample-config.toml > config.toml
+
+# Run devops-deployment-metrics
+poetry run devops-deployment-metrics -c config.toml
