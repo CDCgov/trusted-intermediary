1d2f21a849d54e3feb2a7fb4c8189d9b7006d592
jorge Lopez <jlopez@flexion.us>
2023-05-22 08:50:09 -0700

    synchronization test

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/keyCacheTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/keyCacheTest.groovy
@@ -15,4 +15,40 @@ class keyCacheTest extends Specification {
         then:
         actual == expected
     }
+
+    def "keyCache synchronization works"() {
+        given:
+        def cache = KeyCache.getInstance()
+        def threadsNum = 5
+        def iterations = 25
+
+        when:
+        List<Thread> threads = []
+        (1..threadsNum).each { threadId ->
+            threads.add(new Thread({
+                for (int i = 0; i < iterations; i++) {
+                    cache.put("Thread-" + threadId, "${i}")
+                }
+            }))
+        }
+
+        threads*.start()
+        threads*.join()
+        def keysNum = cache.getProperties().get("keys").collect().toArray().size()
+        def keys = cache.getProperties().get("keys").collect().collectEntries()
+        println("keys: " + keys)
+        println(keysNum)
+        println(cache.getProperties())
+
+        then:
+        keysNum == threadsNum
+        keys.get("Thread-1") == "24"
+        keys.get("Thread-2") == "24"
+        keys.get("Thread-3") == "24"
+        keys.get("Thread-4") == "24"
+        keys.get("Thread-5") == "24"
+        keys.size() == 5 // one entry per thread
+        keys.values().toSet().size() == 1 // all entries have same value, threads had to wait on the lock
+
+    }
 }
