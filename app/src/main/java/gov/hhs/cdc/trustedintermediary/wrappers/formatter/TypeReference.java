6393a2da780014390570c2adbdb9e31d40c5359a
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-05-25 14:12:41 -0700

    Refactored to make TypeReference an interface to avoid code smell

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/formatter/TypeReference.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/formatter/TypeReference.java
@@ -3,8 +3,9 @@ package gov.hhs.cdc.trustedintermediary.wrappers.formatter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-public abstract class TypeReference<T> {
-    public Type getType() {
-        return ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+public interface TypeReference<T> {
+    default Type getType() {
+        ParameterizedType type = (ParameterizedType) this.getClass().getGenericInterfaces()[0];
+        return type.getActualTypeArguments()[0];
     }
 }
