681443734ff4ac9ca64e9b3b493d57ad72f8b0f6
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-05-25 14:36:08 -0700

    Refactored tests to simplify and follow conventions

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/organizations/OrganizationsSettingsTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/organizations/OrganizationsSettingsTest.groovy
@@ -11,12 +11,10 @@ import java.nio.file.Path
 class OrganizationsSettingsTest extends Specification {
 
     Path tempFile
-    OrganizationsSettings settings
 
     def setup() {
         TestApplicationContext.reset()
         TestApplicationContext.init()
-        TestApplicationContext.register(Organization, Organization)
         TestApplicationContext.register(OrganizationsSettings, OrganizationsSettings.getInstance())
         TestApplicationContext.register(Formatter, Jackson.getInstance())
         TestApplicationContext.injectRegisteredImplementations()
@@ -24,7 +22,6 @@ class OrganizationsSettingsTest extends Specification {
         String yamlContent = "- name: test-name\n  description: Test Description"
         tempFile = Files.createTempFile("organizations", ".yaml")
         Files.writeString(tempFile, yamlContent)
-        settings = OrganizationsSettings.getInstance()
     }
 
     def cleanup() {
@@ -32,42 +29,37 @@ class OrganizationsSettingsTest extends Specification {
     }
 
     def "Load organizations from yaml file works"() {
-        given:
-        def expectedOrganizationsSize = 1
-
         when:
-        settings.loadOrganizations(tempFile)
-        def organizations = settings.getOrganizations()
+        OrganizationsSettings.getInstance().loadOrganizations(tempFile)
+        def organizations = OrganizationsSettings.getInstance().getOrganizations()
         def actualOrganizationsSize = organizations.size()
 
         then:
-        actualOrganizationsSize == expectedOrganizationsSize
+        actualOrganizationsSize == 1
     }
 
     def "Properties for loaded organizations match expected values"() {
         given:
-        settings.loadOrganizations(tempFile)
-        def expectedOrganizationName = "test-name"
-        def expectedOrganizationDescription = "Test Description"
+        OrganizationsSettings.getInstance().loadOrganizations(tempFile)
 
         when:
-        def organizations = settings.getOrganizations()
+        def organizations = OrganizationsSettings.getInstance().getOrganizations()
         Organization organization = organizations[0]
         def actualOrganizationName = organization.getName()
         def actualOrganizationDescription = organization.getDescription()
 
         then:
-        actualOrganizationName == expectedOrganizationName
-        actualOrganizationDescription == expectedOrganizationDescription
+        actualOrganizationName == "test-name"
+        actualOrganizationDescription == "Test Description"
     }
 
     def "findOrganization returns correct organization by name when exists"() {
         given:
-        settings.loadOrganizations(tempFile)
+        OrganizationsSettings.getInstance().loadOrganizations(tempFile)
         def expectedOrganizationName = "test-name"
 
         when:
-        def organization = settings.findOrganization(expectedOrganizationName)
+        def organization = OrganizationsSettings.getInstance().findOrganization(expectedOrganizationName)
         def actualOrganizationName = organization.get().getName()
 
         then:
@@ -77,11 +69,10 @@ class OrganizationsSettingsTest extends Specification {
 
     def "findOrganization returns empty optional when organization does not exist"() {
         given:
-        settings.loadOrganizations(tempFile)
-        def expectedOrganizationName = "non-existent-name"
+        OrganizationsSettings.getInstance().loadOrganizations(tempFile)
 
         when:
-        def organization = settings.findOrganization(expectedOrganizationName)
+        def organization = OrganizationsSettings.getInstance().findOrganization("non-existent-name")
 
         then:
         !organization.isPresent()
