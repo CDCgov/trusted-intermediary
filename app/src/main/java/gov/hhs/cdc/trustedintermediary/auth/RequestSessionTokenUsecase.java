e3ca3c30c9a5f162149da0d3928f025446f2d80a
halprin <halprin@users.noreply.github.com>
2023-05-29 10:36:02 -0500

    148: get the private key

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/auth/RequestSessionTokenUsecase.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/auth/RequestSessionTokenUsecase.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.auth;
 
+import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
 import gov.hhs.cdc.trustedintermediary.organizations.OrganizationsSettings;
 import gov.hhs.cdc.trustedintermediary.wrappers.AuthEngine;
 import gov.hhs.cdc.trustedintermediary.wrappers.Cache;
@@ -7,10 +8,6 @@ import gov.hhs.cdc.trustedintermediary.wrappers.InvalidTokenException;
 import gov.hhs.cdc.trustedintermediary.wrappers.SecretRetrievalException;
 import gov.hhs.cdc.trustedintermediary.wrappers.Secrets;
 import gov.hhs.cdc.trustedintermediary.wrappers.TokenGenerationException;
-import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter;
-import gov.hhs.cdc.trustedintermediary.wrappers.formatter.FormatterProcessingException;
-import gov.hhs.cdc.trustedintermediary.wrappers.formatter.TypeReference;
-import java.util.Map;
 import javax.inject.Inject;
 
 /** TODO */
@@ -23,7 +20,6 @@ public class RequestSessionTokenUsecase {
     private static final int TOKEN_TTL = 300;
 
     @Inject private AuthEngine auth;
-    @Inject private Formatter formatter;
     @Inject private Secrets secrets;
     @Inject private OrganizationsSettings organizationsSettings;
     @Inject private Cache cache;
@@ -40,15 +36,12 @@ public class RequestSessionTokenUsecase {
 
         var organizationName = request.scope();
 
-        var organization =
-                organizationsSettings
-                        .findOrganization(request.scope())
-                        .orElseThrow(
-                                () ->
-                                        new UnknownOrganizationException(
-                                                "The organization "
-                                                        + organizationName
-                                                        + " is unknown"));
+        organizationsSettings
+                .findOrganization(request.scope())
+                .orElseThrow(
+                        () ->
+                                new UnknownOrganizationException(
+                                        "The organization " + organizationName + " is unknown"));
 
         // At this point, only organizations registered with us will proceed
 
@@ -57,11 +50,10 @@ public class RequestSessionTokenUsecase {
         auth.validateToken(request.jwt(), organizationPublicKey);
 
         // Provide a short-lived access token for subsequent calls to the TI service
-        return auth.generateToken(
-                OUR_NAME, OUR_NAME, RS_NAME, RS_NAME, TOKEN_TTL, retrieveOrganizationPublicKey());
+        var tiPrivateKey = retrieveTiPrivateKey();
+        return auth.generateToken(OUR_NAME, OUR_NAME, RS_NAME, RS_NAME, TOKEN_TTL, tiPrivateKey);
     }
 
-    /** TODO: Consolidate; copied from ReportStreamLabOrderSender */
     protected String retrieveOrganizationPublicKey(String organizationName)
             throws SecretRetrievalException {
         var organizationPublicKeyName = "organization-" + organizationName + "-public-key";
@@ -78,10 +70,16 @@ public class RequestSessionTokenUsecase {
     }
 
     /** TODO: Consolidate; copied from ReportStreamLabOrderSender */
-    protected String extractToken(String responseBody) throws FormatterProcessingException {
-        var value =
-                formatter.convertJsonToObject(
-                        responseBody, new TypeReference<Map<String, String>>() {});
-        return value.get("access_token");
+    protected String retrieveTiPrivateKey() throws SecretRetrievalException {
+        var senderPrivateKey =
+                "report-stream-sender-private-key-" + ApplicationContext.getEnvironment();
+        String key = cache.get(senderPrivateKey);
+        if (key != null) {
+            return key;
+        }
+
+        key = secrets.getKey(senderPrivateKey);
+        cache.put(senderPrivateKey, key);
+        return key;
     }
 }
