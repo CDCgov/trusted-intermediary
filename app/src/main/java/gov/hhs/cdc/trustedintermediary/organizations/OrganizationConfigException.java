68323094fd9f6eda879c76cd4110da024f922e19
halprin <halprin@users.noreply.github.com>
2023-05-30 09:36:42 -0500

    148: JavaDocs for the OrganizationConfigException

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/organizations/OrganizationConfigException.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/organizations/OrganizationConfigException.java
@@ -1,6 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.organizations;
 
-/** TODO */
+/** Exception thrown when the organizations settings configuration cannot be loaded. */
 public class OrganizationConfigException extends Exception {
     public OrganizationConfigException(String message, Throwable cause) {
         super(message, cause);
