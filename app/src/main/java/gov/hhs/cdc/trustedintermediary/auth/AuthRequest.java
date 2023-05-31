270f1a00d79b056930a27548f55bac6e0c94ff80
halprin <halprin@users.noreply.github.com>
2023-05-31 08:50:58 -0500

    JavaDoc for AuthRequest

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/auth/AuthRequest.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/auth/AuthRequest.java
@@ -1,3 +1,9 @@
 package gov.hhs.cdc.trustedintermediary.auth;
 
+/**
+ * The POJO that is parsed out of an auth login request from a client.
+ *
+ * @param scope The scope field that holds the organizations name.
+ * @param jwt The JWT that comes out of the client_assertion field.
+ */
 public record AuthRequest(String scope, String jwt) {}
