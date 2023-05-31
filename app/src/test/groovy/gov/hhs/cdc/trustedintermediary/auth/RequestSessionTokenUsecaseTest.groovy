3131e6aaede86f7c0b701bd597a87dd2db64a1ea
JohnNKing <jking@flexion.us>
2023-05-31 16:34:41 -0400

    Adding not-yet-working happy path test for RequestSessionTokenUsecaseTest.getToken()

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/auth/RequestSessionTokenUsecaseTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/auth/RequestSessionTokenUsecaseTest.groovy
@@ -1,5 +1,13 @@
 package gov.hhs.cdc.trustedintermediary.auth
 
+import gov.hhs.cdc.trustedintermediary.auth.AuthRequest
+import gov.hhs.cdc.trustedintermediary.auth.RequestSessionTokenUsecase
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.organizations.Organization
+import gov.hhs.cdc.trustedintermediary.organizations.OrganizationsSettings
+import gov.hhs.cdc.trustedintermediary.wrappers.AuthEngine
+import gov.hhs.cdc.trustedintermediary.wrappers.Cache
+import gov.hhs.cdc.trustedintermediary.wrappers.Secrets
 import spock.lang.Specification
 
 class RequestSessionTokenUsecaseTest extends Specification {
@@ -17,6 +25,27 @@ class RequestSessionTokenUsecaseTest extends Specification {
     }
 
     def "happy path returns a session token"() {
+        given:
+        def authEngine = Mock(AuthEngine)
+        def secrets = Mock(Secrets)
+        def organizationsSettings = Mock(OrganizationsSettings)
+
+        TestApplicationContext.register(AuthEngine, authEngine)
+        TestApplicationContext.register(Secrets, secrets)
+        TestApplicationContext.register(OrganizationsSettings, organizationsSettings)
+        TestApplicationContext.injectRegisteredImplementations()
+
+        organizationsSettings.findOrganization(_ as String) >> { Mock(Organization) }
+        secrets.getKey(_ as String) >> "KEY"
+        // authEngine.validateToken(_ as AuthRequest, _ as String)
+        def expected = "SESSION TOKEN"
+        authEngine.generateToken(_ as String, _ as String, _ as String, _ as String, _ as int, _ as String) >> expected
+
+        when:
+        def actual = RequestSessionTokenUsecase.getToken(new AuthRequest("RS", "AUTH TOKEN"))
+
+        then:
+        actual == expected
     }
 
     def "organization is not found"() {
