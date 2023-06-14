b27ac9b9de48a879901ce537363e3d31f19612b9
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-06-14 14:21:52 -0700

    Added missing javadocs

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/DomainConnectorConstructionException.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/DomainConnectorConstructionException.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.domainconnector;
 
+/** Thrown when a domain connector fails to be constructed. */
 public class DomainConnectorConstructionException extends Exception {
     public DomainConnectorConstructionException(String message, Throwable cause) {
         super(message, cause);
--- app/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/UnableToReadOpenApiSpecificationException.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/UnableToReadOpenApiSpecificationException.java
@@ -1,5 +1,6 @@
 package gov.hhs.cdc.trustedintermediary.domainconnector;
 
+/** Thrown when an OpenAPI specification cannot be read. */
 public class UnableToReadOpenApiSpecificationException extends Exception {
     public UnableToReadOpenApiSpecificationException(String message, Throwable cause) {
         super(message, cause);
