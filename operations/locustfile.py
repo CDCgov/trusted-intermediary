34a1ccae3f175cd038aa62663ea560774444f0d6
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-06-16 09:32:51 -0700

    Update locust demographics endpoint test to authenticate with valid token (#387)
    * skeleton
    
    * DemographicsRequestValidator
    
    * isValidAccessToken rename
    
    * add validator to endpoint
    
    * check expiration date of jwt
    
    * used claimsJws instead of calling getExpirationDate
    
    * renamed class and moved to auth package
    
    * no need to extract the expiration date
    
    * readPublicKey and parseBase64
    
    * try catch in isValidToken
    
    * deleted try catch from EtorDomainRegistration
    
    * 148: Make the Auth validator a propper singleton
    
    * Extract and validate the token for the demographics endpoint
    
    * 148: Refactor the controller so it also supports generic error strings, not just from an exception
    
    * If the auth header is null, convert it to the empty string
    
    * 148: get the EtorDomainRegistration unit tests working again
    
    * 148: unit test that we return a 401 from the demographics endpoint when it is aunathenticated
    
    * 148: unit test the demographics endpoint  when the auth check completely fails
    
    * 148: Reformat the e2e AuthTest a bit
    
    * Expand the e2e client to support headers
    
    * 148: Add an AuthClient that does some nice things in our e2e tests
    
    * 148: Add the DemographicsClient to the e2e tests that call the login first
    
    * 148: Remove unecesary readAllBytes and then String construction when you can get a UTF-8 String directly
    
    * 148: Rename our private key to trusted-intermediary-private-key in the AuthRequestValidator
    
    * 148: Transition to Files.readString instead of readAllBytes
    
    * refactored isPrivateKey
    
    * AuthRequestValidatorTest plus unhappy path for tokenHascontent
    
    * AuthRequestValidatorTest plus unhappy path for tokenHascontent
    
    * tokenHasContent happy path
    
    * 148: Make the isPrivateKey test in the JjwtEngine more easily see when something failed
    
    * 148: Revert a too big of a rename
    
    * extractToken happy path works
    
    * extracToken unhappy path works
    
    * retrievePrivateKey works when keyCache not empty
    
    * retrievePrivateKey works when keyCache is empty
    
    * retrievePrivateKey adds key to keyCache works
    
    * add spacing to tests
    
    * tokenHasContent unhappy null path works
    
    * 148: isPrivateKey does not need to be public
    
    * isValidAuthenticatedRequest happy path works
    
    * unit test: isValidAuthenticatedRequest unhappy empty path works
    
    * unit test: isValidAuthenticatedRequest unhappy null path works
    
    * unit test: isValidAuthenticatedRequest unhappy invalidTokenException path works
    
    * javadocs for AuthRequestValidator
    
    * 148: Spruce up JjwtEngine with a dedicated method to read any kind of key and then unit test it
    
    * 148: Single class imports in JjwtEngine
    
    * corrected typos in javadoc
    
    * injected logger to class
    
    * added logging
    
    * refactored unit tests
    
    * Changed demographics test implementation to authenticate with a valid token. Also, extracted endpoint variables and some formatting
    
    ---------
    
    Co-authored-by: jorge Lopez <jlopez@flexion.us>
    Co-authored-by: halprin <halprin@users.noreply.github.com>
    Co-authored-by: Jorge Lopez <49923512+jorg3lopez@users.noreply.github.com>

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -1,16 +1,26 @@
+import json
+import urllib.parse
+import urllib.request
+
 from locust import HttpUser, task, events
 from locust.runners import MasterRunner
-import urllib.parse
 
+
+HEALTH_ENDPOINT = "/health"
+AUTH_ENDPOINT = "/v1/auth"
+DEMOGRAPHICS_ENDPOINT = "/v1/etor/demographics"
 
 demographics_request_body = None
 auth_request_body = None
+access_token = None
 
 
 @events.test_start.add_listener
 def test_start(environment):
     global demographics_request_body
     global auth_request_body
+    global access_token
+
     if isinstance(environment.runner, MasterRunner):
         # in a distributed run, the master does not typically need any test data
         return
@@ -25,28 +35,39 @@ def test_start(environment):
     with open("mock_credentials/report-stream-valid-token.jwt") as f:
         auth_token = f.read()
 
-    auth_request_body = urllib.parse.urlencode({
-        'scope': auth_scope,
-        'client_assertion': auth_token
-    })
+    auth_request_body = urllib.parse.urlencode(
+        {"scope": auth_scope, "client_assertion": auth_token}
+    )
+
+    # get a valid bearer token from the auth endpoint
+    request = urllib.request.Request(
+        f"{environment.host}{AUTH_ENDPOINT}", data=auth_request_body.encode("utf-8")
+    )
+    response = urllib.request.urlopen(request)
+    data = json.load(response)
+    access_token = data["access_token"]
 
 
 class SampleUser(HttpUser):
-
     # Each task gets called randomly, but the number next to '@task' denotes
     # how many more times that task will get called than other tasks
 
     @task
     def get_health(self):
-        self.client.get("/health")
+        self.client.get(HEALTH_ENDPOINT)
 
     @task(5)  # this task will get called 5x more than the other
     def post_v1_etor_demographics(self):
-        self.client.post("/v1/etor/demographics", data=demographics_request_body)
+        self.client.post(
+            DEMOGRAPHICS_ENDPOINT,
+            data=demographics_request_body,
+            headers={"Authorization": access_token},
+        )
 
     @task
     def post_v1_auth(self):
-        self.client.post("/v1/auth", data=auth_request_body)
+        self.client.post(AUTH_ENDPOINT, data=auth_request_body)
+
 
 @events.quitting.add_listener
 def assert_stats(environment):
