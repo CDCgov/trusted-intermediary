236d92597c6eb94c7b08cf4c1e765129ad9a54cf
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-06-19 16:02:22 -0700

    Refresh bearer token before it expires in load tests (#390)
    * WIP: refactored to extract method and looking for a way to refresh bearer token before it expires
    
    * Refactored to refresh token per user +  added logging and some code cleanup
    
    * Removed last_authenticated
    
    * Use FastHttpUser in our load tests to get more performance from the worker
    
    * Removed usunsed import
    
    ---------
    
    Co-authored-by: halprin <halprin@users.noreply.github.com>

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -1,8 +1,10 @@
-import json
+import time
+import logging
+import threading
 import urllib.parse
 import urllib.request
 
-from locust import HttpUser, task, events
+from locust import FastHttpUser, task, events
 from locust.runners import MasterRunner
 
 
@@ -12,45 +14,33 @@ DEMOGRAPHICS_ENDPOINT = "/v1/etor/demographics"
 
 demographics_request_body = None
 auth_request_body = None
-access_token = None
 
 
-@events.test_start.add_listener
-def test_start(environment):
-    global demographics_request_body
-    global auth_request_body
-    global access_token
-
-    if isinstance(environment.runner, MasterRunner):
-        # in a distributed run, the master does not typically need any test data
-        return
-
-    # read the sample request body for the demographics endpoint
-    with open("e2e/src/test/resources/newborn_patient.json", "r") as f:
-        demographics_request_body = f.read()
-
-    # set up the sample request body for the auth endpoint
-    # using a valid test token found in the mock_credentials directory
-    auth_scope = "report-stream"
-    with open("mock_credentials/report-stream-valid-token.jwt") as f:
-        auth_token = f.read()
-
-    auth_request_body = urllib.parse.urlencode(
-        {"scope": auth_scope, "client_assertion": auth_token}
-    )
-
-    # get a valid bearer token from the auth endpoint
-    request = urllib.request.Request(
-        f"{environment.host}{AUTH_ENDPOINT}", data=auth_request_body.encode("utf-8")
-    )
-    response = urllib.request.urlopen(request)
-    data = json.load(response)
-    access_token = data["access_token"]
-
-
-class SampleUser(HttpUser):
+class SampleUser(FastHttpUser):
     # Each task gets called randomly, but the number next to '@task' denotes
     # how many more times that task will get called than other tasks
+
+    token_refresh_interval = 280
+    access_token = None
+
+    def on_start(self):
+        self.authenticate()
+
+        # Start the token refreshing thread
+        threading.Thread(
+            target=self.authenticate_periodically, args=(), daemon=True
+        ).start()
+
+    def authenticate_periodically(self):
+        while True:
+            time.sleep(self.token_refresh_interval)  # Refresh token every few seconds
+            self.authenticate()
+
+    def authenticate(self):
+        logging.debug("Authenticating...")
+        response = self.client.post(AUTH_ENDPOINT, data=auth_request_body)
+        data = response.json()
+        self.access_token = data["access_token"]
 
     @task
     def get_health(self):
@@ -61,19 +51,48 @@ class SampleUser(HttpUser):
         self.client.post(
             DEMOGRAPHICS_ENDPOINT,
             data=demographics_request_body,
-            headers={"Authorization": access_token},
+            headers={"Authorization": self.access_token},
         )
 
-    @task
-    def post_v1_auth(self):
-        self.client.post(AUTH_ENDPOINT, data=auth_request_body)
+
+@events.test_start.add_listener
+def test_start(environment):
+    global demographics_request_body
+    global auth_request_body
+
+    if isinstance(environment.runner, MasterRunner):
+        # in a distributed run, the master does not typically need any test data
+        return
+
+    demographics_request_body = get_demographics_request_body()
+    auth_request_body = get_auth_request_body()
 
 
 @events.quitting.add_listener
 def assert_stats(environment):
     if environment.stats.total.fail_ratio > 0.01:
-        print("Test failed due to failure ratio > 1%")
+        logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 1000:
-        print("Test failed due to 95th percentile response time > 1000 ms")
+        logging.error("Test failed due to 95th percentile response time > 1000 ms")
         environment.process_exit_code = 1
+    else:
+        logging.info("Test passed!")
+
+
+def get_auth_request_body():
+    # set up the sample request body for the auth endpoint
+    # using a valid test token found in the mock_credentials directory
+    auth_scope = "report-stream"
+    with open("mock_credentials/report-stream-valid-token.jwt") as f:
+        auth_token = f.read()
+    params = urllib.parse.urlencode(
+        {"scope": auth_scope, "client_assertion": auth_token}
+    )
+    return params.encode("utf-8")
+
+
+def get_demographics_request_body():
+    # read the sample request body for the demographics endpoint
+    with open("e2e/src/test/resources/newborn_patient.json", "r") as f:
+        return f.read()
