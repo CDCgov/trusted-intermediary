4441925dca358949aaed6d749deec4da8afabb77
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-06-19 14:08:17 -0700

    Refactored to refresh token per user +  added logging and some code cleanup

--- operations/locustfile.py
+++ operations/locustfile.py
@@ -1,5 +1,6 @@
-import json
 import time
+import logging
+import datetime
 import threading
 import urllib.parse
 import urllib.request
@@ -14,7 +15,72 @@ DEMOGRAPHICS_ENDPOINT = "/v1/etor/demographics"
 
 demographics_request_body = None
 auth_request_body = None
-access_token = None
+
+
+class SampleUser(HttpUser):
+    # Each task gets called randomly, but the number next to '@task' denotes
+    # how many more times that task will get called than other tasks
+
+    token_refresh_interval = 280
+    last_authenticated = None
+    access_token = None
+
+    def on_start(self):
+        self.authenticate()
+
+        # Start the token refreshing thread
+        threading.Thread(
+            target=self.authenticate_periodically, args=(), daemon=True
+        ).start()
+
+    def authenticate_periodically(self):
+        while True:
+            time.sleep(self.token_refresh_interval)  # Refresh token every few seconds
+            self.authenticate()
+
+    def authenticate(self):
+        logging.debug("Authenticating...")
+        response = self.client.post(AUTH_ENDPOINT, data=auth_request_body)
+        data = response.json()
+        self.last_authenticated = datetime.datetime.now()
+        self.access_token = data["access_token"]
+
+    @task
+    def get_health(self):
+        self.client.get(HEALTH_ENDPOINT)
+
+    @task(5)  # this task will get called 5x more than the other
+    def post_v1_etor_demographics(self):
+        self.client.post(
+            DEMOGRAPHICS_ENDPOINT,
+            data=demographics_request_body,
+            headers={"Authorization": self.access_token},
+        )
+
+
+@events.test_start.add_listener
+def test_start(environment):
+    global demographics_request_body
+    global auth_request_body
+
+    if isinstance(environment.runner, MasterRunner):
+        # in a distributed run, the master does not typically need any test data
+        return
+
+    demographics_request_body = get_demographics_request_body()
+    auth_request_body = get_auth_request_body()
+
+
+@events.quitting.add_listener
+def assert_stats(environment):
+    if environment.stats.total.fail_ratio > 0.01:
+        logging.error("Test failed due to failure ratio > 1%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 1000:
+        logging.error("Test failed due to 95th percentile response time > 1000 ms")
+        environment.process_exit_code = 1
+    else:
+        logging.info("Test passed!")
 
 
 def get_auth_request_body():
@@ -33,73 +99,3 @@ def get_demographics_request_body():
     # read the sample request body for the demographics endpoint
     with open("e2e/src/test/resources/newborn_patient.json", "r") as f:
         return f.read()
-
-
-def get_access_token(host, data):
-    # get a valid bearer token from the auth endpoint
-    request = urllib.request.Request(f"{host}{AUTH_ENDPOINT}", data=data)
-    response = urllib.request.urlopen(request)
-    jdata = json.load(response)
-    return jdata["access_token"]
-
-
-# def refresh_token(environment):
-#     global access_token
-
-#     while True:
-#         time.sleep(20)  # Refresh token every 5 seconds
-#         access_token = get_access_token(environment.host, auth_request_body)
-
-
-@events.test_start.add_listener
-def test_start(environment):
-    global demographics_request_body
-    global auth_request_body
-    global access_token
-
-    if isinstance(environment.runner, MasterRunner):
-        # in a distributed run, the master does not typically need any test data
-        return
-
-    demographics_request_body = get_demographics_request_body()
-    auth_request_body = get_auth_request_body()
-    access_token = get_access_token(environment.host, auth_request_body)
-
-    # Start the token refreshing thread
-    # threading.Thread(target=refresh_token, args=(environment,), daemon=True).start()
-
-
-class SampleUser(HttpUser):
-    # Each task gets called randomly, but the number next to '@task' denotes
-    # how many more times that task will get called than other tasks
-
-    @task
-    def get_health(self):
-        self.client.get(HEALTH_ENDPOINT)
-
-    @task(5)  # this task will get called 5x more than the other
-    def post_v1_etor_demographics(self):
-        global access_token, auth_request_body
-
-        response = self.client.post(
-            DEMOGRAPHICS_ENDPOINT,
-            data=demographics_request_body,
-            headers={"Authorization": access_token},
-        )
-        if response.status_code == 401:
-            print("401 Unathorized. Assuming token expired and requesting a new one.")
-            access_token = get_access_token(self.environment.host, auth_request_body)
-
-    @task
-    def post_v1_auth(self):
-        self.client.post(AUTH_ENDPOINT, data=auth_request_body)
-
-
-@events.quitting.add_listener
-def assert_stats(environment):
-    if environment.stats.total.fail_ratio > 0.01:
-        print("Test failed due to failure ratio > 1%")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 1000:
-        print("Test failed due to 95th percentile response time > 1000 ms")
-        environment.process_exit_code = 1
