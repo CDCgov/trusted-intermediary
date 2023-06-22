632da67b4f9fa9a8ff9039b17ebd0619d2813599
jorge Lopez <jlopez@flexion.us>
2023-06-22 09:00:37 -0700

    Add validator to handlerOrders

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistration.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistration.java
@@ -100,11 +100,26 @@ public class EtorDomainRegistration implements DomainConnector {
         return patientDemographicsController.constructResponse(patientDemographicsResponse);
     }
 
-    DomainResponse handleOrders(DomainRequest domainRequest) {
+    DomainResponse handleOrders(DomainRequest request) {
         //  Validate token
-        //  var orders = OrdersController.ParseOrders(request)
+        try {
+            if (!authValidator.isValidAuthenticatedRequest(request)) {
+                var errorMessage = "The request failed the authentication check";
+                logger.logError(errorMessage);
+
+                // ordersController.constructResponse(401, errorMessage)
+                return new DomainResponse(401);
+            }
+        } catch (SecretRetrievalException | IllegalArgumentException e) {
+            logger.logFatal("Unable to validate whether the request is authenticated", e);
+
+            // return ordersController.constructResponse(500, e)
+            return new DomainResponse(500);
+        }
+
+        //  var orders = ordersController.ParseOrders(request)
         //  convertAndSendLabOrderUseCase.covertAndSend(order)
-        //  return OrdersController.constructResponse(OrdersResponse)
+        //  return ordersController.constructResponse(OrdersResponse)
         return new DomainResponse(200);
     }
 }
