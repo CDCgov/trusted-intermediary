2ed2a5899b42e3a1f29a82fd437fda5d58328855
halprin <halprin@users.noreply.github.com>
2023-06-23 11:13:03 -0600

    Create a saved log query to view our application logs

--- operations/template/logs.tf
+++ operations/template/logs.tf
@@ -5,6 +5,15 @@ resource "azurerm_log_analytics_workspace" "logs_workspace" {
   location            = azurerm_resource_group.group.location
 }
 
+resource "azurerm_log_analytics_saved_search" "raw_application_logs" {
+  name                       = "rawApplicationLogs"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.logs_workspace.id
+
+  category     = "Application Logs"
+  display_name = "Raw Application Logs"
+  query        = "AppServiceConsoleLogs | extend JsonResult = parse_json(ResultDescription) | project-away TimeGenerated, Level, ResultDescription, Host, Type, _ResourceId, OperationName, TenantId, SourceSystem | evaluate bag_unpack(JsonResult)"
+}
+
 resource "azurerm_monitor_diagnostic_setting" "app_to_logs" {
   name                       = "ti-app-to-logs-${var.environment}"
   target_resource_id         = azurerm_linux_web_app.api.id
