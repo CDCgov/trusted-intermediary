5cc1eed84d1f8e0889146b274822513531bde504
jorge Lopez <jlopez@flexion.us>
2023-06-27 11:36:35 -0700

    getPatientId works

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiLabOrderTest.groovy
@@ -1,6 +1,10 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi
 
 import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.CodeableConcept
+import org.hl7.fhir.r4.model.Coding
+import org.hl7.fhir.r4.model.Identifier
+import org.hl7.fhir.r4.model.Patient
 import spock.lang.Specification
 
 class HapiLabOrderTest extends Specification {
@@ -27,5 +31,21 @@ class HapiLabOrderTest extends Specification {
 
         then:
         orders.getFhirResourceId() == expectedId
+    }
+
+    def "getPatientId works"() {
+        given:
+        def expectedPatientId = "DogCow goes Moof"
+        def innerOrders = new Bundle()
+        def patient = new Patient().addIdentifier(new Identifier()
+                .setValue(expectedPatientId)
+                .setType(new CodeableConcept().addCoding(new Coding("http://terminology.hl7.org/CodeSystem/v2-0203", "MR", "Medical Record Number"))))
+        innerOrders.addEntry(new Bundle.BundleEntryComponent().setResource(patient))
+
+        when:
+        def orders = new HapiDemographics(innerOrders)
+
+        then:
+        orders.getPatientId() == expectedPatientId
     }
 }
