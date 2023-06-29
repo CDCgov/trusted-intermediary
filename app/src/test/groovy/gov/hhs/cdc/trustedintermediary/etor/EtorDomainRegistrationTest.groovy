9ac38ab5b87fb87efd4ad274e209ef31f039e8ac
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-06-29 12:45:59 -0700

    Fixed a couple of tests

--- app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistrationTest.groovy
+++ app/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/EtorDomainRegistrationTest.groovy
@@ -5,6 +5,7 @@ import gov.hhs.cdc.trustedintermediary.auth.AuthRequestValidator
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainResponse
+import gov.hhs.cdc.trustedintermediary.domainconnector.DomainResponseHelper
 import gov.hhs.cdc.trustedintermediary.domainconnector.HttpEndpoint
 import gov.hhs.cdc.trustedintermediary.etor.demographics.ConvertAndSendDemographicsUsecase
 import gov.hhs.cdc.trustedintermediary.etor.demographics.Demographics
@@ -24,8 +25,8 @@ class EtorDomainRegistrationTest extends Specification {
     def "domain registration has endpoints"() {
         given:
         def domainRegistration = new EtorDomainRegistration()
-        def demographicsEndpoint = new HttpEndpoint("POST", EtorDomainRegistration.DEMOGRAPHICS_API_ENDPOINT)
-        def ordersEndpoint = new HttpEndpoint("POST", EtorDomainRegistration.ORDERS_API_ENDPOINT)
+        def demographicsEndpoint = new HttpEndpoint("POST", EtorDomainRegistration.DEMOGRAPHICS_API_ENDPOINT, true)
+        def ordersEndpoint = new HttpEndpoint("POST", EtorDomainRegistration.ORDERS_API_ENDPOINT, true)
 
         when:
         def endpoints = domainRegistration.domainRegistration()
@@ -57,11 +58,12 @@ class EtorDomainRegistrationTest extends Specification {
         mockAuthValidator.isValidAuthenticatedRequest(_ as DomainRequest) >> true
 
         def mockDemographicsController = Mock(PatientDemographicsController)
+        def mockResponseHelper = Mock(DomainResponseHelper)
 
         def mockRequestId = "asdf-12341-jkl-7890"
 
         mockDemographicsController.parseDemographics(_ as DomainRequest) >> new DemographicsMock(mockRequestId, "a patient ID", "demographics")
-        mockDemographicsController.constructResponse(_ as PatientDemographicsResponse) >> new DomainResponse(418)
+        mockResponseHelper.constructOkResponse(_ as PatientDemographicsResponse) >> new DomainResponse(418)
 
         def mockUsecase = Mock(ConvertAndSendDemographicsUsecase)
 
@@ -69,6 +71,7 @@ class EtorDomainRegistrationTest extends Specification {
         TestApplicationContext.register(PatientDemographicsController, mockDemographicsController)
         TestApplicationContext.register(ConvertAndSendDemographicsUsecase, mockUsecase)
         TestApplicationContext.register(AuthRequestValidator, mockAuthValidator)
+        TestApplicationContext.register(DomainResponseHelper, mockResponseHelper)
         TestApplicationContext.injectRegisteredImplementations()
 
         def domainRequest = new DomainRequest()
@@ -77,7 +80,7 @@ class EtorDomainRegistrationTest extends Specification {
         domainRegistration.handleDemographics(domainRequest)
 
         then:
-        1 * mockDemographicsController.constructResponse(_ as PatientDemographicsResponse) >> { PatientDemographicsResponse demographicsResponse ->
+        1 * mockResponseHelper.constructOkResponse(_ as PatientDemographicsResponse) >> { PatientDemographicsResponse demographicsResponse ->
             assert demographicsResponse.fhirResourceId == mockRequestId
         }
         1 * mockUsecase.convertAndSend(_ as Demographics)
