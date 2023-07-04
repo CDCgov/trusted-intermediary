bcb98dbccc669066ee3c22421f577bda1ad9b7ea
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2023-07-04 12:08:14 -0400

    Created e2e tests for Order use case implementation
    - added e2e tests for orders endpoint implementation

---
+++ e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/OrderClient.java
@@ -0,0 +1,45 @@
+package gov.hhs.cdc.trustedintermediary.e2e;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+public class OrderClient {
+    private static final String CLIENT_TOKEN;
+    private static final String API_ENDPOINT = "/v1/etor/orders";
+
+    static {
+        try {
+            CLIENT_TOKEN =
+                    Files.readString(
+                            Path.of("..", "mock_credentials", "report-stream-valid-token.jwt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String submitOrder(String fhirBody) throws IOException, ParseException {
+        try (var response = submitOrderRaw(fhirBody, true)) {
+            return EntityUtils.toString(response.getEntity());
+        }
+    }
+
+    public static ClassicHttpResponse submitOrderRaw(String fhirBody, boolean loginFirst)
+            throws IOException, ParseException {
+
+        Map<String, String> headers = new HashMap<>();
+
+        if (loginFirst) {
+            var loginToken = AuthClient.login("report-stream", CLIENT_TOKEN);
+            headers.put("Authorization", "Bearer " + loginToken);
+        }
+
+        return Client.post(API_ENDPOINT, fhirBody, ContentType.APPLICATION_JSON, headers);
+    }
+}
---
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/OrderTest.groovy
@@ -0,0 +1,64 @@
+package gov.hhs.cdc.trustedintermediary.e2e
+
+import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class OrderTest extends Specification {
+
+    def labOrderJsonFileString = Files.readString(Paths.get("src/test/resources/lab_order.json"))
+
+    def "an order response is returned from the ETOR order endpoint"() {
+        given:
+        def expectedFhirResourceId  = "Bundle/969bcbb3-cd34-49be-ac4f-e1b8479b8219"
+        def expectedPatientId  = "MRN7465737865"
+
+        when:
+        def responseBody = DemographicsClient.submitDemographics(labOrderJsonFileString)
+        def parsedJsonBody = JsonParsing.parse(responseBody, Map.class)
+
+        then:
+        parsedJsonBody.fhirResourceId == expectedFhirResourceId
+        parsedJsonBody.patientId == expectedPatientId
+    }
+
+    def "bad response given for poorly formatted JSON"() {
+
+        when:
+        def responseBody = OrderClient.submitOrder(labOrderJsonFileString.substring(1))
+        //removed beginning '{' to make this JSON invalid
+
+        then:
+        responseBody == "Server Error"
+    }
+
+    def "payload file check"() {
+
+        when:
+        def responseBody = OrderClient.submitOrder(labOrderJsonFileString)
+        def sentPayload = SentPayloadReader.read()
+        def parsedResponseBody = JsonParsing.parse(responseBody, Map.class)
+
+        def parsedSentPayload = JsonParsing.parse(sentPayload, Map.class)
+
+        then:
+
+        parsedSentPayload.entry[0].resource.resourceType == "MessageHeader"
+        parsedSentPayload.entry[3].resource.resourceType == "ServiceRequest"
+
+        parsedSentPayload.entry[2].resource.resourceType == "Patient"
+        parsedSentPayload.entry[2].resource.id == "infant-twin-1"
+
+        parsedSentPayload.entry[2].resource.identifier[0].value == parsedResponseBody.patientId  //the second (index 1) identifier so happens to be the MRN
+        parsedSentPayload.resourceType + "/" + parsedSentPayload.id == parsedResponseBody.fhirResourceId
+    }
+
+    def "a 401 comes from the ETOR demographics endpoint when unauthenticated"() {
+        when:
+        def response = DemographicsClient.submitDemographicsRaw(labOrderJsonFileString, false)
+
+        then:
+        response.getCode() == 401
+    }
+}
