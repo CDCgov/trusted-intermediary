beef5cb84b44f529595e57abd3933401ec3a159e
halprin <halprin@users.noreply.github.com>
2023-07-04 10:05:30 -0600

    382: Add JavaDocs to the LoggerHelper

--- app/src/main/java/gov/hhs/cdc/trustedintermediary/external/slf4j/LoggerHelper.java
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/external/slf4j/LoggerHelper.java
@@ -6,7 +6,14 @@ import org.slf4j.MarkerFactory;
 import org.slf4j.event.Level;
 import org.slf4j.spi.LoggingEventBuilder;
 
+/**
+ * Helper class that centralizes logic used by the different loggers. This class is package private
+ * by choice and should be accessed through the loggers.
+ */
 class LoggerHelper {
+
+    private LoggerHelper() {}
+
     public static LoggingEventBuilder logMessageAtLevel(
             Logger logger, Level level, String message) {
         return logger.atLevel(level).setMessage(() -> message);
