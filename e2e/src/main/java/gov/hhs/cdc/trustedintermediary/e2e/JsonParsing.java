29be73233054f2b0f2add5ea4613a9463a8d6e5d
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-07-05 10:47:26 -0700

    Fixed typing

--- e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/JsonParsing.java
+++ e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/JsonParsing.java
@@ -1,6 +1,7 @@
 package gov.hhs.cdc.trustedintermediary.e2e;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,15 +12,15 @@ public class JsonParsing {
 
     private static final ObjectMapper JSON_OBJECT_MAPPER = new ObjectMapper();
 
-    public static Map parse(String json) throws JsonProcessingException {
-        return JSON_OBJECT_MAPPER.readValue(json, Map.class);
+    public static Map<String, ?> parse(String json) throws JsonProcessingException {
+        return JSON_OBJECT_MAPPER.readValue(json, new TypeReference<>() {});
     }
 
-    public static Map parse(InputStream stream) throws IOException {
-        return JSON_OBJECT_MAPPER.readValue(stream, Map.class);
+    public static Map<String, ?> parse(InputStream stream) throws IOException {
+        return JSON_OBJECT_MAPPER.readValue(stream, new TypeReference<>() {});
     }
 
-    public static Map parseContent(ClassicHttpResponse response) throws IOException {
+    public static Map<String, ?> parseContent(ClassicHttpResponse response) throws IOException {
         return parse(response.getEntity().getContent());
     }
 }
