60db4ffa44e31101d243b0dd0c524471ef5c4fca
halprin <halprin@users.noreply.github.com>
2023-07-05 09:17:30 -0600

    Run the `open` command on Linux after creating the IG
    Co-authored-by: Jorge Lopez <49923512+jorg3lopez@users.noreply.github.com>

--- ig/local-generate.sh
+++ ig/local-generate.sh
@@ -3,6 +3,6 @@
 docker build -t ig-local-dev -f Dockerfile-local-dev .
 docker run -it --rm --mount type=bind,source="$(pwd)",target=/trusted-intermediary ig-local-dev
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux-gnu" ]]; then
   open ./output/index.html
 fi
