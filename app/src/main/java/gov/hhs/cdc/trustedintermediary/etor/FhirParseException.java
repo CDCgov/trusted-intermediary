78b6c39f1c56312ff0261e48f14fab888f1f7eb6
jorge Lopez <jlopez@flexion.us>
2023-07-07 09:11:57 -0700

    fhir parse exception

---
+++ app/src/main/java/gov/hhs/cdc/trustedintermediary/etor/FhirParseException.java
@@ -0,0 +1,12 @@
+package gov.hhs.cdc.trustedintermediary.etor;
+
+/**
+ * This exception class gets triggered when an exception is thrown during parsing of a fhir bundle.
+ * Some causes that will trigger it are: an empty payload, wrong payload format as it expects a fhir
+ * bundle.
+ */
+public class FhirParseException extends Exception {
+    FhirParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
