8fbcf5f04717fa0e51d6fbb0e0c21edee5da81f3
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-08-16 13:47:16 -0700

    Reverted changes to bring back SentPayloadReader and newborn_patient.json (#473)
    * Bringed back newborn_patient.json and moved to examples/fhir folder
    
    * Bringed back SentPayloadReader class
    
    ---------
    
    Co-authored-by: Jorge Lopez <49923512+jorg3lopez@users.noreply.github.com>

--- e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/DemographicsTest.groovy
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/DemographicsTest.groovy
@@ -4,16 +4,15 @@ import spock.lang.Specification
 
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
 
 class DemographicsTest extends Specification {
 
     def demographicsClient = new EndpointClient("/v1/etor/demographics")
-    def newbornPatientJsonFileString = Files.readString(Path.of("..", "examples", "fhir/lab_order.json"))
+    def newbornPatientJsonFileString = Files.readString(Path.of("../examples/fhir/newborn_patient.json"))
 
     def "a demographics response is returned from the ETOR demographics endpoint"() {
         given:
-        def expectedFhirResourceId  = "Bundle/969bcbb3-cd34-49be-ac4f-e1b8479b8219"
+        def expectedFhirResourceId  = "Bundle/bundle-with-patient"
         def expectedPatientId  = "MRN7465737865"
 
         when:
@@ -30,17 +29,18 @@ class DemographicsTest extends Specification {
         when:
         def response = demographicsClient.submit(newbornPatientJsonFileString, true)
         def parsedResponseBody = JsonParsing.parseContent(response)
-        def sentPayload = Files.readString(Path.of("..", "examples", "fhir/lab_order.json"))
+        def sentPayload = SentPayloadReader.read()
         def parsedSentPayload = JsonParsing.parse(sentPayload)
 
         then:
         response.getCode() == 200
         parsedSentPayload.entry[0].resource.resourceType == "MessageHeader"
-        parsedSentPayload.entry[4].resource.resourceType == "ServiceRequest"
+        parsedSentPayload.entry[2].resource.resourceType == "ServiceRequest"
 
-        parsedSentPayload.entry[3].resource.resourceType == "Patient"
+        parsedSentPayload.entry[1].resource.resourceType == "Patient"
+        parsedSentPayload.entry[1].resource.id == "infant-twin-1"
 
-        parsedSentPayload.entry[3].resource.identifier[0].value == parsedResponseBody.patientId  //the second (index 1) identifier so happens to be the MRN
+        parsedSentPayload.entry[1].resource.identifier[1].value == parsedResponseBody.patientId  //the second (index 1) identifier so happens to be the MRN
         parsedSentPayload.resourceType + "/" + parsedSentPayload.id == parsedResponseBody.fhirResourceId
     }
 
