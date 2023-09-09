46e143a9ce85c9f9f51ca642ee3f566a7c518f48
halprin <halprin@users.noreply.github.com>
2023-09-09 16:31:35 -0700

    Shared module and ETOR module

---
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/Main.java
@@ -0,0 +1,7 @@
+package gov.hhs.cdc.trustedintermediary.etor;
+
+public class Main {
+    public static void main(String[] args) {
+        gov.hhs.cdc.trustedintermediary.shared.Main.main(args);
+    }
+}
---
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/shared/Main.java
@@ -0,0 +1,7 @@
+package gov.hhs.cdc.trustedintermediary.shared;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
