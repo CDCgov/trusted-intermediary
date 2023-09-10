3c7afd2104fb1a66960f6cd409af322d495fc547
halprin <halprin@users.noreply.github.com>
2023-09-10 16:16:14 -0700

    Move over SendOrderUseCase.java from fhir-connectathon-34 branch

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/SendOrderUseCase.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/SendOrderUseCase.java
@@ -1,5 +1,7 @@
 package gov.hhs.cdc.trustedintermediary.etor.orders;
 
+import gov.hhs.cdc.trustedintermediary.etor.tasks.Task;
+import gov.hhs.cdc.trustedintermediary.etor.tasks.TaskNotifier;
 import javax.inject.Inject;
 
 /** The overall logic to receive, convert to OML, and subsequently send a lab order. */
@@ -8,6 +10,7 @@ public class SendOrderUseCase {
 
     @Inject OrderConverter converter;
     @Inject OrderSender sender;
+    @Inject TaskNotifier notifier;
 
     private SendOrderUseCase() {}
 
@@ -18,5 +21,16 @@ public class SendOrderUseCase {
     public void send(final Order<?> order) throws UnableToSendOrderException {
         var omlOrder = converter.convertMetadataToOmlOrder(order);
         sender.sendOrder(omlOrder);
+    }
+
+    public void sendWithoutModification(final Order<?> order) throws UnableToSendOrderException {
+        sender.sendOrder(order);
+    }
+
+    public void sendTask(final Task<?> task) throws UnableToSendOrderException {
+        // TODO: save the task, we now own it.
+        // TODO: grab ServiceRequest, from the OG EHR, modify the `intent` and save it for when the
+        // lab requests it.
+        notifier.sendTaskId(task);
     }
 }
