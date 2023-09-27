11654606a8f360cb58527927da657fb62effe654
halprin <halprin@users.noreply.github.com>
2023-09-27 14:57:18 -0600

    Add Flexion environment

---
+++ operations/environments/flexion/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.74.0"
+    }
+  }
+
+  # Use a remote Terraform state in Azure Storage
+  backend "azurerm" {
+    resource_group_name  = "cdcti-terraform"
+    storage_account_name = "cdctiterraform"
+    container_name       = "tfstate"
+    key                  = "flexion.terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_deleted_secrets_on_destroy = false
+    }
+  }
+}
+
+module "template" {
+  source = "../../template/"
+
+  environment = "flexion"
+}
