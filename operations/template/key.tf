7790c257110aa6d4b1aa95394de339dca411e568
halprin <halprin@users.noreply.github.com>
2023-09-27 15:49:36 -0600

    Update the github app registration ID for permission to the key vault

--- operations/template/key.tf
+++ operations/template/key.tf
@@ -13,7 +13,8 @@ resource "azurerm_key_vault" "key_storage" {
 resource "azurerm_key_vault_access_policy" "allow_github_deployer" {
   key_vault_id = azurerm_key_vault.key_storage.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "d59c2c86-de5e-41b7-a752-0869a73f5a60" //github app registration
+#  object_id    = "d59c2c86-de5e-41b7-a752-0869a73f5a60" //github app registration
+  object_id    = "f5feabe7-5d37-40ba-94f2-e5c0760b4561" //github app registration
 
   secret_permissions = [
     "Set",
