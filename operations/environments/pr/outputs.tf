dd09f2ed7c7c6f81e19b05978c9e95f9f3fe4cf8
halprin <halprin@users.noreply.github.com>
2023-10-16 16:02:29 -0600

    Remove acr_username and acr_password from Terraform outputs

--- operations/environments/pr/outputs.tf
+++ operations/environments/pr/outputs.tf
@@ -2,16 +2,6 @@ output "registry" {
   value = module.template.registry
 }
 
-output "acr_username" {
-  value     = module.template.acr_username
-  sensitive = true
-}
-
-output "acr_password" {
-  value     = module.template.acr_password
-  sensitive = true
-}
-
 output "publish_app" {
   value = module.template.publish_app
 }
