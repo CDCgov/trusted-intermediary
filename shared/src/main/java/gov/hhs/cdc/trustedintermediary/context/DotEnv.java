b4b1a8eceb72ac745e4cb39ed41ec90c3e80820c
jorge Lopez <jlopez@flexion.us>
2023-10-26 09:26:36 -0700

    javadocs

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/context/DotEnv.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/context/DotEnv.java
@@ -2,6 +2,22 @@ package gov.hhs.cdc.trustedintermediary.context;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+/**
+ * The <code>DotEnv</code> class provides a simple interface for managing environment variables
+ * using the Dotenv library. This utility is designed to load and retrieve environment variables
+ * from a .env file located in the shared <code>src/main/resource</code> folder. If the .env file is
+ * not found in the specified location, Dotenv will also check system variables.
+ *
+ * <p>To use this utility, you can retrieve environment variables by their keys using the <code>get
+ * </code> methods provided. If a key is not found, you can specify a default value to return.
+ *
+ * <p>The <code>load</code> method is primarily intended for testing purposes, allowing for custom
+ * Dotenv instances to be set. It can be useful when configuring specific environments for testing
+ * scenarios.
+ *
+ * @see <a href="https://github.com/cdimascio/dotenv-java">Dotenv Java GitHub Repository</a>
+ * @see io.github.cdimascio.dotenv.Dotenv
+ */
 class DotEnv {
     private static Dotenv DOTENV;
 
@@ -19,6 +35,7 @@ class DotEnv {
         return DOTENV.get(key, defaultValue);
     }
 
+    // Leave with default scope
     static void load(Dotenv dotenv) {
         DOTENV = dotenv;
     }
