b9f4135ab5c24c4c2b2ee6d0d95be2b81d139c1d
halprin <halprin@users.noreply.github.com>
2023-11-21 11:13:43 -0700

    497: Update the env to RS env mapping for staging

--- operations/environments/staging/main.tf
+++ operations/environments/staging/main.tf
@@ -28,5 +28,5 @@ module "template" {
   source = "../../template/"
 
   environment = "stg"
-  deployer_id = "f5feabe7-5d37-40ba-94f2-e5c0760b4561" //github app registration in Flexion Azure Entra
+  deployer_id = "f5feabe7-5d37-40ba-94f2-e5c0760b4561" //github app registration in CDC Azure Entra
 }
--- operations/template/main.tf
+++ operations/template/main.tf
@@ -1,7 +1,7 @@
 locals {
   environment_to_rs_environment_prefix_mapping = {
     dev     = "staging"
-    staging = "stg"
+    stg     = "staging"
     prod    = ""
   }
   selected_rs_environment_prefix = lookup(local.environment_to_rs_environment_prefix_mapping, var.environment, "staging")
