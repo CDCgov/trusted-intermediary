63786e9f34ff8fb2ff2ac30833566358c9da17c8
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-11-28 01:43:13 +0800

    Refactor internal metadata step to use generics instead of empty interface (#681)
    * Removed MetaDataStep interface and refactored code to use generics instead
    
    * EtorMetaDataStep is not implementing an interface any more

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/metadata/MetaDataEntry.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/metadata/MetaDataEntry.java
@@ -3,9 +3,9 @@ package gov.hhs.cdc.trustedintermediary.metadata;
 import java.time.Instant;
 
 /** An instance of a metadata event to be used for internal troubleshooting of messages */
-public record MetaDataEntry(String bundleId, MetaDataStep entryStep, Instant entryTime) {
+public record MetaDataEntry<T>(String bundleId, T entryStep, Instant entryTime) {
 
-    public MetaDataEntry(String bundleId, MetaDataStep entryStep) {
+    public MetaDataEntry(String bundleId, T entryStep) {
         this(bundleId, entryStep, Instant.now());
     }
 }
--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/MetricMetaData.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/wrappers/MetricMetaData.java
@@ -1,9 +1,7 @@
 package gov.hhs.cdc.trustedintermediary.wrappers;
 
-import gov.hhs.cdc.trustedintermediary.metadata.MetaDataStep;
-
 /** Interface to provide a blueprint for working with metadate */
 public interface MetricMetaData {
 
-    void put(String value, MetaDataStep step);
+    <T> void put(String value, T step);
 }
