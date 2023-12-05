4cd0cf4cd8ce99376ae30078aabdd3bd33975267
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-12-05 15:21:03 -0800

    Added needed container access permissions

--- operations/template/storage.tf
+++ operations/template/storage.tf
@@ -13,3 +13,9 @@ resource "azurerm_storage_container" "metadata" {
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
+
+resource "azurerm_role_assignment" "allow_api_read_write" {
+  scope                = azurerm_storage_container.metadata.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_web_app.api.identity.0.principal_id
+}
