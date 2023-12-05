63f61cc48310e24210e019b6fd8d33d73d3fe918
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-12-05 15:09:24 -0800

    Added exception handling for azure exceptions

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/azure/AzureStorageAccountPartnerMetadataStorage.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/azure/AzureStorageAccountPartnerMetadataStorage.java
@@ -1,11 +1,13 @@
 package gov.hhs.cdc.trustedintermediary.external.azure;
 
+import com.azure.core.exception.AzureException;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
 import gov.hhs.cdc.trustedintermediary.etor.metadata.PartnerMetadata;
+import gov.hhs.cdc.trustedintermediary.etor.metadata.PartnerMetadataException;
 import gov.hhs.cdc.trustedintermediary.etor.metadata.PartnerMetadataStorage;
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
 import java.io.ByteArrayInputStream;
@@ -43,20 +45,31 @@ public class AzureStorageAccountPartnerMetadataStorage implements PartnerMetadat
     }
 
     @Override
-    public void saveMetadata(final PartnerMetadata metadata) {
-        BlobClient blobClient = CONTAINER_CLIENT.getBlobClient(metadata.uniqueId() + ".json");
-        String content = "serialize(metadata)";
-        ByteArrayInputStream inputStream =
-                new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
-        blobClient.upload(inputStream, content.length(), true);
-        logger.logInfo(
-                "Saved metadata for "
-                        + metadata.uniqueId()
-                        + " to "
-                        + METADATA_CONTAINER_NAME
-                        + " container in "
-                        + STORAGE_ACCOUNT_BLOB_ENDPOINT
-                        + " Azure storage account"
-                        + blobClient.getBlobUrl());
+    public void saveMetadata(final PartnerMetadata metadata) throws PartnerMetadataException {
+        String metadataFileName = metadata.uniqueId() + ".json";
+        try {
+            BlobClient blobClient = CONTAINER_CLIENT.getBlobClient(metadataFileName);
+            String content = "serialize(metadata)";
+            ByteArrayInputStream inputStream =
+                    new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+            blobClient.upload(inputStream, content.length(), true);
+            logger.logInfo(
+                    "Saved metadata for "
+                            + metadata.uniqueId()
+                            + " to "
+                            + METADATA_CONTAINER_NAME
+                            + " container in "
+                            + STORAGE_ACCOUNT_BLOB_ENDPOINT
+                            + " Azure storage account"
+                            + blobClient.getBlobUrl());
+        } catch (AzureException e) {
+            throw new PartnerMetadataException(
+                    "Failed to upload "
+                            + metadataFileName
+                            + " in "
+                            + METADATA_CONTAINER_NAME
+                            + " container",
+                    e);
+        }
     }
 }
