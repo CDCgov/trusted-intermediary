8315aeac13566e9493f4573ce153884c3f5a3cbf
halprin <halprin@users.noreply.github.com>
2023-12-12 08:33:56 -0700

    498: Change prod environment name to prd

--- operations/environments/prod/main.tf
+++ operations/environments/prod/main.tf
@@ -27,6 +27,6 @@ provider "azurerm" {
 module "template" {
   source = "../../template/"
 
-  environment = "prod"
+  environment = "prd"
   deployer_id = "f5feabe7-5d37-40ba-94f2-e5c0760b4561" //github app registration in CDC Azure Entra
 }
