67d4b05f3cb1fb157fa07266b806e788298502e6
Jeff Crichlake <jcrichlake@gmail.com>
2023-12-14 09:54:21 -0500

    Fixing sku

--- operations/template/db.tf
+++ operations/template/db.tf
@@ -2,7 +2,7 @@ resource "azurerm_postgresql_flexible_server" "database" {
   name                = "cdcti-${var.environment}-database"
   resource_group_name = data.azurerm_resource_group.group.name
   location            = data.azurerm_resource_group.group.location
-  sku_name            = "B_Gen5_1"
+  sku_name            = "B_Standard_B1ms"
   version             = "16"
   storage_mb          = "32768"
   backup_retention_days = "14"
