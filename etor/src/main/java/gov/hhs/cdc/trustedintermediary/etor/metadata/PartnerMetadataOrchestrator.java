a853ab7b0f2e457c8c57a9f9614e690708d72c87
halprin <halprin@users.noreply.github.com>
2023-12-20 10:52:31 -0700

    672: add comments to what we want the methods in the ParterMetadataOrchestrator to do

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/metadata/PartnerMetadataOrchestrator.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/metadata/PartnerMetadataOrchestrator.java
@@ -12,12 +12,28 @@ public class PartnerMetadataOrchestrator {
 
     private PartnerMetadataOrchestrator() {}
 
-    public void updateMetadataForReceivedOrder(String submissionId, Order<?> order) {}
+    public void updateMetadataForReceivedOrder(String submissionId, Order<?> order) {
+        // will call the RS history API given the submissionId (albeit, right now this won't work
+        // given the way RS works).
+        // from the history API response, extract the sender (organization + sender client), and
+        // time received.
+        // we will calculate the hash.
+        // then we call the metadata storage to save this stuff.
+    }
 
     public void updateMetadataForSentOrder(
-            String receivedSubmissionId, String sentSubmissionId, Order<?> order) {}
+            String receivedSubmissionId, String sentSubmissionId, Order<?> order) {
+        // call the metadata storage and add the sent order's submission ID to the existing metadata
+        // entry
+    }
 
     public PartnerMetadata getMetadata(String submissionId) {
+        // call the metadata storage to get the metadata.
+        // check if the receiver is filled out, and if it isn't, call the RS history API to get the
+        // receiver.
+        // if had to call the history API, extract the receiver and call the metadata storage to
+        // save the metadata with the receiver added.
+        // return the metadata.
         return null;
     }
 }
