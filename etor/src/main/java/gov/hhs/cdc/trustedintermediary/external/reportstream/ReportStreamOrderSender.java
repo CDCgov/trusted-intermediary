a84110784f0bb826f4f2991b46f445a95ae3b6b1
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-12-27 09:58:43 -0800

    Refactored to split RS endpoint logic to its own ReportStreamEndpointClient class

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamOrderSender.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamOrderSender.java
@@ -1,27 +1,17 @@
 package gov.hhs.cdc.trustedintermediary.external.reportstream;
 
-import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
 import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
 import gov.hhs.cdc.trustedintermediary.etor.orders.Order;
 import gov.hhs.cdc.trustedintermediary.etor.orders.OrderSender;
 import gov.hhs.cdc.trustedintermediary.etor.orders.UnableToSendOrderException;
-import gov.hhs.cdc.trustedintermediary.wrappers.AuthEngine;
-import gov.hhs.cdc.trustedintermediary.wrappers.Cache;
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir;
-import gov.hhs.cdc.trustedintermediary.wrappers.HttpClient;
-import gov.hhs.cdc.trustedintermediary.wrappers.HttpClientException;
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
 import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
-import gov.hhs.cdc.trustedintermediary.wrappers.SecretRetrievalException;
-import gov.hhs.cdc.trustedintermediary.wrappers.Secrets;
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter;
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.FormatterProcessingException;
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.TypeReference;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.Optional;
-import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
 /** Accepts a {@link Order} and sends it to ReportStream. */
@@ -29,32 +19,10 @@ public class ReportStreamOrderSender implements OrderSender {
 
     private static final ReportStreamOrderSender INSTANCE = new ReportStreamOrderSender();
 
-    private static final String RS_URL_PREFIX_PROPERTY = "REPORT_STREAM_URL_PREFIX";
-    private static final String RS_WATERS_API_URL =
-            ApplicationContext.getProperty(RS_URL_PREFIX_PROPERTY) + "/api/waters";
-    private static final String RS_AUTH_API_URL =
-            ApplicationContext.getProperty(RS_URL_PREFIX_PROPERTY) + "/api/token";
-    private static final String RS_DOMAIN_NAME =
-            Optional.ofNullable(ApplicationContext.getProperty(RS_URL_PREFIX_PROPERTY))
-                    .map(urlPrefix -> urlPrefix.replace("https://", "").replace("http://", ""))
-                    .orElse("");
-
-    private static final String OUR_PRIVATE_KEY_ID =
-            "trusted-intermediary-private-key-" + ApplicationContext.getEnvironment();
-    private static final String RS_TOKEN_CACHE_ID = "report-stream-token";
-
-    private static final String CLIENT_NAME = "flexion.etor-service-sender";
-    private static final Map<String, String> RS_AUTH_API_HEADERS =
-            Map.of("Content-Type", "application/x-www-form-urlencoded");
-
-    @Inject private HttpClient client;
-    @Inject private AuthEngine jwt;
+    @Inject private ReportStreamEndpointClient rsclient;
     @Inject private Formatter formatter;
     @Inject private HapiFhir fhir;
     @Inject private Logger logger;
-    @Inject private Secrets secrets;
-    @Inject private Cache cache;
-
     @Inject MetricMetadata metadata;
 
     public static ReportStreamOrderSender getInstance() {
@@ -65,10 +33,10 @@ public class ReportStreamOrderSender implements OrderSender {
 
     @Override
     public Optional<String> sendOrder(final Order<?> order) throws UnableToSendOrderException {
-        logger.logInfo("Sending the order to ReportStream at {}", RS_DOMAIN_NAME);
+        //        logger.logInfo("Sending the order to ReportStream at {}", RS_DOMAIN_NAME);
         String json = fhir.encodeResourceToJson(order.getUnderlyingOrder());
-        String bearerToken = getRsToken();
-        String rsResponseBody = sendRequestBody(json, bearerToken);
+        String bearerToken = rsclient.getRsToken();
+        String rsResponseBody = rsclient.sendRequestBody(json, bearerToken);
         Optional<String> submissionId = getSubmissionId(rsResponseBody);
         logger.logInfo("Order successfully sent, ReportStream submissionId={}", submissionId);
         metadata.put(order.getFhirResourceId(), EtorMetadataStep.SENT_TO_REPORT_STREAM);
@@ -86,121 +54,5 @@ public class ReportStreamOrderSender implements OrderSender {
         }
 
         return Optional.empty();
-    }
-
-    protected String getRsToken() throws UnableToSendOrderException {
-        logger.logInfo("Looking up ReportStream token");
-
-        var token = cache.get(RS_TOKEN_CACHE_ID);
-
-        if (token != null && isValidToken(token)) {
-            logger.logDebug("valid cache token");
-            return token;
-        }
-
-        token = requestToken();
-
-        cache.put(RS_TOKEN_CACHE_ID, token);
-
-        return token;
-    }
-
-    protected boolean isValidToken(String token) {
-        LocalDateTime expirationDate = jwt.getExpirationDate(token);
-
-        return LocalDateTime.now().isBefore(expirationDate.minus(15, ChronoUnit.SECONDS));
-    }
-
-    protected String sendRequestBody(@Nonnull String json, @Nonnull String bearerToken)
-            throws UnableToSendOrderException {
-        logger.logInfo("Sending payload to ReportStream");
-
-        String res = "";
-        Map<String, String> headers =
-                Map.of(
-                        "Authorization",
-                        "Bearer " + bearerToken,
-                        "client",
-                        CLIENT_NAME,
-                        "Content-Type",
-                        "application/fhir+ndjson");
-        try {
-            res = client.post(RS_WATERS_API_URL, headers, json);
-        } catch (HttpClientException e) {
-            throw new UnableToSendOrderException("Error POSTing the payload to ReportStream", e);
-        }
-
-        return res;
-    }
-
-    protected String requestToken() throws UnableToSendOrderException {
-        logger.logInfo("Requesting token from ReportStream");
-
-        String ourPrivateKey;
-        String token;
-
-        try {
-            ourPrivateKey = retrievePrivateKey();
-            String senderToken =
-                    jwt.generateToken(
-                            CLIENT_NAME,
-                            CLIENT_NAME,
-                            CLIENT_NAME,
-                            RS_DOMAIN_NAME,
-                            300,
-                            ourPrivateKey);
-            String body = composeRequestBody(senderToken);
-            String rsResponse = client.post(RS_AUTH_API_URL, RS_AUTH_API_HEADERS, body);
-            token = extractToken(rsResponse);
-        } catch (Exception e) {
-            throw new UnableToSendOrderException(
-                    "Error getting the API token from ReportStream", e);
-        }
-
-        // only cache our private key if we successfully authenticate to RS
-        cacheOurPrivateKeyIfNotCachedAlready(ourPrivateKey);
-
-        return token;
-    }
-
-    protected String retrievePrivateKey() throws SecretRetrievalException {
-        String key = cache.get(OUR_PRIVATE_KEY_ID);
-        if (key != null) {
-            return key;
-        }
-
-        key = secrets.getKey(OUR_PRIVATE_KEY_ID);
-
-        return key;
-    }
-
-    void cacheOurPrivateKeyIfNotCachedAlready(String privateKey) {
-        String key = cache.get(OUR_PRIVATE_KEY_ID);
-        if (key != null) {
-            return;
-        }
-
-        cache.put(OUR_PRIVATE_KEY_ID, privateKey);
-    }
-
-    protected String extractToken(String responseBody) throws FormatterProcessingException {
-        var value =
-                formatter.convertJsonToObject(
-                        responseBody, new TypeReference<Map<String, String>>() {});
-        return value.get("access_token");
-    }
-
-    protected String composeRequestBody(String senderToken) {
-        String scope = "flexion.*.report";
-        String grantType = "client_credentials";
-        String clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
-        return "scope="
-                + scope
-                + "&grant_type="
-                + grantType
-                + "&client_assertion_type="
-                + clientAssertionType
-                + "&client_assertion="
-                + senderToken;
     }
 }
--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamOrderSenderTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamOrderSenderTest.groovy
@@ -4,22 +4,17 @@ import gov.hhs.cdc.trustedintermediary.OrderMock
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep
 import gov.hhs.cdc.trustedintermediary.etor.orders.OrderSender
-import gov.hhs.cdc.trustedintermediary.etor.orders.UnableToSendOrderException
-import gov.hhs.cdc.trustedintermediary.external.inmemory.KeyCache
 import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
 import gov.hhs.cdc.trustedintermediary.wrappers.AuthEngine
 import gov.hhs.cdc.trustedintermediary.wrappers.Cache
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
 import gov.hhs.cdc.trustedintermediary.wrappers.HttpClient
-import gov.hhs.cdc.trustedintermediary.wrappers.HttpClientException
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger
 import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata
 import gov.hhs.cdc.trustedintermediary.wrappers.Secrets
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.FormatterProcessingException
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.TypeReference
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 import spock.lang.Specification
 
 class ReportStreamOrderSenderTest extends Specification {
@@ -28,223 +23,12 @@ class ReportStreamOrderSenderTest extends Specification {
         TestApplicationContext.reset()
         TestApplicationContext.init()
         TestApplicationContext.register(OrderSender, ReportStreamOrderSender.getInstance())
+        TestApplicationContext.register(ReportStreamEndpointClient, ReportStreamEndpointClient.getInstance())
         TestApplicationContext.register(MetricMetadata, Mock(MetricMetadata))
-    }
-
-    def "sendRequestBody works"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().sendRequestBody("message_1", "fake token")
-        ReportStreamOrderSender.getInstance().sendRequestBody("message_2", "fake token")
-
-        then:
-        2 * mockClient.post(_ as String, _ as Map<String, String>, _ as String) >> "200"
-    }
-
-    def "sendRequestBody fails from an IOException from the client"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        mockClient.post(_ as String, _ as Map<String, String>, _ as String) >> { throw new IOException("oops") }
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().sendRequestBody("message_1", "fake token")
-        ReportStreamOrderSender.getInstance().sendRequestBody("message_2", "fake token")
-
-        then:
-        def exception = thrown(Exception)
-        exception.getCause().getClass() == IOException
-    }
-
-    def "requestToken works"() {
-        given:
-        def expected = "rs fake token"
-        def mockAuthEngine = Mock(AuthEngine)
-        def mockClient = Mock(HttpClient)
-        def mockSecrets = Mock(Secrets)
-        def mockCache = Mock(Cache)
-        TestApplicationContext.register(AuthEngine, mockAuthEngine)
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.register(Secrets, mockSecrets)
-        TestApplicationContext.register(Cache, mockCache)
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        mockSecrets.getKey(_ as String) >> "Fake Azure Key"
-        def actual = ReportStreamOrderSender.getInstance().requestToken()
-
-        then:
-        1 * mockAuthEngine.generateToken(_ as String, _ as String, _ as String, _ as String, 300, _ as String) >> "sender fake token"
-        1 * mockClient.post(_ as String, _ as Map<String, String>, _ as String) >> """{"access_token":"${expected}", "token_type":"bearer"}"""
-        actual == expected
-    }
-
-    def "requestToken saves our private key only after successful call to RS"() {
-        given:
-        def mockSecrets = Mock(Secrets)
-        def mockCache = Mock(Cache)
-        def mockFormatter = Mock(Formatter)
-
-        def fakeOurPrivateKey = "DogCow" // pragma: allowlist secret
-        mockSecrets.getKey(_ as String) >> fakeOurPrivateKey
-        mockFormatter.convertJsonToObject(_ , _) >> [access_token: "Moof!"]
-
-        TestApplicationContext.register(AuthEngine, Mock(AuthEngine))
-        TestApplicationContext.register(HttpClient, Mock(HttpClient))
-        TestApplicationContext.register(Formatter, mockFormatter)
-        TestApplicationContext.register(Secrets, mockSecrets)
-        TestApplicationContext.register(Cache, mockCache)
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().requestToken()
-
-        then:
-        1 * mockCache.put(_ as String, fakeOurPrivateKey)
-    }
-
-    def "requestToken doesn't cache our private key if RS auth call fails"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        def mockCache = Mock(Cache)
-        def mockFormatter = Mock(Formatter)
-
-        mockClient.post(_, _, _) >> { throw new HttpClientException("Fake failure", new NullPointerException()) }
-
-        mockFormatter.convertJsonToObject(_ , _) >> [access_token: "Moof!"]
-
-        TestApplicationContext.register(AuthEngine, Mock(AuthEngine))
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(Formatter, mockFormatter)
-        TestApplicationContext.register(Secrets, Mock(Secrets))
-        TestApplicationContext.register(Cache, mockCache)
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().requestToken()
-
-        then:
-        thrown(UnableToSendOrderException)
-        0 * mockCache.put(_ , _)
-    }
-
-    def "cacheOurPrivateKeyIfNotCachedAlready doesn't cache when the key is already is cached"() {
-        given:
-        def mockCache = Mock(Cache)
-        mockCache.get(_ as String) >> "DogCow private key"
-
-        TestApplicationContext.register(Cache, mockCache)
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().cacheOurPrivateKeyIfNotCachedAlready("Moof!")
-
-        then:
-        0 * mockCache.put(_, _)
-    }
-
-    def "cacheOurPrivateKeyIfNotCachedAlready caches when the key isn't cached"() {
-        given:
-        def mockCache = Mock(Cache)
-        mockCache.get(_ as String) >> null
-
-        TestApplicationContext.register(Cache, mockCache)
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        ReportStreamOrderSender.getInstance().cacheOurPrivateKeyIfNotCachedAlready("Moof!")
-
-        then:
-        1 * mockCache.put(_, _)
-    }
-
-    def "extractToken works"() {
-        given:
-        TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.injectRegisteredImplementations()
-
-        def expected = "IaMAfaKEt0keNN"
-        def responseBody = """{"foo":"foo value", "access_token":"${expected}", "boo":"boo value"}"""
-
-        when:
-        def actual = ReportStreamOrderSender.getInstance().extractToken(responseBody)
-
-        then:
-        actual == expected
-    }
-
-    def "extractToken works when access_token is a number"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        def mockCache = Mock(Cache)
-        TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(Secrets, Mock(Secrets))
-        TestApplicationContext.register(AuthEngine, Mock(AuthEngine))
-        TestApplicationContext.register(Cache, mockCache)
-        TestApplicationContext.injectRegisteredImplementations()
-        def expectedTokenValue = "3"
-
-        def responseBody = """{"foo":"foo value", "access_token": 3, "boo":"boo value"}"""
-        mockClient.post(_ as String, _ as Map, _ as String) >> responseBody
-
-        when:
-        def actualTokenValue = ReportStreamOrderSender.getInstance().requestToken()
-
-        then:
-        actualTokenValue == expectedTokenValue
-    }
-
-    def "extractToken fails from not getting valid JSON from the auth token endpoint"() {
-        given:
-        def clientMock = Mock(HttpClient)
-        TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.register(HttpClient, clientMock)
-        TestApplicationContext.register(Secrets, Mock(Secrets))
-        TestApplicationContext.register(AuthEngine, Mock(AuthEngine))
-        TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.injectRegisteredImplementations()
-
-        def responseBody = """{"foo":"foo value", "access_token":"""
-        clientMock.post(_ as String, _ as Map, _ as String) >> responseBody
-
-        when:
-        ReportStreamOrderSender.getInstance().requestToken()
-
-        then:
-        def exception = thrown(UnableToSendOrderException)
-        exception.getCause().getClass() == FormatterProcessingException
-    }
-
-    def "composeRequestBody works"() {
-        given:
-        def reportStreamOrderSender = ReportStreamOrderSender.getInstance()
-        def fakeToken = "rsFakeToken"
-        def expected = "scope=flexion.*.report" +
-                "&grant_type=client_credentials" +
-                "&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer" +
-                "&client_assertion=${fakeToken}"
-        when:
-        def actual = reportStreamOrderSender.composeRequestBody(fakeToken)
-        then:
-        actual == expected
     }
 
     def "send order works"() {
         given:
-        def orderSender = ReportStreamOrderSender.getInstance()
-        TestApplicationContext.register(OrderSender, orderSender)
-
         def mockAuthEngine = Mock(AuthEngine)
         TestApplicationContext.register(AuthEngine, mockAuthEngine)
 
@@ -274,6 +58,7 @@ class ReportStreamOrderSenderTest extends Specification {
         then:
         noExceptionThrown()
     }
+
     def "log the step to metadata when send order is called"() {
         given:
 
@@ -305,157 +90,6 @@ class ReportStreamOrderSenderTest extends Specification {
 
         then:
         1 * ReportStreamOrderSender.getInstance().metadata.put(_, EtorMetadataStep.SENT_TO_REPORT_STREAM)
-    }
-
-    def "retrievePrivateKey works when cache is empty" () {
-        given:
-        def mockSecret = Mock(Secrets)
-        def expected = "New Fake Azure Key"
-        mockSecret.getKey(_ as String) >> expected
-        TestApplicationContext.register(Secrets, mockSecret)
-        TestApplicationContext.register(Cache, KeyCache.getInstance())
-        TestApplicationContext.injectRegisteredImplementations()
-        def rsOrderSender = ReportStreamOrderSender.getInstance()
-        when:
-        def actual = rsOrderSender.retrievePrivateKey()
-
-        then:
-        actual == expected
-    }
-
-    def "retrievePrivateKey works when cache is not empty" () {
-        given:
-        def keyCache = KeyCache.getInstance()
-        def key = "trusted-intermediary-private-key-local"
-        def expected = "existing fake azure key"
-        TestApplicationContext.register(Cache, keyCache)
-        TestApplicationContext.injectRegisteredImplementations()
-        def rsOrderSender = ReportStreamOrderSender.getInstance()
-
-        when:
-        keyCache.put(key, expected)
-        def actual = rsOrderSender.retrievePrivateKey()
-
-        then:
-        expected == actual
-    }
-
-    def "ensure jwt that expires 15 seconds from now is valid"() {
-        given:
-        def mockAuthEngine = Mock(AuthEngine)
-
-        mockAuthEngine.getExpirationDate(_ as String) >> LocalDateTime.now().plus(20, ChronoUnit.SECONDS)
-
-        TestApplicationContext.register(AuthEngine, mockAuthEngine)
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        def isValid = ReportStreamOrderSender.getInstance().isValidToken("our token from rs")
-
-        then:
-        isValid
-    }
-
-    def "sendRequestBody bombs out due to http exception"() {
-        given:
-        def orderSender = ReportStreamOrderSender.getInstance()
-        def mockClient = Mock(HttpClient)
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(OrderSender, orderSender)
-        TestApplicationContext.injectRegisteredImplementations()
-
-        mockClient.post(_ as String, _ as Map<String,String>, _ as String) >> {
-            throw new HttpClientException("404",new IOException())
-        }
-
-        when:
-        orderSender.sendRequestBody("json", "bearerToken")
-
-        then:
-        def exception = thrown(UnableToSendOrderException)
-        exception.getCause().getClass() == HttpClientException
-    }
-
-    def "getRsToken when cache is empty we call RS to get a new one"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        def mockFormatter = Mock(Formatter)
-        def mockCache = Mock(Cache)
-
-        //make the cache empty
-        mockCache.get(_ as String) >> null
-
-        def freshTokenFromRs = "new token"
-        mockFormatter.convertJsonToObject(_, _ as TypeReference) >> [access_token: freshTokenFromRs]
-
-        TestApplicationContext.register(Formatter, mockFormatter)
-        TestApplicationContext.register(AuthEngine, Mock(AuthEngine))
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(Cache, mockCache)
-        TestApplicationContext.register(Secrets, Mock(Secrets))
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        def token = ReportStreamOrderSender.getInstance().getRsToken()
-
-        then:
-        1 * mockClient.post(_, _, _)
-        token == freshTokenFromRs
-    }
-
-    def "getRsToken when cache token is invalid we call RS to get a new one"() {
-        given:
-        def mockClient = Mock(HttpClient)
-        def mockAuthEngine = Mock(AuthEngine)
-        def mockFormatter = Mock(Formatter)
-        def mockCache = Mock(Cache)
-
-        mockCache.get(_ as String) >> "shouldn't be returned"
-
-        //mock the auth engine so that the JWT looks like it is invalid
-        mockAuthEngine.getExpirationDate(_) >> LocalDateTime.now().plus(10, ChronoUnit.SECONDS)
-
-        def freshTokenFromRs = "new token"
-        mockFormatter.convertJsonToObject(_, _ as TypeReference) >> [access_token: freshTokenFromRs]
-
-        TestApplicationContext.register(Formatter, mockFormatter)
-        TestApplicationContext.register(AuthEngine, mockAuthEngine)
-        TestApplicationContext.register(HttpClient, mockClient)
-        TestApplicationContext.register(Cache, mockCache)
-        TestApplicationContext.register(Secrets, Mock(Secrets))
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        def token = ReportStreamOrderSender.getInstance().getRsToken()
-
-        then:
-        1 * mockClient.post(_, _, _)
-        token == freshTokenFromRs
-    }
-
-    def "getRsToken when cache token is valid, return that cached token"() {
-        given:
-        def mockAuthEngine = Mock(AuthEngine)
-        def mockCache = Mock(Cache)
-
-        def cachedRsToken = "DogCow goes Moof!"
-        mockCache.get(_ as String) >> cachedRsToken
-
-        //mock the auth engine so that the JWT looks valid
-        mockAuthEngine.getExpirationDate(_) >> LocalDateTime.now().plus(60, ChronoUnit.SECONDS)
-
-        TestApplicationContext.register(AuthEngine, mockAuthEngine)
-        TestApplicationContext.register(Cache, mockCache)
-
-        TestApplicationContext.injectRegisteredImplementations()
-
-        when:
-        def token = ReportStreamOrderSender.getInstance().getRsToken()
-
-        then:
-        token == cachedRsToken
     }
 
     def "getSubmissionId logs submissionId if convertJsonToObject is successful"() {
