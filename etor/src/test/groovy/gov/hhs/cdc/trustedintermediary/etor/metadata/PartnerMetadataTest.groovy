2f758451be5aa59255a1c1e598d2c01b7987538d
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2023-12-28 11:56:32 -0800

    Added tests for PartnerMetadata changes

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/metadata/PartnerMetadataTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/metadata/PartnerMetadataTest.groovy
@@ -34,4 +34,45 @@ class PartnerMetadataTest extends Specification {
         metadata.timeReceived() == timeReceived
         metadata.hash() == hash
     }
+
+    def "test overloaded constructor"() {
+        given:
+        def receivedSubmissionId = "receivedSubmissionId"
+        def sender = "sender"
+        def timeReceived = Instant.now()
+        def hash = "abcd"
+
+        when:
+        def metadata = new PartnerMetadata(receivedSubmissionId, sender, timeReceived, hash)
+
+        then:
+        metadata.receivedSubmissionId() == receivedSubmissionId
+        metadata.sentSubmissionId() == null
+        metadata.sender() == sender
+        metadata.receiver() == null
+        metadata.timeReceived() == timeReceived
+        metadata.hash() == hash
+    }
+
+    def "test withSentSubmissionFields to update PartnerMetadata"() {
+        given:
+        def receivedSubmissionId = "receivedSubmissionId"
+        def sentSubmissionId = "sentSubmissionId"
+        def sender = "sender"
+        def receiver = "receiver"
+        def timeReceived = Instant.now()
+        def hash = "abcd"
+        def metadata = new PartnerMetadata(receivedSubmissionId, sender, timeReceived, hash)
+
+        when:
+        def updatedMetadata = metadata.withSentSubmissionFields(sentSubmissionId, receiver)
+
+        then:
+        updatedMetadata.receivedSubmissionId() == receivedSubmissionId
+        updatedMetadata.sentSubmissionId() == sentSubmissionId
+        updatedMetadata.sender() == sender
+        updatedMetadata.receiver() == receiver
+        updatedMetadata.timeReceived() == timeReceived
+        updatedMetadata.hash() == hash
+    }
 }
