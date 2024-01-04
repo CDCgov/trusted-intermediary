f33e565b0413c966695b62f59f9a3c6f18c3c37f
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-01-04 10:42:21 -0800

    Removed additional javadocs committed by mistake

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/RSEndpointClient.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/RSEndpointClient.java
@@ -8,33 +8,11 @@ import gov.hhs.cdc.trustedintermediary.external.reportstream.ReportStreamEndpoin
  */
 public interface RSEndpointClient {
 
-    /**
-     * Retrieves the Report Stream token.
-     *
-     * @return The Report Stream token.
-     * @throws ReportStreamEndpointClientException If an error occurs while retrieving the token.
-     */
     String getRsToken() throws ReportStreamEndpointClientException;
 
-    /**
-     * Sends a request to the Waters endpoint.
-     *
-     * @param body The request body.
-     * @param bearerToken The bearer token for authentication.
-     * @return The response from the Waters endpoint.
-     * @throws ReportStreamEndpointClientException If an error occurs while sending the request.
-     */
     String requestWatersEndpoint(String body, String bearerToken)
             throws ReportStreamEndpointClientException;
 
-    /**
-     * Sends a request to the History endpoint.
-     *
-     * @param submissionId The ID of the submission.
-     * @param bearerToken The bearer token for authentication.
-     * @return The response from the History endpoint.
-     * @throws ReportStreamEndpointClientException If an error occurs while sending the request.
-     */
     String requestHistoryEndpoint(String submissionId, String bearerToken)
             throws ReportStreamEndpointClientException;
 }
