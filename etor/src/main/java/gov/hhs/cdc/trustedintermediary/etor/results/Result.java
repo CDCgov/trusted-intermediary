3d7311b4bec88384c1b0eea2c0c5905a00a71dc0
jorge Lopez <jlopez@flexion.us>
2024-01-24 11:03:46 -0800

    result interface

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/Result.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/Result.java
@@ -1,3 +1,9 @@
 package gov.hhs.cdc.trustedintermediary.etor.results;
 
-public interface Result<T> {}
+public interface Result<T> {
+    T getUnderlyingOrder();
+
+    String getFhirResourceId();
+
+    String getPatientId();
+}
