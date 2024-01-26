47a10c3c66a51cde51146a5d1b7b21a90faea866
saquino0827 <saquino@flexion.us>
2024-01-26 15:25:40 -0600

    ResultTest e2e base

--- e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/ResultTest.groovy
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/ResultTest.groovy
@@ -38,4 +38,40 @@ class ResultTest extends Specification {
 
 		parsedSentPayload == parsedLabResultJsonFile
 	}
+
+	def "check that message type is converted to ORU_R01"() {
+		when:
+		resultClient.submit(labResultJsonFileString, submissionId, true)
+		def sentPayload = SentPayloadReader.read()
+		def parsedSentPayload = JsonParsing.parse(sentPayload)
+
+		then:
+		//test that the MessageHeader's event is now an OML_O21
+		parsedSentPayload.entry[0].resource.resourceType == "MessageHeader"
+		parsedSentPayload.entry[0].resource.eventCoding.code == "R01"
+		parsedSentPayload.entry[0].resource.eventCoding.display.contains("ORU")
+	}
+
+	def "return a 400 response when request has unexpected format"() {
+		given:
+		def invalidJsonRequest = labResultJsonFileString.substring(1)
+
+		when:
+		def response = resultClient.submit(invalidJsonRequest, submissionId, true)
+		def parsedJsonBody = JsonParsing.parseContent(response)
+
+		then:
+		response.getCode() == 400
+		!(parsedJsonBody.error as String).isEmpty()
+	}
+
+	def "return a 401 response when making an unauthenticated request"() {
+		when:
+		def response = resultClient.submit(labResultJsonFileString, submissionId, false)
+		def parsedJsonBody = JsonParsing.parseContent(response)
+
+		then:
+		response.getCode() == 401
+		!(parsedJsonBody.error as String).isEmpty()
+	}
 }
