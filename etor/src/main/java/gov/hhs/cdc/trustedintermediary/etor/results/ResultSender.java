a25aec81b3c796fd9cce1acb0ebc6d8fcedd75ff
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-01-26 14:47:44 -0800

    Another missed fix

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultSender.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultSender.java
@@ -3,7 +3,7 @@ package gov.hhs.cdc.trustedintermediary.etor.results;
 import gov.hhs.cdc.trustedintermediary.etor.messages.UnableToSendMessageException;
 import java.util.Optional;
 
-/** Interface for sending a lab order. */
+/** Interface for sending a lab result. */
 public interface ResultSender {
     Optional<String> send(Result<?> result) throws UnableToSendMessageException;
 }
