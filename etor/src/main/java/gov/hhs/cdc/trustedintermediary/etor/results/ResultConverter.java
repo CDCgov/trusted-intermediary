e1dcf79d6b06f544e5f40c3002374eaf6ef580a1
saquino0827 <saquino@flexion.us>
2024-02-08 15:01:45 -0600

    Remove unused import

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultConverter.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultConverter.java
@@ -1,7 +1,5 @@
 package gov.hhs.cdc.trustedintermediary.etor.results;
 
-import gov.hhs.cdc.trustedintermediary.etor.orders.Order;
-
 public interface ResultConverter {
 	Result<?> addEtorProcessingTag(Result<?> message);
 }
