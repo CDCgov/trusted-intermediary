0335f6f314ceb21fadd7698f7c54c0983496567c
saquino0827 <saquino@flexion.us>
2024-02-09 15:28:25 -0600

    Add test coverage

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiMessageConverterHelperTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiMessageConverterHelperTest.groovy
@@ -3,7 +3,9 @@ package gov.hhs.cdc.trustedintermediary.external.hapi
 import gov.hhs.cdc.trustedintermediary.ResultMock
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Coding
 import org.hl7.fhir.r4.model.MessageHeader
+import org.hl7.fhir.r4.model.Meta
 import org.hl7.fhir.r4.model.Patient
 import spock.lang.Specification
 
@@ -62,5 +64,39 @@ class HapiMessageConverterHelperTest extends Specification {
         actualMessageTag.getSystem() == expectedSystem
         actualMessageTag.getCode() == expectedCode
         actualMessageTag.getDisplay() == expectedDisplay
+    }
+
+    def "addEtorTag adds the ETOR message header tag to any Bundle with existing Meta tag"() {
+        given:
+        def firstExpectedSystem = "a system"
+        def firstExpectedCode = "test"
+        def firstExpectedDisplay = "Processed by tests"
+        def secondExpectedSystem = "http://localcodes.org/ETOR"
+        def secondExpectedCode = "ETOR"
+        def secondExpectedDisplay = "Processed by ETOR"
+
+        def messageHeader = new MessageHeader()
+        messageHeader.setId(UUID.randomUUID().toString())
+        def meta = new Meta()
+        meta.addTag(new Coding(firstExpectedSystem, firstExpectedCode, firstExpectedDisplay))
+        messageHeader.setMeta(meta)
+        def messageHeaderEntry = new Bundle.BundleEntryComponent().setResource(messageHeader)
+        mockBundle.getEntry().add(messageHeaderEntry)
+
+        when:
+        HapiMessageConverterHelper.getInstance().addEtorTagToBundle(mockBundle) as Bundle
+
+        then:
+        def messageHeaders = mockBundle.getEntry().get(1).getResource() as MessageHeader
+        def firstActualMessageTag = messageHeaders.getMeta().getTag()[0]
+        def secondActualMessageTag = messageHeaders.getMeta().getTag()[1]
+
+        firstActualMessageTag.getSystem() == firstExpectedSystem
+        firstActualMessageTag.getCode() == firstExpectedCode
+        firstActualMessageTag.getDisplay() == firstExpectedDisplay
+
+        secondActualMessageTag.getSystem() == secondExpectedSystem
+        secondActualMessageTag.getCode() == secondExpectedCode
+        secondActualMessageTag.getDisplay() == secondExpectedDisplay
     }
 }
