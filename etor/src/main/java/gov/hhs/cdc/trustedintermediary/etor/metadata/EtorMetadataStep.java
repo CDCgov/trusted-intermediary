267189eccbb23eb1d4f8ee26d25a2047b609c40b
jorge Lopez <jlopez@flexion.us>
2024-02-09 14:41:00 -0800

    metadataStep enum refactoring

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/metadata/EtorMetadataStep.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/metadata/EtorMetadataStep.java
@@ -4,7 +4,7 @@ package gov.hhs.cdc.trustedintermediary.etor.metadata;
 public enum EtorMetadataStep {
     CONTACT_SECTION_ADDED_TO_PATIENT,
     ETOR_PROCESSING_TAG_ADDED_TO_MESSAGE_HEADER,
+    ORDER_CONVERTED_TO_OML,
     RECEIVED_FROM_REPORT_STREAM,
-    SENT_TO_REPORT_STREAM,
-    ORDER_CONVERTED_TO_OML
+    SENT_TO_REPORT_STREAM
 }
