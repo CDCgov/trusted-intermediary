e8595725197d9e6b61b4e86401676e3ef6c4871d
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-12 15:18:59 -0800

    Initial implementation for convertAndSend

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/SendResultUseCase.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/SendResultUseCase.java
@@ -2,6 +2,8 @@ package gov.hhs.cdc.trustedintermediary.etor.results;
 
 import gov.hhs.cdc.trustedintermediary.etor.messages.SendMessageUseCase;
 import gov.hhs.cdc.trustedintermediary.etor.messages.UnableToSendMessageException;
+import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
 import javax.inject.Inject;
 
 /** Use case for converting and sending a lab result message. */
@@ -9,8 +11,8 @@ public class SendResultUseCase implements SendMessageUseCase<Result<?>> {
     private static final SendResultUseCase INSTANCE = new SendResultUseCase();
 
     @Inject ResultConverter converter;
-
     @Inject ResultSender sender;
+    @Inject MetricMetadata metadata;
 
     private SendResultUseCase() {}
 
@@ -22,11 +24,14 @@ public class SendResultUseCase implements SendMessageUseCase<Result<?>> {
     public void convertAndSend(Result<?> result) throws UnableToSendMessageException {
 
         // savePartnerMetadataForReceivedResult(receivedSubmissionId, result)
+
         var convertedResult = converter.addEtorProcessingTag(result);
-        // metadata.put(result.getFhirResourceId(),
-        // EtorMetadataStep.ETOR_PROCESSING_TAG_ADDED_TO_MESSAGE_HEADER);
-        sender.send(convertedResult); // var bundleId = sender.send(convertedResult).orElse(null),
-        // thoughts?
+        metadata.put(
+                result.getFhirResourceId(),
+                EtorMetadataStep.ETOR_PROCESSING_TAG_ADDED_TO_MESSAGE_HEADER);
+
+        String sentSubmissionId = sender.send(convertedResult).orElse(null);
+
         // saveSentResultSubmissionId(receivedSubmissionId, bundleId)
     }
 }
