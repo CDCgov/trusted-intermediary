8533fb8133e8ca816b7ba5309936fe67080f2d4e
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-12 09:22:01 -0800

    Implementation to send the FHIR result to RS Waters endpoint (#859)
    * Refactored to extract common code into helper function and implemented send method for results
    
    * Added methods for results and order sender
    
    * Fixed typo
    
    * Updated tests to reflect new changes
    
    * Moved ReportStreamSenderHelper resgistration
    
    * Added java docs
    
    * Added test coverage
    
    * Improved assertions for test

---
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamSenderHelper.java
@@ -0,0 +1,78 @@
+package gov.hhs.cdc.trustedintermediary.external.reportstream;
+
+import gov.hhs.cdc.trustedintermediary.etor.RSEndpointClient;
+import gov.hhs.cdc.trustedintermediary.etor.messages.UnableToSendMessageException;
+import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter;
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.FormatterProcessingException;
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.TypeReference;
+import java.util.Map;
+import java.util.Optional;
+import javax.inject.Inject;
+
+/** Helper class for sending messages to ReportStream */
+public class ReportStreamSenderHelper {
+    private static final ReportStreamSenderHelper INSTANCE = new ReportStreamSenderHelper();
+
+    @Inject RSEndpointClient rsclient;
+    @Inject Formatter formatter;
+    @Inject Logger logger;
+    @Inject MetricMetadata metadata;
+
+    private ReportStreamSenderHelper() {}
+
+    public static ReportStreamSenderHelper getInstance() {
+        return INSTANCE;
+    }
+
+    public Optional<String> sendOrderToReportStream(String body, String fhirResourceId)
+            throws UnableToSendMessageException {
+        return sendToReportStream(body, fhirResourceId, "order");
+    }
+
+    public Optional<String> sendResultToReportStream(String body, String fhirResourceId)
+            throws UnableToSendMessageException {
+        return sendToReportStream(body, fhirResourceId, "result");
+    }
+
+    protected Optional<String> sendToReportStream(
+            String body, String fhirResourceId, String messageType)
+            throws UnableToSendMessageException {
+        String bearerToken;
+        String rsResponseBody;
+
+        try {
+            bearerToken = rsclient.getRsToken();
+            rsResponseBody = rsclient.requestWatersEndpoint(body, bearerToken);
+        } catch (ReportStreamEndpointClientException e) {
+            throw new UnableToSendMessageException(
+                    "Unable to send " + messageType + " to ReportStream", e);
+        }
+
+        logger.logInfo("{} successfully sent to ReportStream", messageType);
+        metadata.put(fhirResourceId, EtorMetadataStep.SENT_TO_REPORT_STREAM);
+
+        Optional<String> sentSubmissionId = getSubmissionId(rsResponseBody);
+        if (sentSubmissionId.isEmpty()) {
+            logger.logError("Unable to retrieve sentSubmissionId from ReportStream response");
+        } else {
+            logger.logInfo("ReportStream response's sentSubmissionId={}", sentSubmissionId);
+        }
+
+        return sentSubmissionId;
+    }
+
+    protected Optional<String> getSubmissionId(String rsResponseBody) {
+        try {
+            Map<String, Object> rsResponse =
+                    formatter.convertJsonToObject(rsResponseBody, new TypeReference<>() {});
+            return Optional.ofNullable(rsResponse.get("submissionId").toString());
+        } catch (FormatterProcessingException e) {
+            logger.logError("Unable to get the submissionId", e);
+        }
+
+        return Optional.empty();
+    }
+}
---
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/reportstream/ReportStreamSenderHelperTest.groovy
@@ -0,0 +1,137 @@
+package gov.hhs.cdc.trustedintermediary.external.reportstream
+
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.etor.RSEndpointClient
+import gov.hhs.cdc.trustedintermediary.etor.messages.UnableToSendMessageException
+import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep
+import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
+import gov.hhs.cdc.trustedintermediary.external.localfile.MockRSEndpointClient
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.FormatterProcessingException
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.TypeReference
+import spock.lang.Specification
+
+class ReportStreamSenderHelperTest extends Specification {
+
+    def setup() {
+        TestApplicationContext.reset()
+        TestApplicationContext.init()
+        TestApplicationContext.register(ReportStreamSenderHelper, ReportStreamSenderHelper.getInstance())
+        TestApplicationContext.register(MetricMetadata, Mock(MetricMetadata))
+    }
+
+    def "sendToReportStream works"() {
+        given:
+        def requestBody = "testBody"
+        def bearerToken = "fake-token"
+        def responseBody = """{"submissionId": "fake-id"}"""
+
+        def mockFormatter = Mock(Formatter)
+        TestApplicationContext.register(Formatter, mockFormatter)
+
+        def mockRsClient = Mock(RSEndpointClient)
+        TestApplicationContext.register(RSEndpointClient, mockRsClient)
+
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        ReportStreamSenderHelper.getInstance().sendToReportStream(requestBody, _ as String, _ as String)
+
+        then:
+        1 * mockRsClient.getRsToken() >> "fake-token"
+        1 * mockRsClient.requestWatersEndpoint(requestBody, bearerToken) >> responseBody
+        1 * mockFormatter.convertJsonToObject(responseBody, _ as TypeReference) >> [submissionId: "fake-id"]
+        1 * ReportStreamSenderHelper.getInstance().metadata.put(_, EtorMetadataStep.SENT_TO_REPORT_STREAM)
+    }
+
+    def "sendOrderToReportStream works"() {
+        setup:
+        def body = "testBody"
+        def fhirResourceId = "testId"
+        def expected = Optional.of("result")
+
+        def senderHelper = Spy(ReportStreamSenderHelper.getInstance())
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        def order = senderHelper.sendOrderToReportStream(body, fhirResourceId)
+
+        then:
+        order.get() == expected.get()
+        1 * senderHelper.sendToReportStream(body, fhirResourceId, "order") >> expected
+    }
+
+    def "sendResultToReportStream works"() {
+        setup:
+        def body = "testBody"
+        def fhirResourceId = "testId"
+        def expected = Optional.of("result")
+
+        def senderHelper = Spy(ReportStreamSenderHelper.getInstance())
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        def result = senderHelper.sendResultToReportStream(body, fhirResourceId)
+
+        then:
+        result.get() == expected.get()
+        1 * senderHelper.sendToReportStream(body, fhirResourceId, "result") >> expected
+    }
+
+    def "sendToReportStream throws exception if RS client fails"() {
+        setup:
+        def mockEndpointClient = Mock(RSEndpointClient)
+        mockEndpointClient.getRsToken() >> { throw new ReportStreamEndpointClientException("couldn't get token", new Exception())}
+        TestApplicationContext.register(RSEndpointClient, mockEndpointClient)
+
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        ReportStreamSenderHelper.getInstance().sendToReportStream("testBody", "testId", "testType")
+
+        then:
+        thrown(UnableToSendMessageException)
+    }
+
+    def "getSubmissionId logs submissionId if convertJsonToObject is successful"() {
+        given:
+        def mockSubmissionId = "fake-id"
+        def mockResponseBody = """{"submissionId": "${mockSubmissionId}", "key": "value"}"""
+
+        TestApplicationContext.register(Formatter, Jackson.getInstance())
+
+        def mockLogger = Mock(Logger)
+        TestApplicationContext.register(Logger, mockLogger)
+
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        def submissionId = ReportStreamSenderHelper.getInstance().getSubmissionId(mockResponseBody)
+
+        then:
+        submissionId.get() == mockSubmissionId
+    }
+
+    def "getSubmissionId logs error if convertJsonToObject fails"() {
+        given:
+        def mockResponseBody = '{"submissionId": "fake-id", "key": "value"}'
+        def exception = new FormatterProcessingException("couldn't convert json", new Exception())
+
+        def mockFormatter = Mock(Formatter)
+        mockFormatter.convertJsonToObject(_ as String, _ as TypeReference) >> { throw exception }
+        TestApplicationContext.register(Formatter, mockFormatter)
+
+        def mockLogger = Mock(Logger)
+        TestApplicationContext.register(Logger, mockLogger)
+
+        TestApplicationContext.injectRegisteredImplementations()
+
+        when:
+        ReportStreamSenderHelper.getInstance().getSubmissionId(mockResponseBody)
+
+        then:
+        1 * mockLogger.logError(_ as String, exception)
+    }
+}
