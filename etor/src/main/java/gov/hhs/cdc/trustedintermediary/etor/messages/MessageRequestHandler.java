7a744aabada4fef4eb088050f9cc5a20f8ecba09
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-13 12:27:20 -0800

    Added java docs

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/MessageRequestHandler.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/MessageRequestHandler.java
@@ -2,7 +2,20 @@ package gov.hhs.cdc.trustedintermediary.etor.messages;
 
 import gov.hhs.cdc.trustedintermediary.wrappers.FhirParseException;
 
+/**
+ * Functional interface for handling message requests.
+ *
+ * @param <T> the type of the response
+ */
 @FunctionalInterface
 public interface MessageRequestHandler<T> {
+    /**
+     * Parses the request, converts and sends the message
+     *
+     * @param receivedSubmissionId the ID for the submission returned from ReportStream
+     * @return the response
+     * @throws FhirParseException if there is an error parsing the FHIR data
+     * @throws UnableToSendMessageException if there is an error sending the message
+     */
     T handle(String receivedSubmissionId) throws FhirParseException, UnableToSendMessageException;
 }
