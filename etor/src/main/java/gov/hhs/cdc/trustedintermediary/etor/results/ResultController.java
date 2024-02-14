c366be88aef68b8a9d786f55ddccac84a2c2a159
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-14 09:45:29 -0800

    Add etor tag only if it doesn't exist

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultController.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultController.java
@@ -1,10 +1,12 @@
 package gov.hhs.cdc.trustedintermediary.etor.results;
 
 import gov.hhs.cdc.trustedintermediary.domainconnector.DomainRequest;
+import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiResult;
 import gov.hhs.cdc.trustedintermediary.wrappers.FhirParseException;
 import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir;
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
 import javax.inject.Inject;
 import org.hl7.fhir.r4.model.Bundle;
 
@@ -13,6 +15,7 @@ public class ResultController {
     private static final ResultController INSTANCE = new ResultController();
     @Inject HapiFhir fhir;
     @Inject Logger logger;
+    @Inject MetricMetadata metadata;
 
     private ResultController() {}
 
@@ -23,7 +26,7 @@ public class ResultController {
     public Result<?> parseResults(DomainRequest request) throws FhirParseException {
         logger.logInfo("Parsing results");
         var fhirBundle = fhir.parseResource(request.getBody(), Bundle.class);
-        // ETOR Results metadata
+        metadata.put(fhirBundle.getId(), EtorMetadataStep.RECEIVED_FROM_REPORT_STREAM);
         return new HapiResult(fhirBundle);
     }
 }
