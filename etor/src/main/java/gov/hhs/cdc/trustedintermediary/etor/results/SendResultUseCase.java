baf95ec32dab61192db64236f0b0e8c1cda0a112
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-14 08:01:33 -0800

    Refactoring and convertAndSend implementation (#868)
    * Refactored to reuse code fron handleOrders in handleResults
    
    * Initial implementation for convertAndSend
    
    * Added flag to determine if should handle partner metadata
    
    * Fixed test
    
    * Added logging
    
    * Removed unused parenthesis
    
    * Refactored tests and added test coverage
    
    * Added java docs
    
    * Fixed typo

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/SendResultUseCase.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/SendResultUseCase.java
@@ -2,6 +2,9 @@ package gov.hhs.cdc.trustedintermediary.etor.results;
 
 import gov.hhs.cdc.trustedintermediary.etor.messages.SendMessageUseCase;
 import gov.hhs.cdc.trustedintermediary.etor.messages.UnableToSendMessageException;
+import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
 import javax.inject.Inject;
 
 /** Use case for converting and sending a lab result message. */
@@ -9,8 +12,9 @@ public class SendResultUseCase implements SendMessageUseCase<Result<?>> {
     private static final SendResultUseCase INSTANCE = new SendResultUseCase();
 
     @Inject ResultConverter converter;
-
     @Inject ResultSender sender;
+    @Inject MetricMetadata metadata;
+    @Inject Logger logger;
 
     private SendResultUseCase() {}
 
@@ -21,12 +25,18 @@ public class SendResultUseCase implements SendMessageUseCase<Result<?>> {
     @Override
     public void convertAndSend(Result<?> result) throws UnableToSendMessageException {
 
+        // todo: for story #650 (results metadata)
         // savePartnerMetadataForReceivedResult(receivedSubmissionId, result)
+
         var convertedResult = converter.addEtorProcessingTag(result);
-        // metadata.put(result.getFhirResourceId(),
-        // EtorMetadataStep.ETOR_PROCESSING_TAG_ADDED_TO_MESSAGE_HEADER);
-        sender.send(convertedResult); // var bundleId = sender.send(convertedResult).orElse(null),
-        // thoughts?
+        metadata.put(
+                result.getFhirResourceId(),
+                EtorMetadataStep.ETOR_PROCESSING_TAG_ADDED_TO_MESSAGE_HEADER);
+
+        String sentSubmissionId = sender.send(convertedResult).orElse(null);
+        logger.logInfo("Sent result submissionId: {}", sentSubmissionId);
+
+        // todo: for story #650 (results metadata)
         // saveSentResultSubmissionId(receivedSubmissionId, bundleId)
     }
 }
