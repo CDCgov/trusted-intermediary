d3e0d07796cd9d1e4dd728066fd0d4941fc126df
jorge Lopez <jlopez@flexion.us>
2024-02-14 17:35:35 -0800

    comment options for gradle

--- generate_env.sh
+++ generate_env.sh
@@ -3,13 +3,14 @@
 # Define the content of the .env file
 
 
+
 env_content="ENV=local
 KEY_VAULT-NAME=ti-key-vault-name
 REPORT_STREAM_URL_PREFIX=http://localhost:7071
 STORAGE_ACCOUNT_BLOB_ENDPOINT=https://cdctiinternal.blob.core.windows.net
 METADATA_CONTAINER_NAME=metadata
-DB_URL=localhost
-DB_PORT=5433
+DB_URL=postgresql # for gradlew: DB_URL=localhost
+DB_PORT=5432      # for gradlew: DB_PORT=5433
 DB_NAME=intermediary
 DB_USER=intermediary
 DB_PASS=changeIT!
