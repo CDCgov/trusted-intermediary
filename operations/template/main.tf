e20c5727cd4ee24c1c8015e0dc891e6a075ac691
Jeff Crichlake <jcrichlake@gmail.com>
2024-02-16 17:14:28 -0500

    Fixing names 2

--- operations/template/main.tf
+++ operations/template/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_private_dns_zone" "dns_zone" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "db_network_link" {
   name                  = "intermediarylink.com"
-  private_dns_zone_name = azurerm_private_dns_zone.dns_zone
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
   virtual_network_id    = data.azurerm_virtual_network.app_vnet.id
   resource_group_name   = data.azurerm_resource_group.group.name
   depends_on            = [data.azurerm_subnet.db_subnet]
