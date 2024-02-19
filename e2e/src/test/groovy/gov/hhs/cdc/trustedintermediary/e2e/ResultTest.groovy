ad0b663b8814a4e8e66cad2abb634b2a52c4fcd4
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-02-19 14:30:28 -0800

    Update ORU sample files (#884)
    * Updated 004_MN_ORU_R01_NBS final hl7
    
    * Renamed files
    
    * Updated transformed fhir messages
    
    * Updated file name in test
    
    * Fixed test
    
    * Increased max request size
    
    * Updated file path
    
    * Extract hardcoded values
    
    * Updated readme with naming convention

--- e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/ResultTest.groovy
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/ResultTest.groovy
@@ -8,7 +8,7 @@ import java.nio.file.Path
 class ResultTest extends Specification {
     def resultClient = new EndpointClient("/v1/etor/results")
 
-    def labResultJsonFileString = Files.readString(Path.of("../examples/MN/004_MN_ORU_R01_NBS_1_translation_from_initial_hl7_ingestion.fhir"))
+    def labResultJsonFileString = Files.readString(Path.of("../examples/MN/004_MN_ORU_R01_NBS_1_hl7_translation.fhir"))
 
     def submissionId = "submissionId"
 
@@ -18,7 +18,7 @@ class ResultTest extends Specification {
 
     def "a result response is returned from the ETOR order endpoint"() {
         given:
-        def expectedFhirResourceId  = "Bundle/1705511861639940150.e2c69100-24af-4bbd-86bf-2f29be816edf"
+        def expectedJsonBody = JsonParsing.parse(labResultJsonFileString)
 
         when:
         def response = resultClient.submit(labResultJsonFileString, submissionId, true)
@@ -26,7 +26,7 @@ class ResultTest extends Specification {
 
         then:
         response.getCode() == 200
-        parsedJsonBody.fhirResourceId == expectedFhirResourceId
+        parsedJsonBody.fhirResourceId.contains(expectedJsonBody.id)
     }
 
     def "check that the rest of the message is unchanged except the parts we changed"() {
