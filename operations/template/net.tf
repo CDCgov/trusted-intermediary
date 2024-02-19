555c002485f34677c674063df2d5ca3fc2fc4ba8
halprin <halprin@users.noreply.github.com>
2024-02-19 12:52:07 -0700

    Try creating some subnets

--- operations/template/net.tf
+++ operations/template/net.tf
@@ -4,27 +4,50 @@
 #  resource_group_name = azurerm_resource_group.example.name
 #}
 
-data "azurerm_virtual_network" "db_vnet" {
-  name                = "csels-rsti-${var.environment}-moderate-db-vnet"
-  resource_group_name = data.azurerm_resource_group.group.name
-}
+#data "azurerm_virtual_network" "db_vnet" {
+#  name                = "csels-rsti-${var.environment}-moderate-db-vnet"
+#  resource_group_name = data.azurerm_resource_group.group.name
+#}
 
-data "azurerm_virtual_network" "app_vnet" {
+data "azurerm_virtual_network" "app" {
   name                = "csels-rsti-${var.environment}-moderate-app-vnet"
   resource_group_name = data.azurerm_resource_group.group.name
 }
 
-data "azurerm_subnet" "db_subnet" {
-  name                 = data.azurerm_virtual_network.db_vnet.subnets[0]
-  virtual_network_name = data.azurerm_virtual_network.db_vnet.name
+resource "azurerm_subnet" "app" {
+  name                 = "app"
   resource_group_name  = data.azurerm_resource_group.group.name
+  virtual_network_name = data.azurerm_virtual_network.app.name
+  address_prefixes     = ["172.17.69.128/26"]
 }
 
-data "azurerm_subnet" "app_subnet" {
-  name                 = data.azurerm_virtual_network.app_vnet.subnets[0]
-  virtual_network_name = data.azurerm_virtual_network.app_vnet.name
+resource "azurerm_subnet" "database" {
+  name                 = "database"
   resource_group_name  = data.azurerm_resource_group.group.name
+  virtual_network_name = data.azurerm_virtual_network.app.name
+  address_prefixes     = ["172.17.69.192/27"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
+
+#data "azurerm_subnet" "db_subnet" {
+#  name                 = data.azurerm_virtual_network.db_vnet.subnets[0]
+#  virtual_network_name = data.azurerm_virtual_network.db_vnet.name
+#  resource_group_name  = data.azurerm_resource_group.group.name
+#}
+#
+#data "azurerm_subnet" "app_subnet" {
+#  name                 = data.azurerm_virtual_network.app_vnet.subnets[0]
+#  virtual_network_name = data.azurerm_virtual_network.app_vnet.name
+#  resource_group_name  = data.azurerm_resource_group.group.name
+#}
 
 resource "azurerm_private_dns_zone" "dns_zone" {
   name                = "privateintermediary.postgres.database.azure.com"
@@ -34,7 +57,7 @@ resource "azurerm_private_dns_zone" "dns_zone" {
 resource "azurerm_private_dns_zone_virtual_network_link" "db_network_link" {
   name                  = "intermediarylink.com"
   private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
-  virtual_network_id    = data.azurerm_virtual_network.app_vnet.id
+  virtual_network_id    = data.azurerm_virtual_network.app.id
   resource_group_name   = data.azurerm_resource_group.group.name
-  depends_on            = [data.azurerm_subnet.db_subnet]
+  depends_on            = [azurerm_subnet.database]
 }
