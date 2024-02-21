ad1997e81be315acc9e18b7c75dcb5999ab51e4b
Jeff Crichlake <jcrichlake@gmail.com>
2024-02-21 16:21:23 -0500

    Adding security group and rules

--- operations/template/net.tf
+++ operations/template/net.tf
@@ -29,8 +29,7 @@ resource "azurerm_subnet" "app" {
       name    = "Microsoft.Web/serverFarms"
       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
     }
-  }
-}
+  }}
 
 resource "azurerm_subnet" "database" {
   name                 = "database"
@@ -45,8 +44,7 @@ resource "azurerm_subnet" "database" {
       name    = "Microsoft.DBforPostgreSQL/flexibleServers"
       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
     }
-  }
-}
+  }}
 
 
 #data "azurerm_subnet" "db_subnet" {
@@ -72,4 +70,130 @@ resource "azurerm_private_dns_zone_virtual_network_link" "db_network_link" {
   virtual_network_id    = data.azurerm_virtual_network.app.id
   resource_group_name   = data.azurerm_resource_group.group.name
   depends_on            = [azurerm_subnet.database]
+}
+
+resource "azurerm_network_security_group" "db_security_group" {
+  name                = "database-security-group"
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+}
+
+
+resource "azurerm_network_security_rule" "Splunk_UF_omhsinf" {
+  name                       = "Splunk_UF_omhsinf"
+  priority                   = 103
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "9997-9998"
+  source_address_prefixes    = ["10.65.8.211/32","10.65.8.212/32","10.65.7.212/32","10.65.7.211/32","10.65.8.210/32","10.65.7.210/32"]
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_network_security_rule" "Splunk_Indexer_Discovery_omhsinf" {
+  name                       = "Splunk_Indexer_Discovery_omhsinf"
+  priority                   = 104
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "8089"
+  source_address_prefix      = "10.11.7.22/32"
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+
+resource "azurerm_network_security_rule" "Safe_Encase_Monitoring_omhsinf" {
+  name                       = "Safe_Encase_Monitoring_omhsinf"
+  priority                   = 105
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "34445"
+  source_address_prefix      = "10.11.6.145/32"
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_network_security_rule" "ForeScout_Manager_omhsinf" {
+  name                       = "ForeScout_Manager_omhsinf"
+  priority                   = 106
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_ranges    = ["556","443","10003-10006"]
+  source_address_prefixes    = ["10.64.8.184","10.64.8.180/32"]
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_network_security_rule" "ForeScout_Manager_omhsinf" {
+  name                       = "BigFix_omhsinf"
+  priority                   = 107
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "52314"
+  source_address_prefix      = "10.11.4.84/32"
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+
+resource "azurerm_network_security_rule" "Allow_All_Out_omhsinf" {
+  name                       = "Allow_All_Out_omhsinf"
+  priority                   = 109
+  direction                  = "Outbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_ranges     = ["5432"]
+  source_address_prefixes    = ["VirtualNetwork"]
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_network_security_rule" "db_outbound_allow" {
+  name                       = "db_outbound_allow"
+  priority                   = 110
+  direction                  = "Outbound"
+  access                     = "Allow"
+  protocol                   = "*"
+  source_port_range          = "*"
+  destination_port_ranges     = ["*"]
+  source_address_prefixes    = ["*"]
+  destination_address_prefix = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_network_security_rule" "db_inbound_allow" {
+  name                       = "db_inbound_allow"
+  priority                   = 111
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_ranges     = ["5432"]
+  source_address_prefixes    = ["VirtualNetwork"]
+  destination_address_prefix = "VirtualNetwork"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.db_security_group.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "database_security_group" {
+  subnet_id                 = azurerm_subnet.database.id
+  network_security_group_id = azurerm_network_security_group.db_security_group.id
 }
