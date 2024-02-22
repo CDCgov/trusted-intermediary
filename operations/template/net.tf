dd340be2095b0a7354f8053b663b5b6bbededc06
Jeff Crichlake <jcrichlake@gmail.com>
2024-02-22 13:39:09 -0500

    Adding app security group and rules

--- operations/template/net.tf
+++ operations/template/net.tf
@@ -199,3 +199,129 @@ resource "azurerm_subnet_network_security_group_association" "database_security_
   subnet_id                 = azurerm_subnet.database.id
   network_security_group_id = azurerm_network_security_group.db_security_group.id
 }
+
+resource "azurerm_network_security_group" "app_security_group" {
+  name                = "app-security-group"
+  location            = data.azurerm_resource_group.group.location
+  resource_group_name = data.azurerm_resource_group.group.name
+}
+
+
+resource "azurerm_network_security_rule" "Splunk_UF_omhsinf" {
+  name                        = "Splunk_UF_omhsinf"
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "9997-9998"
+  source_address_prefixes     = ["10.65.8.211/32","10.65.8.212/32","10.65.7.212/32","10.65.7.211/32","10.65.8.210/32","10.65.7.210/32"]
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_network_security_rule" "Splunk_Indexer_Discovery_omhsinf" {
+  name                        = "Splunk_Indexer_Discovery_omhsinf"
+  priority                    = 104
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8089"
+  source_address_prefix       = "10.11.7.22/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+
+resource "azurerm_network_security_rule" "Safe_Encase_Monitoring_omhsinf" {
+  name                        = "Safe_Encase_Monitoring_omhsinf"
+  priority                    = 105
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "34445"
+  source_address_prefix       = "10.11.6.145/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_network_security_rule" "ForeScout_Manager_omhsinf" {
+  name                        = "ForeScout_Manager_omhsinf"
+  priority                    = 106
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["556","443","10003-10006"]
+  source_address_prefixes     = ["10.64.8.184","10.64.8.180/32"]
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_network_security_rule" "BigFix_omhsinf" {
+  name                        = "BigFix_omhsinf"
+  priority                    = 107
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "52314"
+  source_address_prefix       = "10.11.4.84/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+
+resource "azurerm_network_security_rule" "Allow_All_Out_omhsinf" {
+  name                        = "Allow_All_Out_omhsinf"
+  priority                    = 109
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["80","443"]
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_network_security_rule" "app_outbound_allow" {
+  name                        = "app_outbound_allow"
+  priority                    = 110
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_network_security_rule" "app_inbound_allow" {
+  name                        = "app_inbound_allow"
+  priority                    = 111
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["80","443"]
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = data.azurerm_resource_group.group.name
+  network_security_group_name = azurerm_network_security_group.app_security_group.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "app_security_group" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app_security_group.id
+}
