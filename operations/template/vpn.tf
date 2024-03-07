558c31ddffeab61d4a19f4218fd90e8bde086840
halprin <halprin@users.noreply.github.com>
2024-03-06 17:14:22 -0700

    Put the configuration back on the VPN

--- operations/template/vpn.tf
+++ operations/template/vpn.tf
@@ -30,4 +30,15 @@ resource "azurerm_virtual_network_gateway" "vpn" {
     private_ip_address_allocation = "Dynamic"
     subnet_id                     = azurerm_subnet.vpn.id
   }
+
+  vpn_client_configuration {
+    address_space = ["172.17.69.0/29"]
+    vpn_auth_types = ["Certificate"]
+    vpn_client_protocols = ["OpenVPN"]
+
+    root_certificate {
+      name = "jeff-test-cert"
+      public_cert_data = "MIIC5jCCAc6gAwIBAgIIGMy2CjfbdWEwDQYJKoZIhvcNAQELBQAwETEPMA0GA1UEAxMGVlBOIENBMB4XDTI0MDMwNTIxMTIzOVoXDTI3MDMwNTIxMTIzOVowETEPMA0GA1UEAxMGVlBOIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnF+rF0RxT8mFwVwQY976uEyR1dr6bC7G+18fJuk8yEG+vVhVrCldSCMNL4QkeZNvBNW4/W2DsuGVKFwc8u3iII/uSQ7ANU1EsFve0GdlSQv8gHYAWwKaR2Rt20uaFMBkeWIUScrMtesd+AvBk5h2Ll6opNR4SGOZSkH6lGl8KMWbUuQbiME4RIF07bNMF+fNHRXxsUMM6OWRzDS8VZaEAz8iuKr9qGi4hIB2dQlJa8fGgU+J5gt2C33t56VaJsde2/MJtTj9P/8elTeGpfPATMNqCThYK3UsfRe5Jrl/wHlugVhPLqwOKwrvVd+Vv3vTkiPhfIhPu37aoHYGZyMpfQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjAdBgNVHQ4EFgQUOUXvWrL8eFMpB8Mc0GJKcySw56UwDQYJKoZIhvcNAQELBQADggEBAEkXMQ4iQ9pK3DutiwY1ejEShF+O0agMGaLDcKNFtNlc4UwN10RNkkBEPLSZhuBKGkBQIxuNLqsFUaZcL4x47a2VQUBuJhXvIXfNtNupYcRusVyBRSYbcZWpernbXSRutCUfO24tRsMG9m+QBAmJYU6XNDQUi55CwAhygg7mnARdcRZAP7qBVUr/ga59mWVVWWcO5VfKQD2XfBp88AOwkw/C9odX1bIfIEu+A9KWrvVh2eqMDmTb4sEOQcuP70kBU3udckPb51a4R4J/LxuDfWjAXrJGHA/W6srbO8FIkRvsUKHk5CEH487+gMKI9Jt70mG2dwdHTqiqE1VY6z3VDbs="  # pragma: allowlist secret
+    }
+  }
 }
