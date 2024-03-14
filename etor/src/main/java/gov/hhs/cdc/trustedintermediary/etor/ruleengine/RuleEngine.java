fd407b8d5c83ab2fa150553467f6474454353212
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-03-14 14:48:36 -0700

    Refactorred to move rules loading logic

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngine.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngine.java
@@ -1,24 +1,33 @@
 package gov.hhs.cdc.trustedintermediary.etor.ruleengine;
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 public class RuleEngine {
+
+    private final String RULES_CONFIG_FILE_NAME = "rule_definitions.json";
     private static final RuleEngine INSTANCE = new RuleEngine();
     private final List<Rule> rules = new ArrayList<>();
 
-    private RuleEngine() {
+    private RuleEngine() {}
+
+    public static RuleEngine getInstance() {
+        return INSTANCE;
+    }
+
+    public void loadRules() {
+        var fileUrl = getClass().getClassLoader().getResource(RULES_CONFIG_FILE_NAME);
+        if (fileUrl == null) {
+            throw new IllegalArgumentException("File not found: " + RULES_CONFIG_FILE_NAME);
+        }
         try {
-            var loadedRules = RuleLoader.getInstance().loadRules();
+            var loadedRules = RuleLoader.getInstance().loadRules(Paths.get(fileUrl.getPath()));
             rules.addAll(loadedRules);
         } catch (RuleLoaderException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static RuleEngine getInstance() {
-        return INSTANCE;
     }
 
     public void addRule(Rule rule) {
