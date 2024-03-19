88ebf667abbe1cc50c50e808079516c08d62b273
Luis Pabon <turkeyfried@gmail.com>
2024-03-19 18:25:30 -0400

    Update RuleEngineTest.groovy
    added validation warning test case

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngineTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngineTest.groovy
@@ -1,52 +1,46 @@
 package gov.hhs.cdc.trustedintermediary.etor.ruleengine
 
-import ca.uhn.fhir.fhirpath.FhirPathExecutionException
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirImplementation
 import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
-import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter
-import org.hl7.fhir.instance.model.api.IBaseResource
 import org.hl7.fhir.r4.model.Bundle
-import org.hl7.fhir.r4.model.DiagnosticReport
-import org.hl7.fhir.r4.model.ServiceRequest
 import spock.lang.Specification
 
 import java.nio.file.Files
 import java.nio.file.Path
 
 class RuleEngineTest extends Specification {
-    //    Bundle bundle
-    //    DiagnosticReport diaReport
-    //    ServiceRequest servRequest
-    HapiFhir fhir = HapiFhirImplementation.getInstance()
-    RuleEngine engine = RuleEngine.getInstance()
+    def fhir = HapiFhirImplementation.getInstance()
+    def engine = RuleEngine.getInstance()
+    def mockLogger = Mock(Logger)
 
-    String labOrderJsonFileString
+    String fhirBody
 
     def setup() {
         TestApplicationContext.reset()
         TestApplicationContext.init()
 
         TestApplicationContext.register(Formatter, Jackson.getInstance())
-        TestApplicationContext.register(HapiFhirImplementation, HapiFhirImplementation.getInstance())
-        TestApplicationContext.register(RuleEngine, RuleEngine.getInstance())
+        TestApplicationContext.register(HapiFhirImplementation, fhir)
+        TestApplicationContext.register(RuleEngine, engine)
         TestApplicationContext.register(RuleLoader, RuleLoader.getInstance())
+        TestApplicationContext.register(Logger, mockLogger)
 
         TestApplicationContext.injectRegisteredImplementations()
-
-        engine.loadRules()
     }
 
-    def "first test"() {
+    def "validation logs a warning when a validation fails"() {
         given:
-        labOrderJsonFileString = Files.readString(Path.of("../examples/Test/Orders/003_AL_ORM_O01_NBS_Fully_Populated_1_hl7_translation.fhir"))
-        def bundle = fhir.parseResource(labOrderJsonFileString, Bundle)
+        fhirBody = Files.readString(Path.of("../examples/Test/Orders/001_OML_O21_short.fhir"))
+        def bundle = fhir.parseResource(fhirBody, Bundle)
 
         when:
+        engine.loadRules()
         engine.validate(bundle)
 
         then:
-        1 == 1
+        1 * mockLogger.logWarning(_ as String)
     }
 }
