441781656868eb6ef48890b3fa80df66bc5aa7e1
Luis Pabon <turkeyfried@gmail.com>
2024-03-20 12:41:43 -0400

    Update HapiFhirImplementation.java
    Adding doc to HapiFhir implementation

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirImplementation.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirImplementation.java
@@ -37,17 +37,37 @@ public class HapiFhirImplementation implements HapiFhir {
         }
     }
 
+    /**
+     * Encode resource to JSON string.
+     *
+     * @param resource Object to encode into a string.
+     * @return String-encoded resource.
+     */
     @Override
     public String encodeResourceToJson(Object resource) {
         IParser encodeResourceParser = CONTEXT.newJsonParser();
         return encodeResourceParser.encodeResourceToString((IBaseResource) resource);
     }
 
+    /**
+     * Evaluate a FHIR Path expression for a given Resource to find matching elements.
+     *
+     * @param root FHIR resource the evaluation starts from.
+     * @param expression FHIR Path statement to run evaluations on.
+     * @return A list of matching Resources within root for the given expression.
+     */
     @Override
     public List<Base> evaluate(IBaseResource root, String expression) {
         return pathEngine.evaluate(root, expression, Base.class);
     }
 
+    /**
+     * Evaluate a FHIR Path expression for a given Resource to find if the expression has matches
+     *
+     * @param root FHIR resource the evaluation starts from.
+     * @param expression FHIR Path statement to run evaluations on.
+     * @return True if the expression has at least one match for the given root, else false.
+     */
     @Override
     public Boolean evaluateCondition(IBaseResource root, String expression) {
         var result = pathEngine.evaluateFirst(root, expression, BooleanType.class);
