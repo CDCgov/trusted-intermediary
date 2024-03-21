8c62d360d360ca9eb1ad82fdbba43d32f11341f4
James Herr <jherr@flexion.us>
2024-03-21 15:55:26 -0500

    Added e2d test for RESULT

--- e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/MetadataTest.groovy
+++ e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/e2e/MetadataTest.groovy
@@ -53,6 +53,46 @@ class MetadataTest extends Specification {
             assert !issue.diagnostics.isEmpty()
         }
     }
+    def "a metadata response is returned from the ETOR metadata endpoint for results"() {
+        given:
+        def expectedStatusCode = 200
+        def inboundSubmissionId = UUID.randomUUID().toString()
+        def outboundSubmissionId = "1234567890"
+        def resultClient = new EndpointClient("/v1/etor/results")
+        def labResult = Files.readString(Path.of("../examples/Test/ORU_R01.fhir"))
+
+        when:
+        def resultResponse = resultClient.submit(labResult, inboundSubmissionId, true)
+
+        then:
+        resultResponse.getCode() == expectedStatusCode
+
+        when:
+        def inboundMetadataResponse = metadataClient.get(inboundSubmissionId, true)
+        def outboundMetadataResponse = metadataClient.get(outboundSubmissionId, true)
+        def inboundParsedJsonBody = JsonParsing.parseContent(inboundMetadataResponse)
+        def outboundParsedJsonBody = JsonParsing.parseContent(outboundMetadataResponse)
+
+        then:
+        inboundMetadataResponse.getCode() == expectedStatusCode
+        outboundMetadataResponse.getCode() == expectedStatusCode
+        inboundParsedJsonBody.get("id") == inboundSubmissionId
+        outboundParsedJsonBody.get("id") == outboundSubmissionId
+
+        [
+            "sender name",
+            "receiver name",
+            "result ingestion",
+            "payload hash",
+            "delivery status",
+            "status message"
+        ].each { String metadataKey ->
+            def issue = (inboundParsedJsonBody.issue as List).find( {issue -> issue.details.text == metadataKey })
+            assert issue != null
+            assert issue.diagnostics != null
+            assert !issue.diagnostics.isEmpty()
+        }
+    }
 
     def "a 404 is returned when there is no metadata for a given ID"() {
         when:
