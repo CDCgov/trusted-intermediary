5ab17cf1c83f125f4198b7e316def5dac47dc9d7
Luis Pabon <turkeyfried@gmail.com>
2024-03-22 12:57:57 -0400

    Update RuleEngine.java
    Preprend rule violation message for easier searching

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngine.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngine.java
@@ -48,7 +48,7 @@ public class RuleEngine {
         ensureRulesLoaded();
         for (Rule rule : rules) {
             if (rule.appliesTo(resource) && !rule.isValid(resource)) {
-                logger.logWarning(rule.getWarningMessage());
+                logger.logWarning("Rule violation: " + rule.getWarningMessage());
             }
         }
     }
