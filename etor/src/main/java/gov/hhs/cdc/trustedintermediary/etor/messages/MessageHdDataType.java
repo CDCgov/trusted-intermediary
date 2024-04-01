b30ba339c3bcc402c0d01a6c8eec7cd95a140ebd
jorge Lopez <jlopez@flexion.us>
2024-04-01 11:59:06 -0700

    refactor javdocs

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/MessageHdDataType.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/MessageHdDataType.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
  * This class represents the result of evaluating a FHIRPath expression, encapsulating specific
  * details extracted from a FHIR resource. This class holds values for namespace, universal
  * identifier (ID), and the type of the universal ID, providing a mechanism to output these details
- * in a concatenated string format. HD rer: <a
+ * in a concatenated string format. HD reference: <a
  * href="https://hl7-definition.caristix.com/v2/HL7v2.5.1/DataTypes/HD">HD-DataType</a>
  */
 public class MessageHdDataType {
