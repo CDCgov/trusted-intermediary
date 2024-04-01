697f34762ba886769371758d7b35cb9e278e4876
Jeff Crichlake <jcrichlake@gmail.com>
2024-04-01 15:39:00 -0400

    Java doc

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/database/DbColumn.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/database/DbColumn.java
@@ -1,3 +1,4 @@
 package gov.hhs.cdc.trustedintermediary.external.database;
 
+/** Represents a database column for upserting data. */
 public record DbColumn(String name, Object value, boolean upsertOverwrite, int type) {}
