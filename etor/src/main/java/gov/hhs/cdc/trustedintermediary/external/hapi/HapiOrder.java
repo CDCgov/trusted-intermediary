39f8f8aa03033f53ff6e8c7a8d7dba41ac0cd965
saquino0827 <saquino@flexion.us>
2024-04-02 15:05:28 -0500

    Update HapiOrder to use HapiMessageHelper

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiOrder.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiOrder.java
@@ -1,16 +1,11 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi;
 
+import gov.hhs.cdc.trustedintermediary.etor.messages.MessageHdDataType;
 import gov.hhs.cdc.trustedintermediary.etor.orders.Order;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Identifier;
-import org.hl7.fhir.r4.model.MessageHeader;
 import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.ServiceRequest;
 
 /**
  * A concrete implementation of a {@link Order} that uses the Hapi FHIR bundle as its underlying
@@ -54,116 +49,34 @@ public class HapiOrder implements Order<Bundle> {
 
     @Override
     public String getPlacerOrderNumber() {
-        return HapiHelper.resourcesInBundle(innerOrder, ServiceRequest.class)
-                .flatMap(serviceRequest -> serviceRequest.getIdentifier().stream())
-                .filter(
-                        identifier ->
-                                identifier
-                                        .getType()
-                                        .hasCoding(
-                                                "http://terminology.hl7.org/CodeSystem/v2-0203",
-                                                "PLAC"))
-                .map(Identifier::getValue)
-                .findFirst()
-                .orElse("");
+        return HapiMessageHelper.getInstance().extractPlacerOrderNumber(innerOrder);
     }
 
     @Override
     public String getSendingApplicationDetails() {
-        return HapiHelper.resourcesInBundle(innerOrder, MessageHeader.class)
-                .filter(Objects::nonNull)
-                .findFirst()
-                .map(
-                        messageHeader -> {
-                            String namespaceId = null;
-                            String universalId = null;
-                            String universalIdType = null;
-                            String endpoint = messageHeader.getSource().getEndpoint();
-
-                            for (Extension extension : messageHeader.getSource().getExtension()) {
-                                if ("https://reportstream.cdc.gov/fhir/StructureDefinition/namespace-id"
-                                        .equals(extension.getUrl())) {
-                                    namespaceId =
-                                            Objects.toString(
-                                                    extension.getValue().primitiveValue(), "");
-                                } else if ("https://reportstream.cdc.gov/fhir/StructureDefinition/universal-id"
-                                        .equals(extension.getUrl())) {
-                                    universalId =
-                                            Objects.toString(
-                                                    extension.getValue().primitiveValue(), "");
-                                } else if ("https://reportstream.cdc.gov/fhir/StructureDefinition/universal-id-type"
-                                        .equals(extension.getUrl())) {
-                                    universalIdType =
-                                            Objects.toString(
-                                                    extension.getValue().primitiveValue(), "");
-                                }
-                            }
-
-                            return concatenateWithCaret(
-                                    namespaceId, universalId, universalIdType, endpoint);
-                        })
-                .orElse("");
-        // extract into SendingApplicationDetail Object
+        MessageHdDataType sendingApplicationDetails =
+                HapiMessageHelper.getInstance().extractSendingApplicationDetails(innerOrder);
+        return sendingApplicationDetails.toString();
     }
 
     @Override
     public String getSendingFacilityDetails() {
-        return HapiMessageHelper.getInstance().extractSendingFacilityDetails(innerOrder);
+        MessageHdDataType sendingFacilityDetails =
+                HapiMessageHelper.getInstance().extractSendingFacilityDetails(innerOrder);
+        return sendingFacilityDetails.toString();
     }
 
     @Override
     public String getReceivingApplicationDetails() {
-        return HapiHelper.resourcesInBundle(innerOrder, MessageHeader.class)
-                .filter(Objects::nonNull)
-                .findFirst()
-                .flatMap(
-                        messageHeader ->
-                                messageHeader.getDestination().stream()
-                                        .filter(Objects::nonNull)
-                                        .findFirst())
-                .map(
-                        destination -> {
-                            String name = Objects.toString(destination.getName(), "");
-                            String universalId =
-                                    destination.getExtension().stream()
-                                            .filter(
-                                                    ext ->
-                                                            "https://reportstream.cdc.gov/fhir/StructureDefinition/universal-id"
-                                                                    .equals(ext.getUrl()))
-                                            .findFirst()
-                                            .map(
-                                                    ext ->
-                                                            Objects.toString(
-                                                                    ext.getValue().primitiveValue(),
-                                                                    ""))
-                                            .orElse("");
-                            String universalIdType =
-                                    destination.getExtension().stream()
-                                            .filter(
-                                                    ext ->
-                                                            "https://reportstream.cdc.gov/fhir/StructureDefinition/universal-id-type"
-                                                                    .equals(ext.getUrl()))
-                                            .findFirst()
-                                            .map(
-                                                    ext ->
-                                                            Objects.toString(
-                                                                    ext.getValue().primitiveValue(),
-                                                                    ""))
-                                            .orElse("");
-
-                            return concatenateWithCaret(name, universalId, universalIdType);
-                        })
-                .orElse("");
+        MessageHdDataType receivingApplicationDetails =
+                HapiMessageHelper.getInstance().extractReceivingApplicationDetails(innerOrder);
+        return receivingApplicationDetails.toString();
     }
 
     @Override
-    public String getReceivingFacilityId() {
-        return null;
-    }
-
-    private String concatenateWithCaret(String... values) {
-        return Arrays.stream(values)
-                .filter(Objects::nonNull) // Ensure null values are ignored
-                .collect(Collectors.joining("^"));
+    public String getReceivingFacilityDetails() {
+        MessageHdDataType receivingFacilityDetails =
+                HapiMessageHelper.getInstance().extractReceivingFacilityDetails(innerOrder);
+        return receivingFacilityDetails.toString();
     }
 }
