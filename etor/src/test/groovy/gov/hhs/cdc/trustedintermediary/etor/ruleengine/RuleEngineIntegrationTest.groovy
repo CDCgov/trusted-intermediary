79c06f01d4837cceab44d2a25e4973ecfd4ffa90
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-02 14:00:40 -0700

    Updated receiver id fhirpath in tests and added new test to make sure we validate using the receiver id

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngineIntegrationTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/RuleEngineIntegrationTest.groovy
@@ -8,6 +8,10 @@ import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
 import gov.hhs.cdc.trustedintermediary.wrappers.Logger
 import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter
 import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Coding
+import org.hl7.fhir.r4.model.MessageHeader
+import org.hl7.fhir.r4.model.Organization
+import org.hl7.fhir.r4.model.Reference
 import spock.lang.Specification
 
 import java.nio.file.Files
@@ -80,7 +84,7 @@ class RuleEngineIntegrationTest extends Specification {
         where:
         testFile | validation
         "Orders/001_OML_O21_short.fhir"                                    | "Bundle.entry.resource.ofType(MessageHeader).focus.resolve().category.exists()"
-        "Orders/003_AL_ORM_O01_NBS_Fully_Populated_1_hl7_translation.fhir" | "Bundle.entry.resource.ofType(MessageHeader).destination.receiver.resolve().identifier.value.exists()"
+        "Orders/003_AL_ORM_O01_NBS_Fully_Populated_1_hl7_translation.fhir" | "Bundle.entry.resource.ofType(MessageHeader).destination.receiver.resolve().identifier.where(system = 'urn:ietf:rfc:3986').value.exists()"
         // Once we fix the mapping for ORM from story #900 and update the FHIR files in /examples/Test/Orders, we can uncomment the below line
         // "Orders/003_AL_ORM_O01_NBS_Fully_Populated_1_hl7_translation.fhir" | "Bundle.entry.resource.ofType(Observation).where(code.coding.code = '57723-9').value.coding.code.exists()"
     }
@@ -95,8 +99,76 @@ class RuleEngineIntegrationTest extends Specification {
 
         where:
         testFile | validation
-        "Orders/001_OML_O21_short.fhir" | "Bundle.entry.resource.ofType(MessageHeader).destination.receiver.resolve().identifier.value.exists()"
+        "Orders/001_OML_O21_short.fhir" | "Bundle.entry.resource.ofType(MessageHeader).destination.receiver.resolve().identifier.where(system = 'urn:ietf:rfc:3986').value.exists()"
         "Orders/001_OML_O21_short.fhir" | "Bundle.entry.resource.ofType(Observation).where(code.coding.code = '57723-9').value.coding.code.exists()"
+    }
+
+    def "validation passes: Message has required receiver id"() {
+        given:
+        def fhirPathValidation = "Bundle.entry.resource.ofType(MessageHeader).destination.receiver.resolve().identifier.where(system = 'urn:ietf:rfc:3986').value.exists()"
+        def rule = createValidationRule([], [fhirPathValidation])
+
+        when:
+        Organization receiverOrganization = new Organization()
+        receiverOrganization.setId(UUID.randomUUID().toString())
+        receiverOrganization
+                .addIdentifier()
+                .setSystem("urn:ietf:rfc:3986")
+                .setValue("simulated-hospital-id")
+        def bundle = createMessageBundle(receiverOrganization: receiverOrganization)
+        // for some reason, we need to encode and decode the bundle for resolve() to work
+        def fhirResource = new HapiFhirResource(fhir.parseResource(fhir.encodeResourceToJson(bundle), Bundle))
+
+        then:
+        rule.isValid(fhirResource)
+
+        when:
+        receiverOrganization = new Organization()
+        receiverOrganization.setId(UUID.randomUUID().toString())
+        receiverOrganization
+                .addIdentifier()
+                .setSystem("another-system")
+                .setValue("simulated-hospital-id")
+        bundle = createMessageBundle(receiverOrganization: receiverOrganization)
+        fhirResource = new HapiFhirResource(fhir.parseResource(fhir.encodeResourceToJson(bundle), Bundle))
+
+        then:
+        !rule.isValid(fhirResource)
+
+        when:
+        receiverOrganization = new Organization()
+        receiverOrganization.setId(UUID.randomUUID().toString())
+        receiverOrganization
+                .addIdentifier()
+                .setValue("simulated-hospital-id")
+        bundle = createMessageBundle(receiverOrganization: receiverOrganization)
+        fhirResource = new HapiFhirResource(fhir.parseResource(fhir.encodeResourceToJson(bundle), Bundle))
+
+        then:
+        !rule.isValid(fhirResource)
+    }
+
+    Bundle createMessageBundle(Map params) {
+        String messageTypeCode = params.messageTypeCode as String ?: "ORM_O01"
+        Organization receiverOrganization = params.receiverOrganization as Organization ?: new Organization()
+        MessageHeader messageHeader = params.messageType as MessageHeader ?: new MessageHeader()
+
+        MessageHeader.MessageDestinationComponent destination = messageHeader.addDestination()
+        String receiverOrganizationFullUrl = "Organization/" + receiverOrganization.getId()
+        destination.setReceiver(new Reference(receiverOrganizationFullUrl))
+
+        Coding eventCoding = new Coding()
+        eventCoding.setSystem("http://terminology.hl7.org/CodeSystem/v2-0003")
+        String[] parts = messageTypeCode.split("_")
+        eventCoding.setCode(parts[1])
+        eventCoding.setDisplay(String.format("%s^%s^%s", parts[0], parts[1], messageTypeCode))
+        messageHeader.setEvent(eventCoding)
+
+        Bundle bundle = new Bundle()
+        bundle.setType(Bundle.BundleType.MESSAGE)
+        bundle.addEntry().setResource(messageHeader)
+        bundle.addEntry().setFullUrl(receiverOrganizationFullUrl).setResource(receiverOrganization)
+        return bundle
     }
 
     Rule createValidationRule(List<String> ruleConditions, List<String> ruleValidations) {
