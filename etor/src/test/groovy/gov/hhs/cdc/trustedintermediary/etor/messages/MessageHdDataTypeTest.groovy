9fe816a8ddf2e24ac40347160126af96b2d8eeb4
jorge Lopez <jlopez@flexion.us>
2024-04-03 14:26:41 -0700

    fixed MessageHDDataTypeTest

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/messages/MessageHdDataTypeTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/messages/MessageHdDataTypeTest.groovy
@@ -11,49 +11,4 @@ class MessageHdDataTypeTest extends Specification {
         then:
         noExceptionThrown()
     }
-
-    def "toString() happy path works"() {
-        given:
-        def namespace = "mock-namespace"
-        def universalId = "mock-universal-id"
-        def universalIdType = "mock-universal-id-type"
-        def expected = "${namespace}^${universalId}^${universalIdType}"
-
-        when:
-        def messageDetails = new MessageHdDataType(namespace, universalId, universalIdType)
-        def actual = messageDetails.toString()
-
-        then:
-        actual == expected
-    }
-
-    def "toString() with null variable unhappy path works"() {
-        given:
-        def namespace = "mock-namespace"
-        def universalId = null
-        def universalIdType = "mock-universal-id-type"
-        def expected = "${namespace}^${universalIdType}"
-
-        when:
-        def messageDetails = new MessageHdDataType(namespace, universalId, universalIdType)
-        def actual = messageDetails.toString()
-
-        then:
-        actual == expected
-    }
-
-    def "toString() with all null variables unhappy path works"() {
-        given:
-        def namespace = null
-        def universalId = null
-        def universalIdType = null
-        def expected = ""
-
-        when:
-        def messageDetails = new MessageHdDataType(namespace, universalId, universalIdType)
-        def actual = messageDetails.toString()
-
-        then:
-        actual == expected
-    }
 }
