c6c9fea76f559877553f31979458562ec75759a7
saquino0827 <saquino@flexion.us>
2024-04-03 14:45:51 -0500

    Update HapiResult and HapiResult

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiResultTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/external/hapi/HapiResultTest.groovy
@@ -1,16 +1,29 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi
 
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
 import org.hl7.fhir.r4.model.Bundle
 import org.hl7.fhir.r4.model.CodeableConcept
 import org.hl7.fhir.r4.model.Coding
 import org.hl7.fhir.r4.model.Extension
 import org.hl7.fhir.r4.model.Identifier
 import org.hl7.fhir.r4.model.MessageHeader
+import org.hl7.fhir.r4.model.Organization
+import org.hl7.fhir.r4.model.Reference
 import org.hl7.fhir.r4.model.ServiceRequest
 import org.hl7.fhir.r4.model.StringType
 import spock.lang.Specification
 
 class HapiResultTest extends Specification{
+
+    def setup() {
+        TestApplicationContext.reset()
+        TestApplicationContext.init()
+        TestApplicationContext.register(HapiFhir.class, HapiFhirImplementation.getInstance())
+        TestApplicationContext.register(HapiMessageHelper.class, HapiMessageHelper.getInstance())
+        TestApplicationContext.injectRegisteredImplementations()
+    }
+
     def "getUnderlyingResult works"() {
         given:
         def expectedResult = new Bundle()
@@ -96,22 +109,63 @@ class HapiResultTest extends Specification{
         actualSendingApplicationId == expectedSendingApplicationId
     }
 
-    def "getSendingFacilityDetails works"() {
+    def "getSendingFacilityDetails happy path works"() {
         given:
-        def expected = 1
+        def innerResults = new Bundle()
+
+        def messageHeader = new MessageHeader()
+        def orgReference = "Organization/1708034743302204787.82104dfb-e854-47de-b7ce-19a2b71e61db"
+        messageHeader.setSender(new Reference(orgReference))
+        innerResults.addEntry(new Bundle.BundleEntryComponent().setResource(messageHeader))
+
+        def organization = new Organization()
+        organization.setId("1708034743302204787.82104dfb-e854-47de-b7ce-19a2b71e61db")
+        // facility name
+        def facilityIdentifier = new Identifier()
+        def facilityName = "MN Public Health Lab"
+        def facilityNameExtension = new Extension("https://reportstream.cdc.gov/fhir/StructureDefinition/hl7v2Field", new StringType("HD.1"))
+        facilityIdentifier.addExtension(facilityNameExtension)
+        facilityIdentifier.setValue(facilityName)
+        // universal id
+        def universalIdIdentifier = new Identifier()
+        def universalIdIdentifierValue = "2.16.840.1.114222.4.1.10080"
+        def universalIdExtension = new Extension("https://reportstream.cdc.gov/fhir/StructureDefinition/hl7v2Field", new StringType("HD.2,HD.3"))
+        universalIdIdentifier.addExtension(universalIdExtension)
+        universalIdIdentifier.setValue(universalIdIdentifierValue)
+        // Type
+        def typeConcept = new CodeableConcept()
+        def theCode = "ISO"
+        def coding = new Coding("http://terminology.hl7.org/CodeSystem/v2-0301", theCode, null)
+        typeConcept.addCoding(coding)
+        universalIdIdentifier.setType(typeConcept)
+
+        organization.addIdentifier(facilityIdentifier)
+        organization.addIdentifier(universalIdIdentifier)
+        innerResults.addEntry(new Bundle.BundleEntryComponent().setResource(organization))
+        def orders = new HapiOrder(innerResults)
+        def expectedFacilityDetails = "$facilityName^$universalIdIdentifierValue^$theCode"
+
         when:
-        def actual = 1
+        def actualFacilityDetails = orders.getSendingFacilityDetails()
+
         then:
-        actual == expected
+        actualFacilityDetails == expectedFacilityDetails
     }
 
-    def "getSendingFacilityDetails unhappy path"() {
+    def "getSendingFacilityDetails unhappy path works"() {
         given:
-        def expected = 1
+        def innerOrders = new Bundle()
+        def expectedFacilityDetails = ""
+        def messageHeader = new MessageHeader()
+        innerOrders.addEntry(new Bundle.BundleEntryComponent().setResource(messageHeader))
+
+        def orders = new HapiOrder(innerOrders)
+
         when:
-        def actual = 1
+        def actualFacilityDetails = orders.getSendingFacilityDetails()
+
         then:
-        actual == expected
+        actualFacilityDetails == expectedFacilityDetails
     }
 
     def "getReceivingApplicationDetails works"() {
