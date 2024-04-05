229914a8ab0448a697cbb5a0b3eb3b4f011b3678
jorge Lopez <jlopez@flexion.us>
2024-04-05 01:46:20 -0700

    javadocs: Message.java

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/Message.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/Message.java
@@ -1,5 +1,10 @@
 package gov.hhs.cdc.trustedintermediary.etor.messages;
 
+/**
+ * Defines the structure and operations for a message. This interface allows for the retrieval of
+ * various pieces of information related to a message, including details about the sending and
+ * receiving applications and facilities, as well as order numbers.
+ */
 public interface Message<T> {
     T getUnderlyingElement();
 
