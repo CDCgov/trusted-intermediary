0a15c0ed752898de475e4b35ea1542d256701897
halprin <halprin@users.noreply.github.com>
2024-04-15 17:38:08 -0600

    Iterator throws NoSuchElementException

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/database/ResultSetIterator.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/external/database/ResultSetIterator.java
@@ -3,6 +3,7 @@ package gov.hhs.cdc.trustedintermediary.external.database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * ResultSetIterator iterates over a SQL {@link ResultSet}. This helps stream over a {@link
@@ -10,22 +11,31 @@ import java.util.Iterator;
  */
 public class ResultSetIterator implements Iterator<ResultSet> {
     private final ResultSet resultSet;
+    private boolean hasNext;
 
     public ResultSetIterator(ResultSet resultSet) {
         this.resultSet = resultSet;
+        hasNext = true;
     }
 
     @Override
     public boolean hasNext() {
         try {
-            return resultSet.next();
+            hasNext = resultSet.next();
         } catch (SQLException e) {
+            hasNext = false;
             throw new RuntimeException(e);
         }
+
+        return hasNext;
     }
 
     @Override
     public ResultSet next() {
+        if (!hasNext) {
+            throw new NoSuchElementException();
+        }
+
         return resultSet;
     }
 }
