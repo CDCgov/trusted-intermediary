30037d8d0201b1c736da9001e5625a0da6d261b3
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-18 08:56:20 -0700

    Added initial skeleton for HapiParser

---
+++ e2e/src/main/java/gov/hhs/cdc/trustedintermediary/e2e/HapiParser.java
@@ -0,0 +1,14 @@
+package gov.hhs.cdc.trustedintermediary.e2e;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.fhirpath.IFhirPath;
+
+public class HapiParser {
+    private final IFhirPath engine;
+    private static final FhirContext CONTEXT = FhirContext.forR4();
+
+    public HapiParser() {
+        engine = CONTEXT.newFhirPath();
+        //        engine.setEvaluationContext(new HapiFhirCustomEvaluationContext());
+    }
+}
