24ae3d3ca11682b33fb1475cdc300d0636072032
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-26 15:01:10 -0700

    Added TransformationRuleEngine skeleton

---
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/TransformationRuleEngine.java
@@ -0,0 +1,12 @@
+package gov.hhs.cdc.trustedintermediary.etor.ruleengine;
+
+public class TransformationRuleEngine implements RuleEngine {
+    @Override
+    public void unloadRules() {}
+
+    @Override
+    public void ensureRulesLoaded() {}
+
+    @Override
+    public void runRules(FhirResource<?> resource) {}
+}
