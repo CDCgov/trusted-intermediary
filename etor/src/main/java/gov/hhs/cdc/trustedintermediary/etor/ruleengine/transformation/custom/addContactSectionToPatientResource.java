53445b30c7a58c9674918aa895ad292cc03b1247
Luis Pabon <turkeyfried@gmail.com>
2024-04-29 21:33:31 -0400

    Create addContactSectionToPatientResource.java

---
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/addContactSectionToPatientResource.java
@@ -0,0 +1,41 @@
+package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom;
+
+import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
+import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation;
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiMessageConverterHelper;
+import java.util.List;
+import java.util.Map;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Coding;
+
+public class addContactSectionToPatientResource implements CustomFhirTransformation {
+    private final List<Coding> CODING_LIST =
+            List.of(
+                    new Coding(
+                            "http://terminology.hl7.org/CodeSystem/v3-RoleCode", "MTH", "mother"));
+
+    @Override
+    public FhirResource<?> transform(FhirResource<?> resource, Map<String, String> args) {
+        Bundle bundle = (Bundle) resource.getUnderlyingResource();
+
+        var patients = HapiMessageConverterHelper.findAllPatients(bundle);
+
+        patients.forEach(
+                p -> {
+                    var myContact = p.addContact();
+                    var motherRelationship = myContact.addRelationship();
+                    motherRelationship.setCoding(CODING_LIST);
+
+                    var mothersMaidenNameExtension =
+                            p.getExtensionByUrl(
+                                    "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName");
+                    if (mothersMaidenNameExtension != null) {
+                        myContact.setName(p.castToHumanName(mothersMaidenNameExtension.getValue()));
+                    }
+                    myContact.setTelecom(p.getTelecom());
+                    myContact.setAddress(p.getAddressFirstRep());
+                });
+
+        return resource;
+    }
+}
