8d88fd124371017838a33ffd3d3739b06fcebb1d
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-30 13:38:16 -0700

    Fixed import

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/addContactSectionToPatientResource.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/addContactSectionToPatientResource.java
@@ -2,7 +2,7 @@ package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom;
 
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation;
-import gov.hhs.cdc.trustedintermediary.external.hapi.HapiMessageConverterHelper;
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiOrderConverter;
 import java.util.List;
 import java.util.Map;
 import org.hl7.fhir.r4.model.Bundle;
@@ -18,7 +18,7 @@ public class addContactSectionToPatientResource implements CustomFhirTransformat
     public void transform(FhirResource<?> resource, Map<String, String> args) {
         Bundle bundle = (Bundle) resource.getUnderlyingResource();
 
-        var patients = HapiMessageConverterHelper.findAllPatients(bundle);
+        var patients = HapiOrderConverter.findAllPatients(bundle);
 
         patients.forEach(
                 p -> {
