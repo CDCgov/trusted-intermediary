b3aeec9bde37eba6352f998c916413283db90f0b
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-30 08:12:18 -0700

    Implemented custom method evaluation. Still need to test

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/TransformationRule.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/TransformationRule.java
@@ -2,6 +2,10 @@ package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation;
 
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.Rule;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
@@ -24,14 +28,44 @@ public class TransformationRule extends Rule<TransformationRuleMethod> {
     }
 
     public void runRule(FhirResource<?> resource) {
+        Path customTransformatiosPath =
+                Path.of(
+                        "etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/");
+        File[] customTransformationFiles = customTransformatiosPath.toFile().listFiles();
+
+        if (customTransformationFiles == null) {
+            logger.logInfo("No custom transformation files found.");
+            return;
+        }
+
         for (TransformationRuleMethod transformation : this.getRules()) {
             String methodName = transformation.name();
             Map<String, String> methodArgs = transformation.args();
-            // evaluate methodName(resource, methodArgs)
-            // i.e. addEtorProcessingTag(resource, methodArgs) where methodArgs is empty
-            // first, need to get methodName from ruleengine/transformation/custom/ using the class
-            // name or file name
-            // then, need to call the method with the resource and methodArgs
+
+            for (File file : customTransformationFiles) {
+                if (file.isFile() && (file.getName().endsWith(".class"))) {
+                    String className = file.getName().replace(".class", "");
+                    if (className.equalsIgnoreCase(methodName)) {
+                        try {
+                            Class<?> clazz = Class.forName(className);
+                            Method method =
+                                    clazz.getDeclaredMethod(
+                                            "transform", FhirResource.class, Map.class);
+                            method.invoke(
+                                    clazz.getDeclaredConstructor().newInstance(),
+                                    resource,
+                                    methodArgs);
+                            return;
+                        } catch (ClassNotFoundException
+                                | NoSuchMethodException
+                                | IllegalAccessException
+                                | InvocationTargetException
+                                | InstantiationException e) {
+                            logger.logError("Error invoking method: " + e.getMessage());
+                        }
+                    }
+                }
+            }
         }
     }
 }
