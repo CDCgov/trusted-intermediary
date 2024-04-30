4392b2714b8026c634213a3fc21f0b09e8def748
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-04-30 08:14:19 -0700

    Added failing integration test for TransformationRuleEngine

---
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/TransformationRuleEngineIntegrationTest.groovy
@@ -0,0 +1,43 @@
+package gov.hhs.cdc.trustedintermediary.etor.ruleengine
+
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.TransformationRuleEngine
+import gov.hhs.cdc.trustedintermediary.etor.ruleengine.validation.ValidationRuleEngine
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirImplementation
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirResource
+import gov.hhs.cdc.trustedintermediary.external.jackson.Jackson
+import gov.hhs.cdc.trustedintermediary.wrappers.HapiFhir
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger
+import gov.hhs.cdc.trustedintermediary.wrappers.formatter.Formatter
+import org.hl7.fhir.r4.model.Bundle
+import spock.lang.Specification
+
+class TransformationRuleEngineIntegrationTest  extends Specification {
+    def fhir = HapiFhirImplementation.getInstance()
+    def engine = TransformationRuleEngine.getInstance("transformation_definitions.json")
+    def mockLogger = Mock(Logger)
+
+    def setup() {
+        TestApplicationContext.reset()
+        TestApplicationContext.init()
+
+        TestApplicationContext.register(Formatter, Jackson.getInstance())
+        TestApplicationContext.register(HapiFhir, fhir)
+        TestApplicationContext.register(ValidationRuleEngine, engine)
+        TestApplicationContext.register(RuleLoader, RuleLoader.getInstance())
+        TestApplicationContext.register(Logger, mockLogger)
+
+        TestApplicationContext.injectRegisteredImplementations()
+    }
+
+    def "transformation rules run"() {
+        given:
+        def bundle = new Bundle()
+
+        when:
+        engine.runRules(new HapiFhirResource(bundle))
+
+        then:
+        0 * mockLogger.logWarning(_ as String)
+    }
+}
