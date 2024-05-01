511ec68d4b33805d2208ae6a65246d3078900131
saquino0827 <saquino@flexion.us>
2024-05-01 11:07:58 -0500

    Start test coverage for existing transformation rules

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/TransformationRuleEngineIntegrationTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/TransformationRuleEngineIntegrationTest.groovy
@@ -40,4 +40,21 @@ class TransformationRuleEngineIntegrationTest  extends Specification {
         then:
         0 * mockLogger.logError(_ as String, _ as Exception)
     }
+
+    def "transformation rules filter and run rules for Orders"() {
+        given:
+        def bundle = new Bundle()
+
+        when:
+        engine.runRules(new HapiFhirResource(bundle))
+
+        then:
+        0 * mockLogger.logError(_ as String, _ as Exception)
+    }
+
+    def "transformation rules filter and run rules for Results"() {
+    }
+
+    def "transformation rules filter and run rules for Demographics"() {
+    }
 }
