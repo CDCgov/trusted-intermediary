4b512a2a8def4346e6fb3ac37d9e7dfe3f2dc501
Sylvie <sschuresko@flexion.us>
2024-05-03 16:09:03 -0500

    try running e2e against DB?

--- e2e-execute.sh
+++ e2e-execute.sh
@@ -15,11 +15,30 @@ start_api() {
     JAR_NAME="app-all.jar"
 
     echo 'Starting API'
+    export DB_URL=localhost
+    export DB_PORT=5433
+    export DB_NAME=intermediary
+    export DB_USER=intermediary
+    export DB_PASS=changeIT!
+    export DB_SSL=require
     java -jar "${DIR}"/"${SUB_DIR}"/"${JAR_NAME}" > /dev/null &
     export API_PID="${!}"
     echo "API starting at PID ${API_PID}"
 
     popd
+}
+
+start_database() {
+    echo 'Starting database'
+    docker compose -f docker-compose.postgres.yml up -d
+    sleep 2
+    echo "Database started"
+}
+
+migrate_database() {
+    echo 'Migrating database'
+    liquibase update --changelog-file ./etor/databaseMigrations/root.yml --url jdbc:postgresql://localhost:5433/intermediary --username intermediary --password 'changeIT!' --label-filter '!azure'
+    echo "Database migrated"
 }
 
 wait_for_api() {
@@ -47,9 +66,17 @@ run_tests() {
 cleanup() {
     echo "Killing API at PID ${API_PID}"
     kill "${API_PID}"
+    echo "PID ${API_PID} killed"
+    echo "Stopping and deleting database"
+    docker stop trusted-intermediary-postgresql-1
+    docker rm -f trusted-intermediary-postgresql-1
+    docker volume rm trusted-intermediary_ti_postgres_data
+    echo "Database stopped and deleted"
 }
 
 trap cleanup EXIT  # Run the cleanup function on exit
+start_database
+migrate_database
 shadowJar
 start_api
 wait_for_api
