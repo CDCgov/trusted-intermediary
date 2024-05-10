a378de06e9ae7d2a5e5ff9b2406b8425b45ac229
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-05-10 07:36:15 -0700

    Refactor transformation helpers (#1085)
    * Refactored to create more generic helper functions and move logic to transformation method
    
    * Fixed failing tests
    
    * Refactored to consolidate hapi helper methods
    
    * Fixed test
    
    * Moved oml coding contanst to hapi helper

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/HappyPathMockClass.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/HappyPathMockClass.groovy
@@ -2,6 +2,7 @@ package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom
 
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper
 import org.hl7.fhir.r4.model.Bundle
 import org.hl7.fhir.r4.model.Coding
 import org.hl7.fhir.r4.model.MessageHeader
@@ -11,14 +12,12 @@ class HappyPathMockClass implements CustomFhirTransformation {
     @Override
     public void transform(final FhirResource<?> resource, final Map<String, String> args) {
         Bundle bundle = (Bundle) resource.getUnderlyingResource()
-        MessageHeader messageHeader = new MessageHeader()
 
-        String messageTypeCode = "mock_code"
-        Coding eventCoding = new Coding()
-        eventCoding.setCode(messageTypeCode)
-        eventCoding.setDisplay(String.format("mock^code^%s", messageTypeCode))
-        eventCoding.setSystem("http://terminology.hl7.org/CodeSystem/v2-0003")
-        messageHeader.setEvent(eventCoding)
-        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(messageHeader))
+        def system = "http://terminology.hl7.org/CodeSystem/v2-0003"
+        def code = "mock_code"
+        def display = String.format("mock^code^%s", code)
+
+        Coding eventCoding = new Coding(system, code, display)
+        HapiHelper.setMessageTypeCoding(bundle, eventCoding)
     }
 }
