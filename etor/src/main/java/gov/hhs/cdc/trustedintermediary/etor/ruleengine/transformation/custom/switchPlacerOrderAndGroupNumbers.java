8b3c62acb5681acb5a0217094f96945923cf86ef
Luis Pabon <turkeyfried@gmail.com>
2024-05-14 16:44:44 -0400

    Transformation now works

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/switchPlacerOrderAndGroupNumbers.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/switchPlacerOrderAndGroupNumbers.java
@@ -1,14 +1,12 @@
 package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom;
 
-import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
-import gov.hhs.cdc.trustedintermediary.etor.metadata.EtorMetadataStep;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation;
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper;
-import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata;
 import java.util.Map;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.ServiceRequest;
+import org.hl7.fhir.r4.model.StringType;
 
 /**
  * Updates Placer Order Number (ORC-2) with content from Placer Group Number (ORC-4). It also
@@ -18,8 +16,8 @@ import org.hl7.fhir.r4.model.ServiceRequest;
  */
 public class switchPlacerOrderAndGroupNumbers implements CustomFhirTransformation {
 
-    private final MetricMetadata metadata =
-            ApplicationContext.getImplementation(MetricMetadata.class);
+    //    private final MetricMetadata metadata =
+    //            ApplicationContext.getImplementation(MetricMetadata.class);
 
     @Override
     public void transform(FhirResource<?> resource, Map<String, String> args) {
@@ -38,19 +36,24 @@ public class switchPlacerOrderAndGroupNumbers implements CustomFhirTransformatio
                                     .getIdentifier()
                                     .get(0)
                                     .getExtensionByUrl(
+                                            "https://reportstream.cdc.gov/fhir/StructureDefinition/assigning-authority")
+                                    .getExtensionByUrl(
                                             "https://reportstream.cdc.gov/fhir/StructureDefinition/namespace-id");
                     var serviceCoding = serviceRequest.getCode().getCoding().get(0);
+                    var obr2_1 = serviceIdentifier.getValue();
+                    var obr2_2 = serviceNamespaceExtension.getValue().primitiveValue();
+                    var obr4_1 = serviceCoding.getCode();
+                    var obr4_2 = serviceCoding.getDisplay();
 
-                    // Switch values on OBR 2.1 and 4.1
-                    var valueHolder = serviceIdentifier.getValue();
-                    serviceIdentifier.setValue(serviceCoding.getCode());
-                    serviceCoding.setCode(valueHolder);
+                    // Switch values between OBR 2.1 and 4.1
+                    serviceIdentifier.setValue(obr4_1);
+                    serviceCoding.setCode(obr2_1);
 
-                    // Switch values on OBR 2.2 and 4.2
-                    valueHolder = serviceNamespaceExtension.getValue().primitiveValue();
-                    serviceNamespaceExtension.setValue(serviceCoding.getDisplayElement());
-                    serviceCoding.setCode(valueHolder);
+                    // Switch values between OBR 2.2 and 4.2
+                    serviceNamespaceExtension.setValue(new StringType(obr4_2));
+                    serviceCoding.setDisplay(obr2_2);
                 });
-        metadata.put(bundle.getId(), EtorMetadataStep.SWITCH_PLACER_ORDER_AND_GROUP_NUMBERS);
+        //        metadata.put(bundle.getId(),
+        // EtorMetadataStep.SWITCH_PLACER_ORDER_AND_GROUP_NUMBERS);
     }
 }
--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/SwitchPlacerOrderAndGroupNumbersTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/SwitchPlacerOrderAndGroupNumbersTest.groovy
@@ -24,17 +24,16 @@ class SwitchPlacerOrderAndGroupNumbersTest extends Specification {
         def fhirResource = ExamplesHelper.getExampleFhirResource("../MN/004_MN_ORU_R01_NBS_1_hl7_translation.fhir")
         def bundle = fhirResource.getUnderlyingResource() as Bundle
         def result = getOBRSections(bundle)
-
         def obr2_1 = result[0]
         def obr2_2 = result[1]
         def obr4_1 = result[2]
         def obr4_2 = result[3]
 
         expect:
-        obr2_1 != null
-        obr2_2 != null
-        obr4_1 != null
-        obr4_2 != null
+        obr2_1 == "423787478"
+        obr2_2 == "EPIC"
+        obr4_1 == "57128-1"
+        obr4_2 == "Newborn Screening Report summary panel"
 
         when:
         transformClass.transform(fhirResource, null)
@@ -45,21 +44,22 @@ class SwitchPlacerOrderAndGroupNumbersTest extends Specification {
         def switchedObr4_2 = switchedResult[3]
 
         then:
-        switchedObr2_1 != obr2_1
-        switchedObr2_2 != obr2_2
-        switchedObr4_1 != obr4_1
-        switchedObr4_2 != obr4_2
+        switchedObr2_1 == obr4_1
+        switchedObr2_2 == obr4_2
+        switchedObr4_1 == obr2_1
+        switchedObr4_2 == obr2_2
     }
 
     // Returns a list of values for the OBR sections that need checking in the following order:
     // [2.1, 2.2, 4.1, 4.2]
     def getOBRSections(Bundle bundle) {
-        def serviceRequest = HapiHelper.resourcesInBundle(bundle, ServiceRequest)
-        def serviceIdentifier = serviceRequest.getIdentifier().get(0)
+        def serviceRequest = HapiHelper.resourceInBundle(bundle, ServiceRequest)
+        def serviceIdentifier = serviceRequest.getIdentifier()[0]
         var serviceNamespaceExtension =
                 serviceRequest
-                .getIdentifier()
-                .get(0)
+                .getIdentifier()[0]
+                .getExtensionByUrl(
+                "https://reportstream.cdc.gov/fhir/StructureDefinition/assigning-authority")
                 .getExtensionByUrl(
                 "https://reportstream.cdc.gov/fhir/StructureDefinition/namespace-id")
         var serviceCoding = serviceRequest.getCode().getCoding().get(0)
@@ -68,7 +68,7 @@ class SwitchPlacerOrderAndGroupNumbersTest extends Specification {
             serviceIdentifier.getValue(),
             serviceNamespaceExtension.getValue().primitiveValue(),
             serviceCoding.getCode(),
-            serviceCoding.getDisplayElement()
+            serviceCoding.getDisplay()
         ]
     }
 }
