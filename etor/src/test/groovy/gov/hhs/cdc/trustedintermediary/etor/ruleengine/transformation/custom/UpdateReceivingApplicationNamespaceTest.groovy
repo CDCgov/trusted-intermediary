1fe1925b6be6ae5a6972c8bceedfb95e2b9bc7fb
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-05-14 16:00:04 -0700

    Moved tests for transformations

---
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/UpdateReceivingApplicationNamespaceTest.groovy
@@ -0,0 +1,40 @@
+package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom
+
+import gov.hhs.cdc.trustedintermediary.FhirBundleHelper
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirResource
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata
+import org.hl7.fhir.r4.model.Organization
+import spock.lang.Specification
+
+class UpdateReceivingApplicationNamespaceTest extends Specification {
+
+    def transformClass
+
+    def setup() {
+        TestApplicationContext.reset()
+        TestApplicationContext.init()
+        TestApplicationContext.register(MetricMetadata, Mock(MetricMetadata))
+        TestApplicationContext.injectRegisteredImplementations()
+
+        transformClass = new updateReceivingApplicationNamespace()
+    }
+
+    def "update receiving application namespace to given name"() {
+        given:
+        def name = "EPIC"
+        def bundle = FhirBundleHelper.createMessageBundle(messageTypeCode: 'ORM_O01')
+
+        expect:
+        HapiHelper.resourceInBundle(bundle, Organization).isEmpty()
+
+        when:
+        transformClass.transform(new HapiFhirResource(bundle), Map.of("name", name))
+        def header = HapiHelper.getMessageHeader(bundle)
+        def org = header.destination.first().getReceiver().getResource() as Organization
+
+        then:
+        org.name == name
+    }
+}
---
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/UpdateSendingFacilityNamespaceTest.groovy
@@ -0,0 +1,47 @@
+package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom
+
+import gov.hhs.cdc.trustedintermediary.ExamplesHelper
+import gov.hhs.cdc.trustedintermediary.FhirBundleHelper
+import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirResource
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper
+import gov.hhs.cdc.trustedintermediary.external.hapi.HapiOrderTest
+import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.MessageHeader
+import org.hl7.fhir.r4.model.Organization
+import org.hl7.fhir.r4.model.Reference
+import spock.lang.Specification
+
+class UpdateSendingFacilityNamespaceTest extends Specification {
+
+    def transformClass
+
+    def setup() {
+        TestApplicationContext.reset()
+        TestApplicationContext.init()
+        TestApplicationContext.register(MetricMetadata, Mock(MetricMetadata))
+        TestApplicationContext.injectRegisteredImplementations()
+
+        transformClass = new updateSendingFacilityNamespace()
+    }
+
+    def "update sending facility namespace to given name"() {
+        given:
+        def name = "CDPH"
+        def fhirResource = ExamplesHelper.getExampleFhirResource("../MN/004_MN_ORU_R01_NBS_1_hl7_translation.fhir")
+        def bundle = fhirResource.getUnderlyingResource() as Bundle
+
+        expect:
+        def existingOrg = HapiHelper.getMessageHeader(bundle).getSender().getResource() as Organization
+        existingOrg.name != name
+
+        when:
+        transformClass.transform(new HapiFhirResource(bundle), Map.of("name", name))
+        def header = HapiHelper.getMessageHeader(bundle)
+        def org = header.getSender().getResource() as Organization
+
+        then:
+        org.name == name
+    }
+}
