76aeb89e9e8342e786faf7927bfd9301bb5c8ebd
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-05-15 08:45:40 -0700

    Added test coverage for removeMessageTypeStructure

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveMessageTypeStructureTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveMessageTypeStructureTest.groovy
@@ -4,6 +4,8 @@ import gov.hhs.cdc.trustedintermediary.FhirBundleHelper
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirResource
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.Coding
 import spock.lang.Specification
 
 class RemoveMessageTypeStructureTest  extends Specification {
@@ -21,7 +23,7 @@ class RemoveMessageTypeStructureTest  extends Specification {
     def "remove message type structure"() {
         given:
         def bundle = FhirBundleHelper.createMessageBundle(messageTypeCode: 'ORM_O01')
-        def messageHeader = HapiHelper.getOrCreateMessageHeader(bundle)
+        def messageHeader = HapiHelper.getMessageHeader(bundle)
         def displayArray = messageHeader.getEventCoding().getDisplay().split("\\^")
 
         expect:
@@ -30,11 +32,27 @@ class RemoveMessageTypeStructureTest  extends Specification {
 
         when:
         transformClass.transform(new HapiFhirResource(bundle), null)
-        def convertedMessageHeader = HapiHelper.getOrCreateMessageHeader(bundle)
+        def convertedMessageHeader = HapiHelper.getMessageHeader(bundle)
         def convertedDisplay = convertedMessageHeader.getEventCoding().getDisplay()
         def convertedDisplayArray = convertedDisplay.split("\\^")
 
         then:
         convertedDisplayArray.size() == 2
+    }
+
+    def "don't do anything if message type structure not present"() {
+        given:
+        def messageTypeDisplay = "ORU^R01"
+        def bundle = new Bundle()
+        def messageHeader = HapiHelper.getOrCreateMessageHeader(bundle)
+        messageHeader.setEvent(new Coding().setDisplay(messageTypeDisplay))
+
+        when:
+        transformClass.transform(new HapiFhirResource(bundle), null)
+        def convertedMessageHeader = HapiHelper.getMessageHeader(bundle)
+        def convertedDisplay = convertedMessageHeader.getEventCoding().getDisplay()
+
+        then:
+        convertedDisplay == messageTypeDisplay
     }
 }
