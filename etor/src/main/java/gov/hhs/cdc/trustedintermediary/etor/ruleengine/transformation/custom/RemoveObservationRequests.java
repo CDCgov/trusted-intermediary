92d2bdcf2920ccd3c8c732c532bf058f87f86fe8
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-06-03 16:16:28 -0700

    Improved performance by reducing overhead of stream operations and reducing needed iterations

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveObservationRequests.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveObservationRequests.java
@@ -3,14 +3,16 @@ package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation;
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Set;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.DiagnosticReport;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ServiceRequest;
 
 /**
@@ -23,28 +25,37 @@ public class RemoveObservationRequests implements CustomFhirTransformation {
     public void transform(FhirResource<?> resource, Map<String, String> args) {
         Bundle bundle = (Bundle) resource.getUnderlyingResource();
 
-        Stream<DiagnosticReport> diagnosticReports =
-                HapiHelper.resourcesInBundle(bundle, DiagnosticReport.class);
-        List<Observation> observations =
-                HapiHelper.resourcesInBundle(bundle, Observation.class).toList();
-        List<Reference> references =
-                observations.stream()
-                        .map(observation -> new Reference(observation.getId()))
-                        .collect(Collectors.toList());
+        String universalServiceIdentifier = args.get("universalServiceIdentifier");
+        Set<Resource> resourcesToRemove = new HashSet<>();
+        List<Reference> observationReferences = new ArrayList<>();
+        DiagnosticReport singleDiagnosticReport = null;
+        ServiceRequest singleServiceRequest = null;
 
-        for (DiagnosticReport report : diagnosticReports.toList()) {
-            ServiceRequest serviceRequest = HapiHelper.getServiceRequest(report);
-            if (serviceRequest != null) {
-                String ob4_1 = HapiHelper.getOBR4_1Value(serviceRequest);
-                if (args.get("universalServiceIdentifier").equals(ob4_1)) {
-                    HapiHelper.removeTopLevelResources(
-                            bundle, List.of(DiagnosticReport.class, ServiceRequest.class));
-                    bundle.addEntry().setResource(report);
-                    bundle.addEntry().setResource(serviceRequest);
-                    report.setResult(references);
-                    break;
+        for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
+            Resource resourceEntry = entry.getResource();
+            if (resourceEntry instanceof Observation observation) {
+                observationReferences.add(new Reference(observation.getId()));
+            } else if (resourceEntry instanceof DiagnosticReport diagnosticReport) {
+                resourcesToRemove.add(diagnosticReport);
+                ServiceRequest serviceRequest = HapiHelper.getServiceRequest(diagnosticReport);
+                if (serviceRequest != null) {
+                    String obr4_1 = HapiHelper.getOBR4_1Value(serviceRequest);
+                    if (universalServiceIdentifier.equals(obr4_1)) {
+                        singleDiagnosticReport = diagnosticReport;
+                        singleServiceRequest = serviceRequest;
+                    }
                 }
+
+            } else if (resourceEntry instanceof ServiceRequest serviceRequest) {
+                resourcesToRemove.add(serviceRequest);
             }
+        }
+
+        if (singleDiagnosticReport != null) {
+            bundle.getEntry().removeIf(entry -> resourcesToRemove.contains(entry.getResource()));
+            bundle.addEntry().setResource(singleDiagnosticReport);
+            bundle.addEntry().setResource(singleServiceRequest);
+            singleDiagnosticReport.setResult(observationReferences);
         }
     }
 }
