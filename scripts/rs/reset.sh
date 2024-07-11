1f6f38b488bff44ac14a65350c1c8f53d7dea3ec
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-07-11 09:56:12 -0700

    Added commands to load keys

--- scripts/rs/reset.sh
+++ scripts/rs/reset.sh
@@ -18,13 +18,19 @@
 ./prime organization addkey --public-key $CDCTI_HOME/mock_credentials/organization-trusted-intermediary-public-key-local.pem --scope "flexion.*.report" --orgName flexion --kid flexion.simulated-hospital --doit
 ./prime organization addkey --public-key $CDCTI_HOME/mock_credentials/organization-trusted-intermediary-public-key-local.pem --scope "flexion.*.report" --orgName flexion --kid flexion.simulated-lab --doit
 
+## AL
+./prime multiple-settings set -s -i ./settings/STLTs/AL/al-phl.yml
+# ./prime organization addkey --public-key /path/to/public/key.pem --scope "al-phl.*.report" --orgName al-phl --kid al-phl.etor-nbs-results --doit
+./prime multiple-settings set -s -i ./settings/STLTs/Oracle/oracle-rln.yml
+# ./prime organization addkey --public-key /path/to/public/key.pem --scope "oracle-rln.*.report" --orgName oracle-rln --kid oracle-rln.etor-nbs-orders --doit
+
 ## CA
 ./prime multiple-settings set -s -i ./settings/STLTs/CA/ucsd.yml
+./prime multiple-settings set -s -i ./settings/STLTs/CA/ca-phl.yml
+# ./prime organization addkey --public-key /path/to/public/key.pem --scope "ca-phl.*.report" --orgName ca-phl --kid ca-phl.etor-nbs-results --doit
 
 ## LA
 ./prime multiple-settings set -s -i ./settings/STLTs/LA/la-phl.yml
+# ./prime organization addkey --public-key /path/to/public/key.pem --scope "la-phl.*.report" --orgName la-phl --kid la-phl.etor-nbs-results --doit
 ./prime multiple-settings set -s -i ./settings/STLTs/LA/la-ochsner.yml
-
-## CA
-./prime multiple-settings set -s -i ./settings/STLTs/AL/al-phl.yml
-./prime multiple-settings set -s -i ./settings/STLTs/Oracle/oracle-rln.yml
+# ./prime organization addkey --public-key /path/to/public/key.pem --scope "la-ochsner.*.report" --orgName la-ochsner --kid la-ochsner.etor-nbs-orders --doit
