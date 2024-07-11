803a224df8362c2e0a582bf9c92eae7edfb7ad9e
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-07-11 09:45:55 -0700

    Updated RS reset script

--- scripts/rs/reset.sh
+++ scripts/rs/reset.sh
@@ -3,11 +3,28 @@
 # Make sure to add a CDCTI_HOME environment variable pointing to the trusted-intermediary directory
 # export CDCTI_HOME="/path/to/trusted-intermediary"
 
-./gradlew resetDB
-./gradlew reloadTable
-./gradlew flywayRepair
-./gradlew reloadSettings
-./prime multiple-settings set -s -i ./settings/staging/0166-flexion-staging-results-handling.yml
+# Reset database
+../gradlew resetDB
+
+# Reload tables
+./prime lookuptables loadall
+
+# Load organization settings
+./prime multiple-settings set -s -i ./settings/organizations.yml
+
+## Flexion
+./prime multiple-settings set -s -i ./settings/STLTs/Flexion/flexion.yml
 ./prime organization addkey --public-key $CDCTI_HOME/mock_credentials/organization-trusted-intermediary-public-key-local.pem --scope "flexion.*.report" --orgName flexion --kid flexion.etor-service-sender --doit
 ./prime organization addkey --public-key $CDCTI_HOME/mock_credentials/organization-trusted-intermediary-public-key-local.pem --scope "flexion.*.report" --orgName flexion --kid flexion.simulated-hospital --doit
 ./prime organization addkey --public-key $CDCTI_HOME/mock_credentials/organization-trusted-intermediary-public-key-local.pem --scope "flexion.*.report" --orgName flexion --kid flexion.simulated-lab --doit
+
+## CA
+./prime multiple-settings set -s -i ./settings/STLTs/CA/ucsd.yml
+
+## LA
+./prime multiple-settings set -s -i ./settings/STLTs/LA/la-phl.yml
+./prime multiple-settings set -s -i ./settings/STLTs/LA/la-ochsner.yml
+
+## CA
+./prime multiple-settings set -s -i ./settings/STLTs/AL/al-phl.yml
+./prime multiple-settings set -s -i ./settings/STLTs/Oracle/oracle-rln.yml
