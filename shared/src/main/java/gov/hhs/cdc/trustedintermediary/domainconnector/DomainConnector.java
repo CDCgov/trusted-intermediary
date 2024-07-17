419ee347af5ced7470bb24eec7ef3eae47c36982
Luis Pabon <turkeyfried@gmail.com>
2024-07-17 12:31:50 -0400

    Update DomainConnector.java
    Add @throws definition to remove warning

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/DomainConnector.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/domainconnector/DomainConnector.java
@@ -28,7 +28,7 @@ public interface DomainConnector {
      * An implementation would return the OpenAPI specification in YAML format for this domain
      *
      * @return The OpenAPI specifications for just this given domain
-     * @throws UnableToReadOpenApiSpecificationException
+     * @throws UnableToReadOpenApiSpecificationException When unable to load the expected file
      */
     String openApiSpecification() throws UnableToReadOpenApiSpecificationException;
 }
