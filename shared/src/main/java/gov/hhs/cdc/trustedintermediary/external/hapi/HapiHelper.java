ab56bd0ee0b958bb6ee7a37b80a601ed567e1944
Luis Pabon <turkeyfried@gmail.com>
2024-07-25 14:09:28 -0400

    Update HapiHelper.java
    Add implementation of OBR2 methods

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiHelper.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiHelper.java
@@ -47,6 +47,7 @@ public class HapiHelper {
     public static final StringType EXTENSION_HD2_HD3_DATA_TYPE = new StringType("HD.2,HD.3");
     public static final StringType EXTENSION_ORC2_DATA_TYPE = new StringType("ORC.2");
     public static final StringType EXTENSION_ORC4_DATA_TYPE = new StringType("ORC.4");
+    public static final StringType EXTENSION_OBR2_DATA_TYPE = new StringType("OBR.2");
 
     public static final String EXTENSION_CODING_SYSTEM =
             "https://reportstream.cdc.gov/fhir/StructureDefinition/cwe-coding-system";
@@ -60,6 +61,9 @@ public class HapiHelper {
                     "http://terminology.hl7.org/CodeSystem/v2-0003",
                     "O21",
                     "OML - Laboratory order");
+
+    public static final String EXTENSION_OBR_URL =
+            "https://reportstream.cdc.gov/fhir/StructureDefinition/obr-observation-request";
 
     /**
      * Returns a {@link Stream} of FHIR resources inside the provided {@link Bundle} that match the
@@ -412,9 +416,51 @@ public class HapiHelper {
     // OBR - Observation Request
 
     // OBR-2 - Placer Order Number
-    public static void setOBR2_1Value(ServiceRequest serviceRequest, String value) {}
+    public static Identifier getOBR2Identifier(ServiceRequest serviceRequest) {
+        if (!serviceRequest.hasExtension(EXTENSION_OBR_URL)
+                || !serviceRequest
+                        .getExtensionByUrl(EXTENSION_OBR_URL)
+                        .hasExtension(EXTENSION_OBR2_DATA_TYPE.toString())) {
+            return null;
+        }
+        var extension =
+                serviceRequest
+                        .getExtensionByUrl(EXTENSION_OBR_URL)
+                        .getExtensionByUrl(EXTENSION_OBR2_DATA_TYPE.toString());
+        return extension.castToIdentifier(extension.getValue());
+    }
 
-    public static void setOBR2_2Value(ServiceRequest serviceRequest, String value) {}
+    // OBR-2.1 - Entity Identifier
+    public static String getOBR2_1Value(ServiceRequest serviceRequest) {
+        var identifier = getOBR2Identifier(serviceRequest);
+        if (identifier == null) {
+            return null;
+        }
+        return getEI1Value(identifier);
+    }
+
+    public static void setOBR2_1Value(ServiceRequest serviceRequest, String value) {
+        var identifier = getOBR2Identifier(serviceRequest);
+        if (identifier != null) {
+            setEI1Value(identifier, value);
+        }
+    }
+
+    // OBR-2.2 - Namespace ID
+    public static String getOBR2_2Value(ServiceRequest serviceRequest) {
+        var identifier = getOBR2Identifier(serviceRequest);
+        if (identifier == null) {
+            return null;
+        }
+        return getEI2Value(identifier);
+    }
+
+    public static void setOBR2_2Value(ServiceRequest serviceRequest, String value) {
+        var identifier = getOBR2Identifier(serviceRequest);
+        if (identifier != null) {
+            setEI2Value(identifier, value);
+        }
+    }
 
     // OBR-4 - Universal Service Identifier
 
