c436c91083e191ff851791f71a52569d2f6f0dde
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-07-31 13:10:52 -0700

    Transformation to update OBR-2 with ORC-4 (#1190)
    * Added placeholder for the updates to OBR-2
    
    * Update SwapPlacerOrderAndGroupNumbersTest.groovy
    
    Add missing transformation on test case
    
    * Add Fhir Mapping for OBR.2 and renamed the ORC.2 Mapping
    
    * Update HapiHelper.java
    
    Add implementation of OBR2 methods
    
    * Added test coverage
    
    * Fixed test
    
    * Updated description for SwapPlacerOrderAndGroupNumbers transformation
    
    * Added more context to test name
    
    ---------
    
    Co-authored-by: Luis Pabon <turkeyfried@gmail.com>

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/SwapPlacerOrderAndGroupNumbers.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/SwapPlacerOrderAndGroupNumbers.java
@@ -8,10 +8,11 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.ServiceRequest;
 
 /**
- * Updates Placer Order Number (ORC-2) with content from Placer Group Number (ORC-4). It also
- * replaces Placer Order Number's Entity Identifier (ORC-2.1) and Namespace Id (ORC-2.2) with Placer
- * Group Number's Entity Identifier (ORC-4.1) and Namespace Id (ORC-4.2) respectively. Effectively,
- * we're swapping ORC-2 for ORC-4 and vice versa.
+ * Updates Placer Order Number in ORC and OBR (ORC-2 and OBR-4) with content from Placer Group
+ * Number (ORC-4). Specifically, it updated ORC-2.1/OBR-2.1 with OBR-4.1, and ORC-2.2/OBR-2.2 with
+ * OBR-4.2. It also updates Placer Group Number (ORC-4) with the original value for Placer Order
+ * Number (ORC-2). Specifically, it updates ORC-4.1 and ORC-4.2 with the original values in ORC-2.1
+ * and ORC-2.2 respectively.
  */
 public class SwapPlacerOrderAndGroupNumbers implements CustomFhirTransformation {
 
@@ -28,6 +29,8 @@ public class SwapPlacerOrderAndGroupNumbers implements CustomFhirTransformation 
 
             HapiHelper.setORC2_1Value(serviceRequest, orc4_1);
             HapiHelper.setORC2_2Value(serviceRequest, orc4_2);
+            HapiHelper.setOBR2_1Value(serviceRequest, orc4_1);
+            HapiHelper.setOBR2_2Value(serviceRequest, orc4_2);
             HapiHelper.setORC4_1Value(serviceRequest, orc2_1);
             HapiHelper.setORC4_2Value(serviceRequest, orc2_2);
         }
