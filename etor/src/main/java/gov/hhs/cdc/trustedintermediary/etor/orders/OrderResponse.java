ff26a710876385a4e92daa3ab53bdf2957ef7d0e
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-08-14 11:09:32 -0400

    Refactored constructor and made class final

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/OrderResponse.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/OrderResponse.java
@@ -1,34 +1,25 @@
 package gov.hhs.cdc.trustedintermediary.etor.orders;
 
 /** Response for the v1/etor/orders endpoint. */
-public class OrderResponse {
+public final class OrderResponse {
 
-    private String fhirResourceId;
-    private String patientId;
+    private final String fhirResourceId;
+    private final String patientId;
 
     OrderResponse(String fhirResourceId, String patientId) {
-        setFhirResourceId(fhirResourceId);
-        setPatientId(patientId);
+        this.fhirResourceId = fhirResourceId;
+        this.patientId = patientId;
     }
 
     public OrderResponse(Order<?> orders) {
-        setFhirResourceId(orders.getFhirResourceId());
-        setPatientId(orders.getPatientId());
+        this(orders.getFhirResourceId(), orders.getPatientId());
     }
 
     public String getFhirResourceId() {
         return fhirResourceId;
     }
 
-    public void setFhirResourceId(String fhirResourceId) {
-        this.fhirResourceId = fhirResourceId;
-    }
-
     public String getPatientId() {
         return patientId;
-    }
-
-    public void setPatientId(String patientId) {
-        this.patientId = patientId;
     }
 }
