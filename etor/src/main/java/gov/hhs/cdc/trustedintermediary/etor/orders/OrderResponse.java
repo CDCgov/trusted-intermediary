7f50e7c128206e8885cabaadfe8b3447d63fcf1d
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-08-16 08:19:07 -0700

    Converted immutable classes into records

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/OrderResponse.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/orders/OrderResponse.java
@@ -1,25 +1,9 @@
 package gov.hhs.cdc.trustedintermediary.etor.orders;
 
 /** Response for the v1/etor/orders endpoint. */
-public final class OrderResponse {
-
-    private final String fhirResourceId;
-    private final String patientId;
-
-    OrderResponse(String fhirResourceId, String patientId) {
-        this.fhirResourceId = fhirResourceId;
-        this.patientId = patientId;
-    }
+public record OrderResponse(String fhirResourceId, String patientId) {
 
     public OrderResponse(Order<?> orders) {
         this(orders.getFhirResourceId(), orders.getPatientId());
-    }
-
-    public String getFhirResourceId() {
-        return fhirResourceId;
-    }
-
-    public String getPatientId() {
-        return patientId;
     }
 }
