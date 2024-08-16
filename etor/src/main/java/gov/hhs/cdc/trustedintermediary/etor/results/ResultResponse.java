6be531240e0760884822f7dc24b155e700b74dd9
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-08-16 08:58:46 -0700

    Fix `Constructor Invokes Overridable Function` Fortify finding (#1245)
    * Refactored constructor and made class final
    
    * Converted immutable classes into records
    
    * Added missing tests fixes

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultResponse.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/results/ResultResponse.java
@@ -1,21 +1,8 @@
 package gov.hhs.cdc.trustedintermediary.etor.results;
 
-public class ResultResponse {
-    private String fhirResourceId;
-
-    public ResultResponse(String fhirResourceId) {
-        this.fhirResourceId = fhirResourceId;
-    }
+public record ResultResponse(String fhirResourceId) {
 
     public ResultResponse(Result<?> result) {
-        this.fhirResourceId = result.getFhirResourceId();
-    }
-
-    public String getFhirResourceId() {
-        return fhirResourceId;
-    }
-
-    public void setFhirResourceId(final String fhirResourceId) {
-        this.fhirResourceId = fhirResourceId;
+        this(result.getFhirResourceId());
     }
 }
