96cad9f802129b32274fb1bfbc23fddd12da3e0b
Joel Biskie <jbiskie@flexion.us>
2024-08-22 16:28:31 -0500

    WIP - writing first test

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/CopyOrcOrderProviderToObrOrderProviderTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/CopyOrcOrderProviderToObrOrderProviderTest.groovy
@@ -1,12 +1,15 @@
 package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom
 
+import gov.hhs.cdc.trustedintermediary.ExamplesHelper
 import gov.hhs.cdc.trustedintermediary.context.TestApplicationContext
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiFhirHelper
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper
 import gov.hhs.cdc.trustedintermediary.wrappers.MetricMetadata
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.ServiceRequest
 
 
-class CopyOrcOrderProviderToObrOrderProviderTest {
+class CopyOrcOrderProviderToObrOrderProviderTest extends Specification{
 
     def transformClass
 
@@ -19,7 +22,44 @@ class CopyOrcOrderProviderToObrOrderProviderTest {
         transformClass = new AddEtorProcessingTag()
     }
 
-    def "add ETOR meta tag to message header"() {
+    def "when both practitioner resources populated"() {
+        given:
+        def fhirResource = ExamplesHelper.getExampleFhirResource("../CA/007_CA_ORU_R01_CDPH_produced_UCSD2024-07-11-16-02-17-749_1_hl7_translation.fhir")
+        def bundle = fhirResource.getUnderlyingResource() as Bundle
+        def diagnosticReport = HapiHelper.getDiagnosticReport(bundle)
+        def serviceRequest = HapiHelper.getServiceRequest(diagnosticReport)
+        def practitionerRole = HapiHelper.getPractitionerRole(serviceRequest)
+
+        when:
+        def result = ""
+
+        then:
+        practitionerRole.identifier.value.toString() == "the correct NPI"
+    }
+
+
+    def "when ORC-12 extension populated and OBR-16 extension not populated"() {
+        given:
+        when:
+        def result = ""
+        then:
+        1 == 0
+    }
+
+    def "when ORC-12 extension not populated and OBR-16 extension is populated"() {
+        given:
+        when:
+        def result = ""
+        then:
+        1 == 0
+    }
+
+    def "when neither is populated"() {
+        given:
+        when:
+        def result = ""
+        then:
+        1 == 0
     }
 }
 
