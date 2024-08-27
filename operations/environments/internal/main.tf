d3bf55879be19a8cf1b0a8c2a2b7fecab636201e
jcrichlake <jeff.crichlake@agile6.com>
2024-08-27 14:54:37 -0400

    Fixing tf lint issues
    Co-authored-by: halprin <halprin@users.noreply.github.com>

--- operations/environments/internal/main.tf
+++ operations/environments/internal/main.tf
@@ -19,7 +19,7 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_deleted_secrets_on_destroy = false,
+      purge_soft_deleted_secrets_on_destroy = false
       purge_soft_deleted_keys_on_destroy    = false
     }
   }
