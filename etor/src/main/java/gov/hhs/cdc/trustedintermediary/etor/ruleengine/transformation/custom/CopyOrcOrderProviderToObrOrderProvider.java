6d3e72c22e3e04c4daae98a41796835c851250b6
tjohnson7021 <tjohnson@flexion.us>
2024-08-28 10:59:57 -0400

    Cleaned up transform method

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/CopyOrcOrderProviderToObrOrderProvider.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/CopyOrcOrderProviderToObrOrderProvider.java
@@ -22,41 +22,11 @@ public class CopyOrcOrderProviderToObrOrderProvider implements CustomFhirTransfo
         if (serviceRequest == null) {
             return;
         }
-
-        // Get values
-
-        // ORC 12.1 - id # XCN.1
-        // String orc12_1 = HapiHelper.getORC12_1Value(serviceRequest);
-
-        var ref = serviceRequest.getRequester();
         var pract = HapiHelper.getPractitionerRole(serviceRequest);
-
-        //        def obr16Practitioner = getObr16ExtensionPractitioner(serviceRequest)
-        //        def obr16XcnExtension =
-        // obr16Practitioner.getExtensionByUrl(PRACTITIONER_EXTENSION_URL)
-
         var toOverwrite =
                 serviceRequest
                         .getExtensionByUrl(HapiHelper.EXTENSION_OBR_URL)
                         .getExtensionByUrl(HapiHelper.EXTENSION_OBR16_DATA_TYPE.toString());
-
-        //        toOverwrite.setValue(ref);
         toOverwrite.setValue(pract.getPractitioner());
-
-        //        return serviceRequest
-        //                .getExtensionByUrl(HapiHelper.EXTENSION_OBR_URL)
-        //                .getExtensionByUrl(HapiHelper.EXTENSION_OBR16_DATA_TYPE.toString())
-        //                .value
-        //                .getResource()
-
-        //        String orc21_10 = HapiHelper.getORC21Value(serviceRequest);
-        //        HapiHelper.setMSH6_1Value(bundle, orc21_10);
-        //        HapiHelper.removeMSH6_2_and_3_Identifier(bundle);
-
-        //        ORC 12.2 - family name XCN.2
-        //        ORC 12.3 - given name XCN.3
-        //        ORC 12.10 - NPI XCN.10
-
-        // Set values in OBR-16
     }
 }
