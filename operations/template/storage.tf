85c0e1bafbf0c4e6f486e5a462732e5fedbbf03a
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-08-28 10:04:12 -0700

    Added initial terraform config for sftp storage

--- operations/template/storage.tf
+++ operations/template/storage.tf
@@ -39,3 +39,40 @@ resource "azurerm_role_assignment" "allow_api_read_write" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_linux_web_app.api.identity.0.principal_id
 }
+
+resource "azurerm_storage_account" "sftp_storage" {
+  name                              = "cdctisftp${var.environment}"
+  resource_group_name               = data.azurerm_resource_group.group.name
+  location                          = data.azurerm_resource_group.group.location
+  account_tier                      = "Standard"
+  account_replication_type          = "GRS"
+  account_kind                      = "StorageV2"
+  allow_nested_items_to_be_public   = false
+  sftp_enabled                      = true
+  min_tls_version                   = "TLS1_2"
+  infrastructure_encryption_enabled = true
+
+  #   below tags are managed by CDC
+  lifecycle {
+    ignore_changes = [
+      tags["business_steward"],
+      tags["center"],
+      tags["environment"],
+      tags["escid"],
+      tags["funding_source"],
+      tags["pii_data"],
+      tags["security_compliance"],
+      tags["security_steward"],
+      tags["support_group"],
+      tags["system"],
+      tags["technical_steward"],
+      tags["zone"]
+    ]
+  }
+}
+
+resource "azurerm_storage_container" "sftp_container" {
+  name                  = "samples"
+  storage_account_name  = azurerm_storage_account.sftp_storage.name
+  container_access_type = "private"
+}
