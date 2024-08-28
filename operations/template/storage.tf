b4dcf7c7ec7e28544e9c4cca9084648ec07ea028
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-08-28 13:27:32 -0700

    Create buckets for initial and final hl7 files. Disabled SFTP as we may not use it

--- operations/template/storage.tf
+++ operations/template/storage.tf
@@ -40,15 +40,14 @@ resource "azurerm_role_assignment" "allow_api_read_write" {
   principal_id         = azurerm_linux_web_app.api.identity.0.principal_id
 }
 
-resource "azurerm_storage_account" "sftp_storage" {
-  name                              = "cdctisftp${var.environment}"
+resource "azurerm_storage_account" "automated_storage" {
+  name                              = "cdctiautomated${var.environment}"
   resource_group_name               = data.azurerm_resource_group.group.name
   location                          = data.azurerm_resource_group.group.location
   account_tier                      = "Standard"
   account_replication_type          = "GRS"
   account_kind                      = "StorageV2"
   allow_nested_items_to_be_public   = false
-  sftp_enabled                      = true
   min_tls_version                   = "TLS1_2"
   infrastructure_encryption_enabled = true
 
@@ -71,8 +70,14 @@ resource "azurerm_storage_account" "sftp_storage" {
   }
 }
 
-resource "azurerm_storage_container" "sftp_container" {
-  name                  = "samples"
-  storage_account_name  = azurerm_storage_account.sftp_storage.name
+resource "azurerm_storage_container" "initial_container" {
+  name                  = "initial"
+  storage_account_name  = azurerm_storage_account.automated_storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "final_container" {
+  name                  = "final"
+  storage_account_name  = azurerm_storage_account.automated_storage.name
   container_access_type = "private"
 }
