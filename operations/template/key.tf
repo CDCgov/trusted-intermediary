a2402ed1fe4af28f5f1c916542c4f77b8f1c1a22
halprin <halprin@users.noreply.github.com>
2024-08-29 13:31:18 -0600

    Also do encryption for the storage storage account

--- operations/template/key.tf
+++ operations/template/key.tf
@@ -76,6 +76,18 @@ resource "azurerm_key_vault_access_policy" "allow_storage_account_wrapping" {
   ]
 }
 
+resource "azurerm_key_vault_access_policy" "allow_storage_account_real_wrapping" {
+  key_vault_id = azurerm_key_vault.key_storage.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_storage_account.storage.identity.0.principal_id
+
+  key_permissions = [
+    "Get",
+    "UnwrapKey",
+    "WrapKey",
+  ]
+}
+
 resource "azurerm_key_vault_secret" "report_stream_public_key" {
   name  = "organization-report-stream-public-key-${var.environment}"
   value = "dogcow"
