07374a48f0af4678eafff4765861daf598e8ab28
saquino0827 <saquino@flexion.us>
2024-09-13 09:35:51 -0500

    Extract alert non pr conditional check as a local variable

--- operations/template/main.tf
+++ operations/template/main.tf
@@ -8,6 +8,7 @@ locals {
   rs_domain_prefix               = "${local.selected_rs_environment_prefix}${length(local.selected_rs_environment_prefix) == 0 ? "" : "."}"
   higher_environment_level       = var.environment == "stg" || var.environment == "prd"
   cdc_domain_environment         = var.environment == "dev" || var.environment == "stg" || var.environment == "prd"
+  non_pr_environment             = contains(["internal", "dev", "stg", "prd"], var.environment)
 }
 
 data "azurerm_resource_group" "group" {
