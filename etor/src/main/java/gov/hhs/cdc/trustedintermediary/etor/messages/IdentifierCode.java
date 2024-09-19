b44c4079b9138a386e3599a6705ac3f0fa7a7b3f
Joel Biskie <jbiskie@flexion.us>
2024-09-19 09:46:59 -0500

    Minor naming tweaks and comment updates

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/IdentifierCode.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/messages/IdentifierCode.java
@@ -3,7 +3,7 @@ package gov.hhs.cdc.trustedintermediary.etor.messages;
 /**
  * Represents an identifier triplet, consisting of a code, display value, and coding system.
  * Identifiers are used in HL7 fields such as OBR-4 and OBX-3. These fields contain two identifier
- * triplets, one in subfields 1/2/3, and an alternate in subfields 4/5/6. See: <a
+ * triplets, one in subfields 1/2/3, and an alternate in subfields 4/5/6. For reference, see: <a
  * href="https://hl7-definition.caristix.com/v2/HL7v2.5.1/Fields/OBX.3"</a>
  */
 public record IdentifierCode(String code, String display, String codingSystem) {}
--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/MapLocalObservationCodes.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/MapLocalObservationCodes.java
@@ -15,21 +15,24 @@ import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.StringType;
 
 /**
- * Maps local observation codes in OBX-3.4/5/6 to LOINC/PLT codes, placing the LOINC/PLT codes in
- * OBX-3.1/2/3.
+ * Maps local observation codes to LOINC/PLT codes. In order to map a code, the observation must
+ * have a single local code with cwe-coding extension 'alt-coding'. The mapped LOINC or PLT code is
+ * added to the observation as an additional observation coding with cwe-coding extension 'coding'.
+ * When converted to/from HL7, these codings correspond to OBX-3.4/5/6 for the local code, and
+ * OBX-3.1/2/3 for the LOINC/PLT code.
  */
 public class MapLocalObservationCodes implements CustomFhirTransformation {
     protected final Logger LOGGER = ApplicationContext.getImplementation(Logger.class);
 
-    private HashMap<String, IdentifierCode> map;
+    private HashMap<String, IdentifierCode> codingMap;
 
     public MapLocalObservationCodes() {
-        InitMap();
+        initMap();
     }
 
     @Override
     public void transform(FhirResource<?> resource, Map<String, String> args) {
-        Bundle bundle = (Bundle) resource.getUnderlyingResource();
+        var bundle = (Bundle) resource.getUnderlyingResource();
         var observations = HapiHelper.resourcesInBundle(bundle, Observation.class);
 
         for (Observation obv : observations.toList()) {
@@ -43,12 +46,12 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
                                 .getValue()
                                 .toString();
 
-                // alt coding is HL7 OBX-3.4,5,6
+                // Alt coding is HL7 OBX-3.4,5,6
                 if (Objects.equals(cwe, "alt-coding")
                         && coding.getSystem().equals(HapiHelper.LOCAL_CODE_URL)) {
 
-                    // look up the code in the hash map
-                    var identifier = map.get(coding.getCode());
+                    // Look up the code in the hash map
+                    var identifier = codingMap.get(coding.getCode());
 
                     if (identifier == null) {
                         // The local code was not found in the mapping
@@ -67,7 +70,7 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
                     // Create a new coding source and add it to the coding list
                     var mappedCoding =
                             new Coding(
-                                    UrlForCodetype(identifier.codingSystem()),
+                                    urlForCodeType(identifier.codingSystem()),
                                     identifier.code(),
                                     identifier.display());
                     mappedCoding.addExtension(
@@ -83,7 +86,7 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
         }
     }
 
-    private String UrlForCodetype(String code) {
+    private String urlForCodeType(String code) {
         return switch (code) {
             case HapiHelper.LOINC_CODE -> HapiHelper.LOINC_URL;
             case HapiHelper.PLT_CODE -> null;
@@ -91,27 +94,27 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
         };
     }
 
-    private void InitMap() {
-        this.map = new HashMap<String, IdentifierCode>();
-        map.put(
+    private void initMap() {
+        this.codingMap = new HashMap<String, IdentifierCode>();
+        codingMap.put(
                 "99717-32",
                 new IdentifierCode(
                         "85269-9",
                         "X-linked Adrenoleukodystrophy (X- ALD) newborn screen interpretation",
                         HapiHelper.LOINC_CODE));
-        map.put(
+        codingMap.put(
                 "99717-33",
                 new IdentifierCode(
                         "85268-1",
                         "X-linked Adrenoleukodystrophy (X- ALD) newborn screening comment-discussion",
                         HapiHelper.LOINC_CODE));
-        map.put(
+        codingMap.put(
                 "99717-34",
                 new IdentifierCode(
                         "PLT325",
                         "ABCD1 gene mutation found [Identifier] in DBS by Sequencing",
                         HapiHelper.PLT_CODE));
-        map.put(
+        codingMap.put(
                 "99717-6",
                 new IdentifierCode(
                         "53340-6",
@@ -121,7 +124,7 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
         // HapiHelper.PLT_CODE));
         // map.put("99717-36", new IdentifierCode("REQUEST_PLT", "REQUEST_PLT",
         // HapiHelper.PLT_CODE));
-        map.put(
+        codingMap.put(
                 "99717-48",
                 new IdentifierCode(
                         "PLT3258",
@@ -129,13 +132,13 @@ public class MapLocalObservationCodes implements CustomFhirTransformation {
                         HapiHelper.PLT_CODE));
         // map.put("99717-44", new IdentifierCode("REQUEST_PLT", "REQUEST_PLT",
         // HapiHelper.PLT_CODE));
-        map.put(
+        codingMap.put(
                 "99717-50",
                 new IdentifierCode(
                         "PLT3275",
                         "IDS gene mutations found [Identifier] in Dried Bloodspot by Molecular genetics method Nominal",
                         HapiHelper.PLT_CODE));
-        map.put(
+        codingMap.put(
                 "99717-47",
                 new IdentifierCode(
                         "PLT3252",
