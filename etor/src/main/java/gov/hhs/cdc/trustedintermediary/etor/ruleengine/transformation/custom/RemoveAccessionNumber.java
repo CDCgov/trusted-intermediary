33221adac2cc6637e91f9eaf680e2eacc50e9b5a
Luis Pabon <turkeyfried@gmail.com>
2024-09-26 13:12:36 -0400

    Update RemoveAccessionNumber.java
    Reworking the transformation to remove via entry

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveAccessionNumber.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveAccessionNumber.java
@@ -3,10 +3,13 @@ package gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.custom;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.FhirResource;
 import gov.hhs.cdc.trustedintermediary.etor.ruleengine.transformation.CustomFhirTransformation;
 import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Resource;
 
 public class RemoveAccessionNumber implements CustomFhirTransformation {
     private static final String ACCESSION_NUMBER_CODE = "99717-5";
@@ -14,21 +17,21 @@ public class RemoveAccessionNumber implements CustomFhirTransformation {
     @Override
     public void transform(FhirResource<?> resource, Map<String, String> args) {
         var bundle = (Bundle) resource.getUnderlyingResource();
-        var observations = HapiHelper.resourcesInBundle(bundle, Observation.class);
+        Set<Resource> resourcesToRemove = new HashSet<>();
 
-        for (Observation obv : observations.toList()) {
-            var codingList = obv.getCode().getCoding();
+        for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
+            Resource resourceEntry = entry.getResource();
 
-            if (codingList.size() != 1) {
-                continue;
-            }
+            if (resourceEntry instanceof Observation observation) {
+                var coding = observation.getCode().getCodingFirstRep();
 
-            var coding = codingList.get(0);
-
-            if (HapiHelper.hasLocalCodeInAlternateCoding(coding)
-                    && Objects.equals(coding.getCode(), ACCESSION_NUMBER_CODE)) {
-                codingList.clear();
+                if (HapiHelper.hasLocalCodeInAlternateCoding(coding)
+                        && Objects.equals(coding.getCode(), ACCESSION_NUMBER_CODE)) {
+                    resourcesToRemove.add(observation);
+                }
             }
         }
+
+        bundle.getEntry().removeIf(entry -> resourcesToRemove.contains(entry.getResource()));
     }
 }
