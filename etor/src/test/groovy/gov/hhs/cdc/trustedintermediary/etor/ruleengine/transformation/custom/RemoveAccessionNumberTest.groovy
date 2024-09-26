7f0399cf77d5a98365a8c24fa2cc8a1e5a18276f
Joel Biskie <jbiskie@flexion.us>
2024-09-26 13:03:27 -0500

    Adding 2 more tests

--- etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveAccessionNumberTest.groovy
+++ etor/src/test/groovy/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemoveAccessionNumberTest.groovy
@@ -105,6 +105,62 @@ class RemoveAccessionNumberTest extends Specification {
         "99717-5" | "L"             | "coding"
     }
 
+    def "When an observation has no identifier (OBX-3), it should NOT be removed"() {
+        given:
+        def bundle = HapiFhirHelper.createMessageBundle(messageTypeCode: 'ORU_R01')
+
+        // Add an observation with an observation value and a status, but no observation identifier
+        def observation = new Observation()
+        observation.status = Observation.ObservationStatus.FINAL
+        def valueCoding = new Coding()
+        valueCoding.code = "123456"
+        observation.valueCodeableConcept.coding.add(valueCoding)
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(observation))
+
+        def args = getArgs("55555-5", "LN", "coding")
+
+        expect:
+        HapiHelper.resourcesInBundle(bundle, Observation.class).count() == 1
+
+        when:
+        transformClass.transform(new HapiFhirResource(bundle), args)
+
+        then:
+        HapiHelper.resourcesInBundle(bundle, Observation.class).count() == 1
+    }
+
+    def "When there is >1 matching observation, all matching observations should be removed"() {
+        given:
+        final String MATCHING_CODE = "99717-5"
+        final String MATCHING_CODING_SYSTEM_EXT = "L"
+        final String MATCHING_CODING_EXT = "alt-coding"
+
+        def bundle = HapiFhirHelper.createMessageBundle(messageTypeCode: 'ORU_R01')
+
+        def observation1 = new Observation()
+        def observation2 = new Observation()
+        def observation3 = new Observation()
+
+        addCodingToObservation(observation1, MATCHING_CODE, MATCHING_CODING_SYSTEM_EXT, MATCHING_CODING_EXT)
+        addCodingToObservation(observation2, MATCHING_CODE, MATCHING_CODING_SYSTEM_EXT, MATCHING_CODING_EXT)
+        addCodingToObservation(observation3, "A_DIFFERENT_CODE", MATCHING_CODING_SYSTEM_EXT, MATCHING_CODING_EXT)
+
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(observation1))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(observation2))
+        bundle.addEntry(new Bundle.BundleEntryComponent().setResource(observation3))
+
+        def args = getArgs(MATCHING_CODE, MATCHING_CODING_SYSTEM_EXT, MATCHING_CODING_EXT)
+
+        expect:
+        HapiHelper.resourcesInBundle(bundle, Observation.class).count() == 3
+
+        when:
+        transformClass.transform(new HapiFhirResource(bundle), args)
+
+        then:
+        HapiHelper.resourcesInBundle(bundle, Observation.class).count() == 1
+    }
+
     void addCodingToObservation(Observation observation, String code, String codingSystemExtension, String codingExtension) {
         def coding = new Coding()
 
@@ -116,8 +172,8 @@ class RemoveAccessionNumberTest extends Specification {
 
     Map<String, String> getArgs(String code, String codingSystem, String coding) {
         return [
-            (CODE_NAME): code,
+            (CODE_NAME)         : code,
             (CODING_SYSTEM_NAME): codingSystem,
-            (CODING_NAME): coding]
+            (CODING_NAME)       : coding]
     }
 }
