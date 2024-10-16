aec5cef241b7e87f65de05a859f1368042d4e337
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-10-16 16:41:40 -0700

    Added a way to get the outboud submission id, and then check that the second leg of the message is delivered

--- scripts/hurl/rs/update_examples.sh
+++ scripts/hurl/rs/update_examples.sh
@@ -9,6 +9,35 @@ fi
 # Maximum time to wait for the overallStatus to be "Delivered" (in seconds)
 TIMEOUT=300       # 5 minutes
 RETRY_INTERVAL=10 # Retry every 10 seconds
+CURRENT_DIR=$(pwd)
+
+check_submission_status() {
+    local submission_id=$1
+    start_time=$(date +%s)
+
+    while true; do
+        history_response=$(./hrl history.hurl -i "$submission_id")
+        overall_status=$(echo "$history_response" | jq -r '.overallStatus')
+
+        echo -n "Status: $overall_status"
+        if [ "$overall_status" == "Delivered" ]; then
+            echo "!"
+            return 0 # Success
+        else
+            echo ". Retrying in $RETRY_INTERVAL seconds..."
+        fi
+
+        # Check if the timeout has been reached
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+        if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
+            echo "Timeout reached after $elapsed_time seconds. Status is still: $overall_status."
+            return 1 # Timeout
+        fi
+
+        sleep $RETRY_INTERVAL
+    done
+}
 
 find "$CDCTI_HOME/examples/MN" -type f -name '*0_initial_message.hl7' | while read -r file; do
     echo "Submitting file: $file"
@@ -19,42 +48,34 @@ find "$CDCTI_HOME/examples/MN" -type f -name '*0_initial_message.hl7' | while re
 
     # Read the file content, update MSH-6, and write the result to a temporary file
     awk -F'|' '/^MSH/ { $6 = "^simulated-hospital-id^" } 1' OFS='|' "$file" >"$temp_file"
-    echo "$temp_file"
+    # echo "$temp_file"
 
     # Submit the updated file, capture the JSON response and extract the submission ID
     waters_response=$(./hrl waters.hurl -f "$temp_file_name" -r "$temp_dir_path")
     submission_id=$(echo "$waters_response" | jq -r '.id')
 
-    if [ -n "$submission_id" ]; then
-        echo "Submission ID: $submission_id"
-        history_response=$(./hrl history.hurl -i $submission_id)
-        status=$(echo "$history_response" | jq '.overallStatus')
-    else
-        echo "Failed to extract submission ID."
+    echo "[First leg] Checking submission status for ID: $submission_id"
+    if ! check_submission_status "$submission_id"; then
+        echo "Failed to deliver the first leg of the message. Skipping the next steps."
+        rm "$temp_file"
+        continue
     fi
 
-    start_time=$(date +%s)
-    while true; do
-        history_response=$(./hrl history.hurl -i "$submission_id")
-        overall_status=$(echo "$history_response" | jq -r '.overallStatus')
+    cd ../ti || exit 1
+    inbound_submission_id=$(echo $history_response | jq '.destinations[0].sentReports[0].externalName' | sed 's/^[^-]*-\(.*\)-[^-]*$/\1/')
+    metadata_response=$(./hrl metadata.hurl -i "$inbound_submission_id")
+    outbound_submission_id=$(echo "$metadata_response" | jq -r '.issue[] | select(.details.text == "outbound submission id") | .diagnostics')
+    cd "$CURRENT_DIR"
 
-        if [ "$overall_status" == "Delivered" ]; then
-            echo "Submission $submission_id is Delivered."
-            break
-        else
-            echo "Status: $overall_status. Retrying in $RETRY_INTERVAL seconds..."
-        fi
+    echo "[Second leg] Checking submission status for ID: $outbound_submission_id"
+    if ! check_submission_status "$outbound_submission_id"; then
+        echo "Failed to deliver the second leg of the message. Skipping the next steps."
+        rm "$temp_file"
+        continue
+    fi
 
-        # Check if the timeout has been reached
-        current_time=$(date +%s)
-        elapsed_time=$((current_time - start_time))
-        if [ "$elapsed_time" -ge "$TIMEOUT" ]; then
-            echo "Timeout reached after $elapsed_time seconds. Status is still: $overall_status."
-            break
-        fi
-
-        sleep $RETRY_INTERVAL
-    done
+    final_file_name=$(echo $history_response | jq '.destinations[0].sentReports[0].externalName')
+    echo "Final file name: $final_file_name"
 
     rm "$temp_file"
 
