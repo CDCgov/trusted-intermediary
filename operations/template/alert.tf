07fbaf3dee15f577942d6de7eac4f05edb5e4059
James Herr <jherr@flexion.us>
2024-10-18 11:19:37 -0500

    Attempt action_group_id fix
    Co-Authored-By: Samuel Aquino <saquino@flexion.us>

--- operations/template/alert.tf
+++ operations/template/alert.tf
@@ -46,7 +46,7 @@ resource "azurerm_monitor_activity_log_alert" "azure_service_health_alert" {
   }
 
   action {
-    action_group_id = [azurerm_monitor_action_group.notify_slack_email[count.index].id]
+    action_group_id = azurerm_monitor_action_group.notify_slack_email[count.index].id
     email_subject   = "FATAL: Azure Outage Alert!"
   }
 
