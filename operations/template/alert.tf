1f7344f68fb1e4a229fafcd145a47cbaa90655cd
saquino0827 <saquino@flexion.us>
2024-10-21 16:09:19 -0500

    Add 4XX metric alert and update ignore tags
    Co-authored-by: pluckyswan <96704946+pluckyswan@users.noreply.github.com>
    Co-authored-by: James Herr <jherr@flexion.us>

--- operations/template/alert.tf
+++ operations/template/alert.tf
@@ -9,21 +9,10 @@ resource "azurerm_monitor_action_group" "notify_slack_email" {
     email_address = var.alert_slack_email
   }
 
-  #   below tags are managed by CDC
   lifecycle {
     ignore_changes = [
-      tags["business_steward"],
-      tags["center"],
-      tags["environment"],
-      tags["escid"],
-      tags["funding_source"],
-      tags["pii_data"],
-      tags["security_compliance"],
-      tags["security_steward"],
-      tags["support_group"],
-      tags["system"],
-      tags["technical_steward"],
-      tags["zone"]
+      # Ignore changes to tags because the CDC sets these automagically
+      tags,
     ]
   }
 }
@@ -53,18 +42,8 @@ resource "azurerm_monitor_activity_log_alert" "azure_service_health_alert" {
 
   lifecycle {
     ignore_changes = [
-      tags["business_steward"],
-      tags["center"],
-      tags["environment"],
-      tags["escid"],
-      tags["funding_source"],
-      tags["pii_data"],
-      tags["security_compliance"],
-      tags["security_steward"],
-      tags["support_group"],
-      tags["system"],
-      tags["technical_steward"],
-      tags["zone"]
+      # Ignore changes to tags because the CDC sets these automagically
+      tags,
     ]
   }
 }
@@ -102,21 +81,10 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "database_token_expired_a
     threshold = 1
   }
 
-  #   below tags are managed by CDC
   lifecycle {
     ignore_changes = [
-      tags["business_steward"],
-      tags["center"],
-      tags["environment"],
-      tags["escid"],
-      tags["funding_source"],
-      tags["pii_data"],
-      tags["security_compliance"],
-      tags["security_steward"],
-      tags["support_group"],
-      tags["system"],
-      tags["technical_steward"],
-      tags["zone"]
+      # Ignore changes to tags because the CDC sets these automagically
+      tags,
     ]
   }
 }
@@ -125,24 +93,28 @@ resource "azurerm_monitor_metric_alert" "azure_4XX_alert" {
   count               = local.non_pr_environment ? 1 : 0
   name                = "cdcti-${var.environment}-azure-http-4XX-alert"
   resource_group_name = data.azurerm_resource_group.group.name
-  scopes              = [azurerm_storage_account.storage.id]
-  description         = "Action will be triggered when Transactions count is greater than 50."
+  scopes              = [data.azurerm_resource_group.group.id]
+  description         = "Action will be triggered when Http Status Code 4XX is greater than or equal to 1"
 
   criteria {
-    metric_namespace = "Microsoft.Storage/storageAccounts"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 50
-
-    dimension {
-      name     = "ApiName"
-      operator = "Include"
-      values   = ["*"]
-    }
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "4XX-Http-Statuses"
+    aggregation      = "Count"
+    operator         = "GreaterThanOrEqualTo"
+    threshold        = 1
+    frequency        = "PT1M" // Checks every 1 minute
+    window_size      = "PT5M" // Every Check, looks back 5 minutes in history
+    //TBD: How frequent do we want this alert and how far do we want it to look back.
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.main.id
+    action_group_id = azurerm_monitor_action_group.notify_slack_email[count.index].id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags because the CDC sets these automagically
+      tags,
+    ]
   }
 }
