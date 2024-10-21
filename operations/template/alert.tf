8b86d18ad87faad3aef01f3b2f64542088d38a6d
Bella Luz Quintero <bquintero@flexion.us>
2024-10-21 15:54:06 -0600

    fixed operator value

--- operations/template/alert.tf
+++ operations/template/alert.tf
@@ -95,15 +95,15 @@ resource "azurerm_monitor_metric_alert" "azure_4XX_alert" {
   resource_group_name = data.azurerm_resource_group.group.name
   scopes              = [data.azurerm_resource_group.group.id]
   description         = "Action will be triggered when Http Status Code 4XX is greater than or equal to 1"
-  frequency        = "PT1M" // Checks every 1 minute
-  window_size      = "PT5M" // Every Check, looks back 5 minutes in history
+  frequency           = "PT1M" // Checks every 1 minute
+  window_size         = "PT5M" // Every Check, looks back 5 minutes in history
   //TBD: How frequent do we want this alert and how far do we want it to look back.
 
   criteria {
     metric_namespace = "Microsoft.Web/sites"
     metric_name      = "Http4xx"
     aggregation      = "Count"
-    operator         = "GreaterThanOrEqualTo"
+    operator         = "GreaterThanOrEqual"
     threshold        = 1
   }
 
