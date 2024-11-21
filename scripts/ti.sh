421ae5b3b750726009a7d0dc30bb402de4804df4
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-11-21 10:37:07 -0800

    Hurl scripts refactor (#1522)
    * Simplified load-etor-org-settings script to only run in local env
    
    * Using preferred gradlew commands instead of prime ones. Also added env variable for RS_HOME
    
    * Moved common functions to /scripts/utils.sh and importing scripts using absolute path
    
    * Extract env variables to a shared .env file, updated docs, renamed some of the envars. Also working of having absolute paths so the scripts could run from anywhere
    
    * Fixed .env loading and some cleanup
    
    * Renamed reset.sh => setup.sh
    
    * Consolidated RS setup scripts into one
    
    * Moved rs/setup.sh to setup-rs.sh and removed rs/ folder
    
    * Updated RS setup instructions in readme to:
    - Use new setup-rs.sh script
    - Add alternate ways to build and run RS
    - Fixed RS docs URL
    - Clean up and simplify
    
    * Moved files to reorganize and simplify file structure
    
    * Updated paths
    
    * Reverted moving hurl files
    
    * Moved and renamed function scripts
    
    * Moved and renamed more scripts + added to readme
    
    * Moved function to common.sh
    
    * Removed unused hurl/readme.md
    
    * Fixed .env file reference
    
    * Added to readme and added user message to find instructions
    
    * Added context to instruction
    
    * Added setup script for env vars
    
    * Changed relative path to hurl files for absolute paths
    
    * Fixed typos and cleanup
    
    * Removed setup script not working as intended
    
    * Readme update
    
    * Fixed issues after running shellcheck
    
    * Removed dead code and renamed readme section
    
    * Reverted adding double quotes as it introduces a bug
    
    * Added missing export of env vars that are required to create vault credentials
    
    * Fixed typo
    
    * Renamed client => sender, cleanup
    
    * Refactored to extract common code + fix bug
    
    * Updated readme + cleanup
    
    * Refactored to extract more common code and changed to use global variables for consistency and fto simplify
    
    * Refactored scripts to include epic.sh
    
    * Refactored to simplify + cleanup
    
    * Cleanup
    
    * Renamed epic => ucsd
    
    * Renamed epic => ucsd

--- scripts/ti.sh
+++ scripts/ti.sh
@@ -2,91 +2,19 @@
 
 [ -z "${CDCTI_HOME}" ] && echo "Error: Environment variable CDCTI_HOME is not set. Please refer to /scripts/README.md for instructions" && exit 1
 source "$CDCTI_HOME/scripts/lib/common.sh"
+source "$CDCTI_HOME/scripts/lib/api.sh"
 
 # default values
-env=local
-root=$CDCTI_HOME/examples/
-client=report-stream
+ENVIRONMENT=local
+ROOT_PATH=$CDCTI_HOME/examples/
+CONTENT_TYPE=application/fhir+ndjson
+SENDER=report-stream
+DEFAULT_SENDER_PRIVATE_KEY_ORG=report-stream
+DEFAULT_SENDER_PRIVATE_KEY_PATH="$RS_LOCAL_PRIVATE_KEY_PATH"
 
-show_usage() {
-    cat <<EOF
-Usage: ./$(basename "$0") <ENDPOINT_NAME> [OPTIONS]
-
-ENDPOINT_NAME:
-    The name of the endpoint to call (required)
-
-Options:
-    -f <REL_PATH>       Path to the hl7/fhir file to submit (Required for orders and results APIs)
-    -r <ROOT_PATH>      Root path to the hl7/fhir files (Default: $root)
-    -e <ENVIRONMENT>    Environment: local|staging (Default: $env)
-    -c <CLIENT>         Client ID to create JWT with (Default: $client)
-    -k <KEY_PATH>       Path to the client private key (Required for non-local environments)
-    -i <SUBMISSION_ID>  Submission ID for metadata API (Required for orders, results and metadata API)
-    -v                  Verbose mode
-    -h                  Display this help and exit
-EOF
+parse_args "ti" "$@" || {
+    show_usage "$(basename "$0")"
+    exit 0
 }
 
-parse_arguments() {
-    if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        show_usage
-        exit 0
-    fi
-
-    [ $# -eq 0 ] && fail "Missing required argument <ENDPOINT_NAME>"
-    hurl_file_path="$CDCTI_HOME/scripts/hurl/ti/$1.hurl"
-    shift # Remove endpoint name from args
-
-    while getopts ':f:r:e:c:k:i:v' opt; do
-        case "$opt" in
-        f) fpath="$OPTARG" ;;
-        r) root="$OPTARG" ;;
-        e) env="$OPTARG" ;;
-        c) client="$OPTARG" ;;
-        k) private_key="$OPTARG" ;;
-        i) submission_id="--variable submissionid=$OPTARG" ;;
-        v) verbose="--verbose" ;;
-        ?) fail "Invalid option -$OPTARG" ;;
-        esac
-    done
-
-    shift "$((OPTIND - 1))"
-    remaining_args="$*"
-}
-
-setup_credentials() {
-    if [ -z "$private_key" ] && [ "$client" = "report-stream" ] && [ "$env" = "local" ]; then
-        if [ -f "$RS_LOCAL_PRIVATE_KEY_PATH" ]; then
-            private_key="$RS_LOCAL_PRIVATE_KEY_PATH"
-        else
-            fail "Local environment client private key not found at: $RS_LOCAL_PRIVATE_KEY_PATH"
-        fi
-    fi
-
-    if [ "$env" != "local" ]; then
-        [ -z "$private_key" ] && fail "Client private key (-k) is required for non-local environments"
-    fi
-
-    [ ! -f "$private_key" ] && fail "Client private key file not found: $private_key"
-}
-
-run_hurl_command() {
-    url=$(get_api_url "$env" "ti")
-    host=$(extract_host_from_url "$url")
-    jwt_token=$(generate_jwt "$client" "$host" "$private_key") || fail "Failed to generate JWT token"
-
-    hurl \
-        --variable "fpath=$fpath" \
-        --file-root "$root" \
-        --variable "url=$url" \
-        --variable "client=$client" \
-        --variable "jwt=$jwt_token" \
-        ${submission_id:-} \
-        ${verbose:-} \
-        "$hurl_file_path" \
-        ${remaining_args:+$remaining_args}
-}
-
-parse_arguments "$@"
-setup_credentials
-run_hurl_command
+handle_api_request "ti" "$@"
