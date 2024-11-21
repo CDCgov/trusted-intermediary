cb554c13202a049ab3ac7f852a9f034416b5657a
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-11-21 10:26:19 -0800

    Renamed epic => ucsd

--- scripts/ucsd.sh
+++ scripts/ucsd.sh
@@ -10,9 +10,9 @@ ROOT_PATH=$CDCTI_HOME/examples/CA/
 CONTENT_TYPE=application/hl7-v2
 URL=https://epicproxy-np.et0502.epichosted.com/FhirProxy/oauth2/token
 
-parse_args "epic" "$@" || {
+parse_args "ucsd" "$@" || {
     show_usage "$(basename "$0")"
     exit 0
 }
 
-handle_api_request "epic" "$@"
+handle_api_request "ucsd" "$@"
