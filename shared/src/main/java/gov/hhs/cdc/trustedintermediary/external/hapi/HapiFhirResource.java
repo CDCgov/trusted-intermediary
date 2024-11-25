067eba4596d11293bd48499939addbd6309c109d
jorge Lopez <jlopez@flexion.us>
2024-11-25 15:59:20 -0800

    changes: added logger to HapiFhirResource in order to test a non-singleton class created HapiFhirResourceTest in order to test the changes to HapiFhirResource

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
@@ -1,19 +1,25 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi;
 
+import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
 import gov.hhs.cdc.trustedintermediary.wrappers.HealthData;
+import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
+import javax.inject.Inject;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 /** An implementation of {@link HealthData} to use as a wrapper around HAPI FHIR IBaseResource */
 public class HapiFhirResource implements HealthData<IBaseResource> {
 
+    @Inject private Logger logger;
     private final IBaseResource innerResource;
 
     public HapiFhirResource(IBaseResource innerResource) {
+        ApplicationContext.injectIntoNonSingleton(this);
         this.innerResource = innerResource;
     }
 
     @Override
     public IBaseResource getUnderlyingData() {
+        this.logger.logDebug("testing @Inject");
         return innerResource;
     }
 }
