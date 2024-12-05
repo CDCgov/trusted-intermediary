b6846e3ce6208c85b7e50d0df50b1dbc8a8ba726
Joel Biskie <jbiskie@flexion.us>
2024-12-05 15:01:30 -0600

    WIP - remove cx.5 patient identifier warnings

--- etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemovePatientIdentifiers.java
+++ etor/src/main/java/gov/hhs/cdc/trustedintermediary/etor/ruleengine/transformation/custom/RemovePatientIdentifiers.java
@@ -5,6 +5,7 @@ import gov.hhs.cdc.trustedintermediary.external.hapi.HapiHelper;
 import gov.hhs.cdc.trustedintermediary.wrappers.HealthData;
 import java.util.Map;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Identifier;
 
 /**
  * Removes Assigning Authority (PID-3.4) and Identifier Type Code (PID-3.5) from Patient Identifier
@@ -15,7 +16,26 @@ public class RemovePatientIdentifiers implements CustomFhirTransformation {
     @Override
     public void transform(HealthData<?> resource, Map<String, Object> args) {
         Bundle bundle = (Bundle) resource.getUnderlyingData();
-        HapiHelper.setPID3_4Value(bundle, ""); // remove PID.3-4
-        HapiHelper.setPID3_5Value(bundle, ""); // remove PID.3-5
+
+        Identifier identifier = HapiHelper.getPID3Identifier(bundle);
+        if (identifier == null) {
+            return;
+        }
+        identifier.setAssigner(null);
+
+        if (identifier.hasExtension(HapiHelper.EXTENSION_CX_IDENTIFIER_URL)) {
+            identifier
+                    .getExtensionByUrl(HapiHelper.EXTENSION_CX_IDENTIFIER_URL)
+                    .removeExtension(HapiHelper.EXTENSION_CX5_URL);
+        }
+
+        if (identifier
+                .getExtensionByUrl(HapiHelper.EXTENSION_CX_IDENTIFIER_URL)
+                .getExtension()
+                .isEmpty()) {
+            identifier.removeExtension(HapiHelper.EXTENSION_CX_IDENTIFIER_URL);
+        }
+
+        identifier.setType(null);
     }
 }
