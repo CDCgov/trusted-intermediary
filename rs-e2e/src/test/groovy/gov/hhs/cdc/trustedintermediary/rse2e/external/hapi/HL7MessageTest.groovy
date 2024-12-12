600cbd91babbf2d2eab6f5e333a37236bdde82f3
Luis Pabon <turkeyfried@gmail.com>
2024-12-12 12:14:24 -0500

    Update HL7MessageTest.groovy
    Remove library usage on test class, one test not finished yet.

--- rs-e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/rse2e/external/hapi/HL7MessageTest.groovy
+++ rs-e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/rse2e/external/hapi/HL7MessageTest.groovy
@@ -1,31 +1,40 @@
 package gov.hhs.cdc.trustedintermediary.rse2e.external.hapi
 
-import ca.uhn.hl7v2.model.Message
-import ca.uhn.hl7v2.model.v251.message.ORU_R01
-import ca.uhn.hl7v2.model.v251.segment.MSH
 import gov.hhs.cdc.trustedintermediary.rse2e.HL7Message
 import spock.lang.Specification
 
 class HL7MessageTest extends Specification {
 
-    def "getUnderlyingData should correctly initialize and return underlying data"() {
+    def "getUnderlyingData should correctly return itself"() {
         given:
-        def mockMessage = Mock(Message)
-        def hl7Message = new HL7Message(mockMessage)
+        def segments = [
+            MSH: [
+                "MSH|^~\\&|Epic^1.2.840.114350.1.13.145.2.7.2.695071^ISO\n"
+            ],
+            PID: [
+                "PID|1||11102779^^^CR^MR||SMITH^BB SARAH^^^^^L\n"
+            ]
+        ]
+        def hl7Message = new HL7Message(segments as Map<String, List<String>>, "|^~\\&" as char[])
 
         expect:
-        hl7Message.getUnderlyingData() == mockMessage
+        hl7Message.getUnderlyingData() == hl7Message
     }
 
     def "getIdentifier should return the MSH-10 identifier of the underlying message"() {
         given:
         def expectedIdentifier = "0001"
-        def oruMessage = new ORU_R01()
-        MSH mshSegment = oruMessage.getMSH()
-        mshSegment.getMessageControlID().setValue(expectedIdentifier)
+        def segments = [
+            MSH: [
+                "MSH|^~\\&|Epic^1.2.840.114350.1.13.145.2.7.2.695071^ISO\n"
+            ],
+            PID: [
+                "PID|1||11102779^^^CR^MR||SMITH^BB SARAH^^^^^L\n"
+            ]
+        ]
 
         and:
-        def hl7Message = new HL7Message(oruMessage)
+        def hl7Message = new HL7Message(segments, "|^~\\&" as char[])
 
         when:
         def name = hl7Message.getIdentifier()
