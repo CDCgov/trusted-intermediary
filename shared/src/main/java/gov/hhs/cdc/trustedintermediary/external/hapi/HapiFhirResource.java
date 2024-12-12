37a57fa48812bcb99da2b3806cdb04e6721c29b8
jorge Lopez <jlopez@flexion.us>
2024-12-12 11:35:12 -0800

    deleted test changes: in HapiFhirResource and deleted file HapifhirResourceTest.groovy

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
@@ -1,26 +1,19 @@
 package gov.hhs.cdc.trustedintermediary.external.hapi;
 
-import gov.hhs.cdc.trustedintermediary.context.ApplicationContext;
 import gov.hhs.cdc.trustedintermediary.wrappers.HealthData;
-import gov.hhs.cdc.trustedintermediary.wrappers.Logger;
-import javax.inject.Inject;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 /** An implementation of {@link HealthData} to use as a wrapper around HAPI FHIR IBaseResource */
 public class HapiFhirResource implements HealthData<IBaseResource> {
 
-    @Inject private Logger logger; // this line will get deleted once PR is apporved.
     private final IBaseResource innerResource;
 
     public HapiFhirResource(IBaseResource innerResource) {
-        ApplicationContext.injectIntoNonSingleton(
-                this); // this line will get deleted once PR is apporved.
         this.innerResource = innerResource;
     }
 
     @Override
     public IBaseResource getUnderlyingData() {
-        this.logger.logDebug("testing @Inject"); // this line will get deleted once PR is apporved.
         return innerResource;
     }
 }
