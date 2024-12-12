ba81073377d78dd88f50aa578b92895f2e7e0466
jorge Lopez <jlopez@flexion.us>
2024-12-12 09:32:54 -0800

    added comments to indicate changes that will be deleted

--- shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
+++ shared/src/main/java/gov/hhs/cdc/trustedintermediary/external/hapi/HapiFhirResource.java
@@ -9,17 +9,18 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 /** An implementation of {@link HealthData} to use as a wrapper around HAPI FHIR IBaseResource */
 public class HapiFhirResource implements HealthData<IBaseResource> {
 
-    @Inject private Logger logger;
+    @Inject private Logger logger; // this line will get deleted once PR is apporved.
     private final IBaseResource innerResource;
 
     public HapiFhirResource(IBaseResource innerResource) {
-        ApplicationContext.injectIntoNonSingleton(this);
+        ApplicationContext.injectIntoNonSingleton(
+                this); // this line will get deleted once PR is apporved.
         this.innerResource = innerResource;
     }
 
     @Override
     public IBaseResource getUnderlyingData() {
-        this.logger.logDebug("testing @Inject");
+        this.logger.logDebug("testing @Inject"); // this line will get deleted once PR is apporved.
         return innerResource;
     }
 }
