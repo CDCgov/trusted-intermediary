d0d73b2d2524609fac575f448f85cb1f8071ae86
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-12-12 11:24:55 -0800

    Improved delimiter handling and added test assertions

--- rs-e2e/src/main/java/gov/hhs/cdc/trustedintermediary/rse2e/HL7Message.java
+++ rs-e2e/src/main/java/gov/hhs/cdc/trustedintermediary/rse2e/HL7Message.java
@@ -12,11 +12,18 @@ import java.util.regex.Pattern;
 public class HL7Message implements HealthData<HL7Message> {
 
     private final Map<String, List<String>> segments;
-    private final char[] delimiters;
+    private final Map<String, Character> delimiters;
+    private final Character escapeCharacter;
 
-    public HL7Message(Map<String, List<String>> segments, char[] delimiters) {
+    public HL7Message(Map<String, List<String>> segments, char[] encodingCharacters) {
         this.segments = segments;
-        this.delimiters = delimiters;
+        this.delimiters =
+                Map.of(
+                        "field", HL7Parser.DEFAULT_FIELD_SEPARATOR,
+                        "component", encodingCharacters[0],
+                        "repetition", encodingCharacters[1],
+                        "subcomponent", encodingCharacters[3]);
+        this.escapeCharacter = encodingCharacters[2];
     }
 
     public List<String> getSegment(String segment) {
@@ -29,9 +36,10 @@ public class HL7Message implements HealthData<HL7Message> {
             return null;
         }
 
+        char[] levelDelimiters = this.getOrderedLevelDelimiters();
         String value = fields.get(indices[0] - 1);
         for (int i = 1; i < indices.length; i++) {
-            char levelDelimiter = delimiters[i - 1];
+            char levelDelimiter = levelDelimiters[i];
             int index = indices[i] - 1;
             String[] parts = value.split(Pattern.quote(String.valueOf(levelDelimiter)));
             if (index < 0 || index >= parts.length) {
@@ -40,6 +48,23 @@ public class HL7Message implements HealthData<HL7Message> {
             value = parts[index];
         }
         return value;
+    }
+
+    public char getEscapeCharacter() {
+        return this.escapeCharacter;
+    }
+
+    public char getDelimiter(String type) {
+        return this.delimiters.get(type);
+    }
+
+    public char[] getOrderedLevelDelimiters() {
+        return new char[] {
+            getDelimiter("field"),
+            getDelimiter("component"),
+            getDelimiter("repetition"),
+            getDelimiter("subcomponent")
+        };
     }
 
     @Override
--- rs-e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/rse2e/HL7ParserTest.groovy
+++ rs-e2e/src/test/groovy/gov/hhs/cdc/trustedintermediary/rse2e/HL7ParserTest.groovy
@@ -7,7 +7,7 @@ class HL7ParserTest extends Specification {
     def "test defined assertions on relevant messages"() {
         given:
         def content = """MSH|^~\\&|Sender Application^sender.test.com^DNS|Sender Facility^0.0.0.0.0.0.0.0^ISO|Receiver Application^0.0.0.0.0.0.0.0^ISO|Receiver Facility^automated-staging-test-receiver-id^DNS|20230101010000-0000||ORM^O01^ORM_O01|001|N|2.5.1||||||||||
-PID|1||1300974^^^Baptist East^MR||ONE^TESTCASE||202402210152-0500|F^Female^HL70001||2106-3^White^HL70005|1234 GPCS WAY^^MONTGOMERY^Alabama^36117^USA^home^^Montgomery|||||||2227600015||||N^Not Hispanic or Latino^HL70189|||1|||||
+PID|1||1300974^^^Baptist East^MR||ONE^TESTCASE||202402210152-0500|F^Female^HL70001||2106-3^White^HL70005|1234 GPCS WAY^^MONTGOMERY^Alabama^36117^USA^home^^Montgomery|||||||2227600015||||N^Not Hispanic or Latino^HL70189|||1|||||||||||||||LRI_NG_FRN_PROFILE^^2.16.840.1.113883.9.195.3.4^ISO~LRI_NDBS_COMPONENT^^2.16.840.1.113883.9.195.3.6^ISO~LAB_PRN_Component^^2.16.840.1.113883.9.81^ISO~LAB_TO_COMPONENT^^2.16.840.1.113883.9.22^ISO|
 NK1|1|ONE^MOMFIRST|MTH^Mother^HL70063||^^^^^804^5693861||||||||||||||||||||||||||||123456789^^^Medicaid&2.16.840.1.113883.4.446&ISO^MD||||000-00-0000^^^ssn&2.16.840.1.113883.4.1&ISO^SS
 ORC|NW|4560411583^ORDERID||||||||||12345^^^^^^^^NPI&2.16.840.1.113883.4.6&ISO^L|||||||||
 OBR|1|4560411583^ORDERID||54089-8^Newborn screening panel AHIC^LN|||202402221854-0500|||||||||12345^^^^^^^^NPI&2.16.840.1.113883.4.6&ISO^L||||||||
@@ -18,10 +18,16 @@ OBX|1|ST|57723-9^Unique bar code number of Current sample^LN||123456||||||F|||20
         HL7Message message = HL7Parser.parse(content)
 
         then:
+        message.getValue("PID", 3) == "1300974^^^Baptist East^MR"
         message.getValue("PID", 3, 0) == null
         message.getValue("PID", 3, 1) == "1300974"
+        message.getValue("PID", 3, 2) == ""
         message.getValue("PID", 3, 4) == "Baptist East"
         message.getValue("PID", 3, 5) == "MR"
         message.getValue("PID", 3, 6) == null
+        message.getValue("PID", 40, 4, 1) == "ISO"
+        message.getValue("PID", 40, 4, 2) == "LRI_NDBS_COMPONENT"
+        message.getValue("PID", 40, 4, 3) == null
+        message.getValue("NK1", 33, 4, 1, 1) == "Medicaid"
     }
 }
