12ee9560e08e2952b68a7cd6caa3bfbec7d38cef
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2024-12-23 09:55:58 -0800

    Override equals method in HL7Path to address issue raised by SonarCloud

--- rs-e2e/src/main/java/gov/hhs/cdc/trustedintermediary/rse2e/hl7/HL7Path.java
+++ rs-e2e/src/main/java/gov/hhs/cdc/trustedintermediary/rse2e/hl7/HL7Path.java
@@ -1,4 +1,16 @@
 package gov.hhs.cdc.trustedintermediary.rse2e.hl7;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /** The HL7Path class represents a path to a specific field in an HL7 message. */
-public record HL7Path(String segmentName, int[] indices) {}
+public record HL7Path(String segmentName, int[] indices) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HL7Path hl7Path = (HL7Path) o;
+        return (Objects.equals(segmentName, hl7Path.segmentName))
+                && Arrays.equals(indices, hl7Path.indices);
+    }
+}
